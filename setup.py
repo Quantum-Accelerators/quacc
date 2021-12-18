47a6fdbdbc34825d3a7090ff3345b09b6acc1f67
Andrew S. Rosen <asrosen93@gmail.com>
2021-12-18 12:58:10 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Enhance ASE for high-throughput DFT",
     author="Andrew S. Rosen",
     author_email="asrosen93@gmail.com",
-    url="https://github.com/arosen93/htase",
+    url="https://github.com/arosen93/HT-ASE",
     requires_python=">=3.7.0",
     version="0.0.1",
     packages=find_packages(),
