c69d5e7f8f600acedf56bcce8ff26ca58e2849c5
Andrew <asrosen93@gmail.com>
2022-01-19 22:11:40 -0800

    Fix chargemol caller

--- htase/util/pop_analysis.py
+++ htase/util/pop_analysis.py
@@ -1,5 +1,5 @@
 from pymatgen.command_line.bader_caller import bader_analysis_from_path
-from pymatgen.command_line.bader_caller import chargemol_analysis_from_path
+from pymatgen.command_line.chargemol_caller import ChargemolAnalysis
 import os
 
 
@@ -9,7 +9,8 @@ def run_bader(path=None):
     output files in the given path. This function requires that `bader` or
     `bader.exe` is located in your PATH environment variable. See
     http://theory.cm.utexas.edu/henkelman/code/bader for the bader code.
-
+    Note: If you want to use Bader on a code other than VASP, this function
+    will need to be slightly modified.
     Args:
         path (str): The path where the VASP output files are located.
         Must include CHGCAR, AECCAR0, AECCAR2, and POTCAR files. These
@@ -17,7 +18,16 @@ def run_bader(path=None):
             Default: None (current working directory).
 
     Returns:
-        Dictionary containing the Bader analysis summary.
+        Dictionary containing the Bader analysis summary:
+            {
+                "min_dist": List[float],
+                "atomic_volume": List[float],
+                "vacuum_charge": float,
+                "vacuum_volume": float,
+                "bader_version": float,
+                "partial_charge": List[float],
+                "spin_moment": List[float],
+            }
 
     """
 
@@ -29,7 +39,7 @@ def run_bader(path=None):
         if not os.path.exists(os.path.join(path, f)) and not os.path.exists(
             os.path.join(path, f"{f}.gz")
         ):
-            raise ValueError("Could not find {} in {}".format(f, path))
+            raise FileNotFoundError("Could not find {f} in {path}")
 
     # Run Bader analysis
     bader_stats = bader_analysis_from_path(path)
@@ -43,6 +53,8 @@ def run_bader(path=None):
     # Some cleanup of the returned dictionary
     if "magmom" in bader_stats:
         bader_stats["spin_moment"] = bader_stats["magmom"]
+    else:
+        bader_stats["spin_moment"] = [0.0] * len(bader_stats["partial_charge"])
     bader_stats.pop("charge", None)
     bader_stats.pop("charge_transfer", None)
     bader_stats.pop("reference_used", None)
@@ -51,24 +63,42 @@ def run_bader(path=None):
     return bader_stats
 
 
-def run_chargemol(path=None):
+def run_chargemol(path=None, atomic_densities_path=None):
     """
     Runs a Chargemol (i.e. DDEC6 + CM5) analysis using the VASP output files
     in the given path. This function requires that the chargemol executable,
     given by the name `Chargemol_09_26_2017_linux_parallel`,
     `Chargemol_09_26_2017_linux_serial`, or `chargemol` is in the system PATH
     environment variable. See https://sourceforge.net/projects/ddec/files for
-    the Chargemol code.
+    the Chargemol code. Note: If you want to use Chargemol on a code other
+    than VASP, this function will need to be slightly modified.
 
     Args:
         path (str): The path where the VASP output files are located.
         Must include CHGCAR, AECCAR0, AECCAR2, and POTCAR files. These
         files can be gzip'd or not -- it doesn't matter.
             Default: None (current working directory).
+        atomic_densities_path (str): The path where the reference atomic densities
+        are located for Chargemol. If None, we assume that this
+        directory is defined in an environment variable named DDEC6_ATOMIC_DENSITIES_DIR.
+        See the Chargemol documentation for more information.
 
     Returns:
-        Dictionary containing the Chargemol analysis summary.
-
+        Dictionary containing the Chargemol analysis summary:
+            {
+                "ddec": {
+                            "partial_charges": List[float],
+                            "spin_moments": List[float],
+                            "dipoles": List[float],
+                            "rsquared_moments": List[float],
+                            "rcubed_moments": List[float],
+                            "rfourth_moments": List[float],
+                            "bond_order_dict": Dict
+                        },
+                "cm5": {
+                            "partial_charges": List[float],
+                        }
+            }
     """
     if path is None:
         path = os.getcwd()
@@ -78,9 +108,21 @@ def run_chargemol(path=None):
         if not os.path.exists(os.path.join(path, f)) and not os.path.exists(
             os.path.join(path, f"{f}.gz")
         ):
-            raise ValueError("Could not find {} in {}".format(f, path))
+            raise FileNotFoundError(f"Could not find {f} in {path}")
 
     # Run Chargemol analysis
-    chargemol_stats = chargemol_analysis_from_path(path)
+    chargemol_stats = ChargemolAnalysis(
+        chgcar_filename=os.path.join(path, "CHGCAR"),
+        potcar_filename=os.path.join(path, "POTCAR"),
+        aeccar0_filename=os.path.join(path, "AECCAR0"),
+        aeccar2_filename=os.path.join(path, "AECCAR2"),
+        atomic_densities_path=atomic_densities_path,
+    )
+
+    # Some cleanup of the returned dictionary
+    if "spin_moments" not in chargemol_stats["ddec"]:
+        chargemol_stats["ddec"]["spin_moment"] = [0.0] * len(
+            chargemol_stats["ddec"]["partial_charge"]
+        )
 
     return chargemol_stats
