2fb7dd2d886a29846b001e53ddf256a7c7c863f3
Andrew <asrosen93@gmail.com>
2022-01-21 12:27:31 -0800

    Store Atoms objects with a hashID

--- htase/schemas/common/atoms.py
+++ htase/schemas/common/atoms.py
@@ -5,6 +5,7 @@ from ase.io.jsonio import encode
 from monty.json import jsanitize
 import numpy as np
 from copy import deepcopy
+import hashlib
 
 
 def atoms_to_db(atoms, get_metadata=True, strip_info=False):
@@ -26,6 +27,8 @@ def atoms_to_db(atoms, get_metadata=True, strip_info=False):
     """
 
     atoms = deepcopy(atoms)
+    atoms_no_info = deepcopy(atoms)
+    atoms_no_info.info = {}
     results = {}
 
     # Get Atoms metadata, if requested. Atomate2 already has built-in tools for
@@ -40,17 +43,26 @@ def atoms_to_db(atoms, get_metadata=True, strip_info=False):
     else:
         metadata = {}
 
-    # Store the info flags separately for easy querying
+    # Give the Atoms object a unique ID. This will be helpful for querying later.
+    # Also store any old IDs somewhere else for future reference.
+    if atoms.info.get("_id", None) is not None:
+        if atoms.info.get("_old_ids") is None:
+            atoms.info["_old_ids"] = []
+        atoms.info["_old_ids"].append(atoms.info["_id"])
+    atoms.info["_id"] = hashlib.md5(encode(atoms_no_info).encode("utf-8")).hexdigest()
+
+    # Copy the info flags as a separate entry in the DB for easy querying
     results["atoms_info"] = {}
     for key, val in atoms.info.items():
         # We use jsanitize to make sure all data is stored in
         # a JSON-friendly formaat (or converted to a string if not).
         results["atoms_info"][key] = jsanitize(val)
 
-    # Store the encoded Atoms object without the .info properties
+    # Store the encoded Atoms object
     if strip_info:
-        atoms.info = {}
-    results["atoms"] = encode(atoms)
+        results["atoms"] = encode(atoms_no_info)
+    else:
+        results["atoms"] = encode(atoms)
 
     # Combine the metadata and results dictionaries
     results_full = {**metadata, **results}
