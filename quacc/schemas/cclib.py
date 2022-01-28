df19464eab15dfb2f561d8672fc226c78b8957cb
Andrew <asrosen93@gmail.com>
2022-01-28 15:39:08 -0800

    use new atomate2 cclib parser

--- quacc/schemas/cclib.py
+++ quacc/schemas/cclib.py
@@ -1,6 +1,7 @@
 import os
-from typing import Dict, Optional
+from typing import Dict, List
 from ase.atoms import Atoms
+from atomate2.common.schemas.cclib import TaskDocument
 from cclib.io import ccopen
 from monty.json import jsanitize
 from quacc.schemas.atoms import atoms_to_db
@@ -8,7 +9,10 @@ from quacc.util.atoms import prep_next_run as prep_next_run_
 
 
 def summarize_run(
-    atoms: Atoms, output_file: Optional[str] = None, prep_next_run: bool = True
+    atoms: Atoms,
+    logfile_extensions: str | List[str] = None,
+    dir_path: str = None,
+    prep_next_run: bool = True,
 ) -> Dict:
     """
     Get tabulated results from a molecular DFT run and store them in a database-friendly format.
@@ -18,9 +22,14 @@ def summarize_run(
     ----------
     atoms
         ASE Atoms object following a calculation.
-    output_file
-        Path to the main output file. If None, common output files will be searched for automatically
-        in the working directory, and the one with the most recent modification time will be used.
+    logfile_extensions
+        Possible extensions of the log file (e.g. ".log", ".out", ".txt", ".chk"). Note that
+        only a partial match is needed. For instance, `.log` will match `.log.gz` and `.log.1.gz`.
+        If multiple files with this extension are found, the one with the most recent change time
+        will be used. For an exact match only, put in the full file name.
+    dir_name
+        The path to the folder containing the calculation outputs. A value of None specifies the
+        current working directory.
     prep_next_run
         Whether the Atoms object storeed in {"atoms": atoms} should be prepared for the next run.
         This clears out any attached calculator and moves the final magmoms to the initial magmoms.
@@ -36,31 +45,11 @@ def summarize_run(
     if not atoms.calc.results:
         raise ValueError("ASE Atoms object's calculator has no results.")
 
-    # Try to find the most recent output file
-    if output_file is None:
-        logfile_list = [".log", ".out", ".chk"]  # needs to be updated
-        mod_time = 0
-        for f in os.listdir(os.getcwd()):
-            if f.split(".gz")[0] in logfile_list:
-                if output_file:
-                    if os.path.getmtime(f) > mod_time:
-                        output_file = f
-                        mod_time = os.path.getmtime(output_file)
-                else:
-                    output_file = os.path.getmtime(f)
-                    mod_time = os.path.getmtime(output_file)
+    if dir_path is None:
+        dir_path = os.getcwd()
 
-    # Fetch all tabulated results from the attached calculator
-    if not output_file or not os.path.exists(output_file):
-        raise FileNotFoundError(f"Could not find {output_file}")
-    output_io = ccopen(output_file)
-    if output_io is None:
-        raise ValueError(f"cclib could not parse {output_file}")
-
-    output = output_io.parse()
-    results = {"attributes": output.getattributes()}
-    results["attributes"].pop("metadata", None)
-    metadata = {"metadata": output.metadata}
+    # Fortunately, there is alreayd a cclib parser in Atomate2
+    results = TaskDocument.from_file(dir_path, logfile_extensions)
 
     # Get the calculator inputs
     inputs = {"parameters": atoms.calc.parameters}
@@ -75,7 +64,7 @@ def summarize_run(
     atoms_db = atoms_to_db(atoms)
 
     # Create a dictionary of the inputs/outputs
-    results_full = {**atoms_db, **inputs, **metadata, **results}
+    results_full = {**atoms_db, **inputs, **results}
 
     # Make sure it's all JSON serializable
     task_doc = jsanitize(results_full)
