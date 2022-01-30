555e37160ff34d4543f562119861761bba01de3f
Andrew <asrosen93@gmail.com>
2022-01-29 18:12:23 -0800

    Fix handlng of MSONable

--- quacc/util/json.py
+++ quacc/util/json.py
@@ -2,6 +2,7 @@ from typing import Any
 from ase.atoms import Atom, Atoms
 from ase.io.jsonio import encode
 from monty.json import jsanitize as jsanitize_
+from monty.json import MSONable
 import numpy as np
 
 
@@ -9,7 +10,7 @@ def jsanitize(obj: Any) -> Any:
     """
     Sanitize an input object such that it is JSON serializable. This is
     a wrapper around Monty's jsanitize function but is modified to deal
-    with Atom/Atoms objects.
+    with Atom/Atoms objects and better handling of MSONables.
 
     Anything being passed as an input or output for Jobflow should be
     jsanitized.
@@ -31,5 +32,7 @@ def jsanitize(obj: Any) -> Any:
         return [jsanitize(o) for o in obj]
     if isinstance(obj, dict):
         return {k.__str__(): jsanitize(v) for k, v in obj.items()}
+    if isinstance(obj, MSONable):
+        return obj.as_dict()
 
     return jsanitize_(obj)
