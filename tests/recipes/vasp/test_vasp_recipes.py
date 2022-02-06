82df5ca2fbda03d030199f1d776e9d7f2ca9ff0f
Andrew <asrosen93@gmail.com>
2022-02-06 03:07:23 -0800

    update tests

--- tests/recipes/vasp/test_vasp_recipes.py
+++ tests/recipes/vasp/test_vasp_recipes.py
@@ -1,5 +1,6 @@
 from ase.build import bulk
 from quacc.recipes.vasp.core import RelaxMaker, StaticMaker
+from quacc.recipes.vasp.slabs import SlabRelaxMaker, SlabStaticMaker, SlabToAdsSlabMaker
 from quacc.util.json import jsonify
 from jobflow.managers.local import run_locally
 from pathlib import Path
@@ -9,20 +10,37 @@ import os
 FILE_DIR = Path(__file__).resolve().parent
 run1 = os.path.join(FILE_DIR, "..", "..", "schemas", "vasp_run1")
 
-
+# Copy VASP files to current directory so the "output" can be parsed
 def setup_module():
     for f in os.listdir(run1):
         copy(os.path.join(run1, f), os.path.join(FILE_DIR, f))
 
 
+# Remove copied VASP files after tests are done
 def teardown_module():
     for f in os.listdir(run1):
         os.remove(os.path.join(FILE_DIR, f))
 
 
-def test_static_maker():
-
+def test_core():
     atoms_json = jsonify(bulk("Cu"))
+
     job = StaticMaker().make(atoms_json)
+    output = run_locally(job)
+    assert output is not None
+
+    job = RelaxMaker().make(atoms_json)
+    output = run_locally(job)
+    assert output is not None
+
+
+def test_slabs():
+    atoms_json = jsonify(bulk("Cu"))
+
+    job = SlabStaticMaker().make(atoms_json)
+    output = run_locally(job)
+    assert output is not None
+
+    job = SlabRelaxMaker().make(atoms_json)
     output = run_locally(job)
     assert output is not None
