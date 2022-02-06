21e77c110ddad09f579602e8d5c99985b3d9b144
Andrew <asrosen93@gmail.com>
2022-02-05 23:19:01 -0800

    newline fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,3 @@ def set_env(monkeypatch):
     # Set environment variables in pytest so the tests can run
     monkeypatch.setenv("VASP_PARALLEL_CMD", "")
     monkeypatch.setenv("VASP_PP_PATH", ".")
-    
