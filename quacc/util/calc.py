bbc59dce1eba7d0e5af14bbbef883c2621c9873b
Andrew <asrosen93@gmail.com>
2022-02-05 17:10:07 -0800

    Fix gzipping

--- quacc/util/calc.py
+++ quacc/util/calc.py
@@ -1,13 +1,13 @@
 import os
 from copy import deepcopy
+import gzip
 from shutil import copy, copytree, copyfileobj
 from tempfile import TemporaryDirectory
 from ase.atoms import Atoms
-import warnings
 
 
 def run_calc(
-    atoms: Atoms, run_dir: str = None, scratch_dir: str = None, gzip: bool = False
+    atoms: Atoms, run_dir: str = None, scratch_dir: str = None, compress: bool = False
 ) -> float:
     """
     Run a calculation in a scratch directory and copy the results back to the
@@ -27,8 +27,8 @@ def run_calc(
         Path to the base directory to store the scratch temp directories.
         If None, a temporary directory in $SCRATCH will be used. If $SCRATCH
         is not present, everything will be run in run_dir.
-    gzip : bool
-        Whether to gzip the output files.
+    compress : bool
+        Whether to compress the output files.
 
     Returns
     -------
@@ -51,12 +51,7 @@ def run_calc(
 
     with TemporaryDirectory(dir=scratch_dir, prefix="quacc-") as scratch_path:
 
-        # Copy files from working directory to scratch directory
-        for f in os.listdir(run_dir):
-            if os.path.isdir(os.path.join(run_dir, f)):
-                copytree(os.path.join(run_dir, f), os.path.join(scratch_path, f))
-            else:
-                copy(os.path.join(run_dir, f), os.path.join(scratch_path, f))
+        _copy_to_scratch(run_dir, scratch_path)
 
         # Leave a note in the run directory for where the scratch is located in case
         # the job dies partway through
@@ -69,22 +64,55 @@ def run_calc(
         e = atoms.get_potential_energy()
         os.chdir(run_dir)
 
-        # Copy files from scratch directory to working directory
-        for f in os.listdir(scratch_path):
-            if os.path.isdir(os.path.join(scratch_path, f)):
-                copytree(os.path.join(scratch_path, f), os.path.join(run_dir, f))
-                # NOTE: Would be worth .tar.gz'ing the directory if gzip is True.
-            else:
-                # gzip the files in scratch before copying them back
-                if gzip and os.path.splitext(f)[1] != ".gz":
-                    with open(os.path.join(scratch_path, f), "rb") as f_in:
-                        with gzip.open(os.path.join(run_dir, f + ".gz"), "wb") as f_out:
-                            copyfileobj(f_in, f_out)
-                else:
-                    copy(os.path.join(scratch_path, f), os.path.join(run_dir, f))
+        _copy_from_scratch(run_dir, scratch_path, compress=compress)
 
     # Remove the scratch note
     if os.path.exists(scratch_path_note):
         os.remove(scratch_path_note)
 
     return atoms
+
+
+def _copy_to_scratch(run_dir: str, scratch_path: str):
+    """
+    Copy files from working directory to scratch directory.
+
+    Parameters
+    ----------
+    run_dir : str
+        Path to the run directory
+    scratch_path : str
+        Path to the scratch directory
+    """
+    # Copy files from working directory to scratch directory
+    for f in os.listdir(run_dir):
+        if os.path.isdir(os.path.join(run_dir, f)):
+            copytree(os.path.join(run_dir, f), os.path.join(scratch_path, f))
+        else:
+            copy(os.path.join(run_dir, f), os.path.join(scratch_path, f))
+
+
+def _copy_from_scratch(run_dir: str, scratch_path: str, compress: bool = False):
+    """
+    Copy files from scratch directory to working directory.
+
+    Parameters
+    ----------
+    run_dir : str
+        Path to the run directory
+    scratch_path : str
+        Path to the scratch directory
+    """
+    # Copy files from scratch directory to working directory
+    for f in os.listdir(scratch_path):
+        if os.path.isdir(os.path.join(scratch_path, f)):
+            copytree(os.path.join(scratch_path, f), os.path.join(run_dir, f))
+            # NOTE: Would be worth .tar.gz'ing the directory if compress is True.
+        else:
+            # gzip the files in scratch before copying them back
+            if compress and os.path.splitext(f)[1] != ".gz":
+                with open(os.path.join(scratch_path, f), "rb") as f_in:
+                    with gzip.open(os.path.join(run_dir, f + ".gz"), "wb") as f_out:
+                        copyfileobj(f_in, f_out)
+            else:
+                copy(os.path.join(scratch_path, f), os.path.join(run_dir, f))
