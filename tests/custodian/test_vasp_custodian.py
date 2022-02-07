e27b8d01ecc30ca408de2593ed03a5d290cc8242
Andrew <asrosen93@gmail.com>
2022-02-07 01:28:02 -0800

    fix code comments

--- tests/custodian/test_vasp_custodian.py
+++ tests/custodian/test_vasp_custodian.py
@@ -8,30 +8,27 @@ from quacc.defaults import custodian_settings
 
 
 class MockRun:
+    # Add a mock Custodian.run() function
+
     @staticmethod
     def run():
         return True
 
 
 def mock_custodian_run(*args, **kwargs):
-    # Instead of running the VASP-specific summarize_run(), we mock it with the
-    # general calculator schema which does not require VASP files to be
-    # in the working directory and will work with pytest.
-
+    # Instead of running Custodian, we will mock it to return True
+    # when .run() is called
     return MockRun()
 
 
 @pytest.fixture(autouse=True)
 def patch_custodian_run(monkeypatch):
-    # Monkeypatch the summarize_run() function so that we aren't relying on real
-    # VASP files to be in the working directory during the test. Note that even though
-    # summarize_run() is a function in the quacc.schemas.vasp module, we modify it
-    # only in quacc.recipes.vasp.core/.slabs because otherwise it will not work properly.
+    # Monkeypatch the Custodian.run() function so that it doesn't actually
+    # launch Custodian during a test
     monkeypatch.setattr(Custodian, "run", mock_custodian_run)
 
 
 def test_run_vasp_custodian(monkeypatch):
-
     monkeypatch.setenv(
         "VASP_CUSTODIAN_SETTINGS",
         os.path.join(
