4f4fa993eb35a4f712a7aae5719830603b195f95
Andrew <asrosen93@gmail.com>
2022-02-07 00:11:39 -0800

    bader test suite

---
+++ tests/util/test_pop_analysis.py
@@ -0,0 +1,78 @@
+import os
+import pytest
+
+from quacc.util.pop_analysis import run_bader, run_chargemol
+
+
+def mock_bader_analysis(*args, **kwargs):
+    bader_stats = {
+        "min_dist": [1.0],
+        "charge": [1.0],
+        "atomic_volume": [1.0],
+        "vacuum_charge": 1.0,
+        "vacuum_volume": 1.0,
+        "bader_version": 1.0,
+        "reference_used": [0.0],
+        "charge_transfer": [-1.0],
+        "magmom": [0.0],
+    }
+    return bader_stats
+
+
+def mock_chargemol_analysis(*args, **kwargs):
+    chargemol_stats = {
+        "ddec": {
+            "partial_charges": [1.0],
+            "spin_moments": [0.0],
+            "dipoles": [1.0],
+            "rsquared_moments": [1.0],
+            "rcubed_moments": [1.0],
+            "rfourth_moments": [1.0],
+        },
+        "cm5": {
+            "partial_charges": [1.0],
+        },
+    }
+    return chargemol_stats
+
+
+@pytest.fixture(autouse=True)
+def patch_pop_analyses(monkeypatch):
+    # Monkeypatch the Bader and Chargemol analyses so they don't run via pytest
+    monkeypatch.setattr(
+        "quacc.util.pop_analysis.bader_analysis_from_path",
+        mock_bader_analysis,
+    )
+    monkeypatch.setattr(
+        "quacc.util.pop_analysis.ChargemolAnalysis",
+        mock_chargemol_analysis,
+    )
+
+
+def setup_module():
+    for f in ["CHGCAR", "AECCAR0", "AECCAR2", "POTCAR"]:
+        with open(f, "w") as w:
+            w.write("test")
+
+
+def teardown_module():
+    for f in ["CHGCAR", "AECCAR0", "AECCAR2", "POTCAR"]:
+        if os.path.isfile(f):
+            os.remove(f)
+
+
+def test_run_bader():
+    bader_stats = run_bader()
+    assert bader_stats["min_dist"] == [1.0]
+    assert bader_stats["partial_charges"] == [1.0]
+    assert bader_stats["spin_moments"] == [0.0]
+    assert bader_stats.get("charge") is None
+    assert bader_stats.get("charge_transfer") is None
+    assert bader_stats.get("reference_used") is None
+    assert bader_stats.get("magmom") is None
+
+
+def test_bader_erorr():
+    os.remove("CHGCAR")
+    with pytest.raises(FileNotFoundError):
+        run_bader()
