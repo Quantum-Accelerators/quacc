4e64eed17a3874ba9a56155baf8e266b8ce046d9
Andrew <asrosen93@gmail.com>
2022-02-08 16:45:30 -0800

    Fix cclib version

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
             "atomate2==0.0.6",
             "monty==2022.1.19",
             "FireWorks==2.0.2",
-            "cclib==1.7.2",
+            "cclib==1.7.1",
         ],
         extras_require={
             "tests": ["pytest==7.0.0"],
