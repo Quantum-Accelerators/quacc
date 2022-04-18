cbef843caeb73818e9b6e0bf7d58ef022e7fb5a8
Andrew <asrosen93@gmail.com>
2022-04-17 21:41:11 -0700

    More xTB tests

--- tests/recipes/xtb/test_xtb_recipes.py
+++ tests/recipes/xtb/test_xtb_recipes.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 import pytest
 from ase.build import bulk, molecule
 from jobflow.managers.local import run_locally
@@ -32,24 +33,28 @@ def test_static_Job():
     assert output["parameters"]["method"] == "GFN2-xTB"
     assert output["name"] == "xTB-Static"
     assert output["results"]["energy"] == pytest.approx(-137.96777587302995)
+    assert np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
     job = StaticJob(method="GFN1-xTB").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
     assert output["parameters"]["method"] == "GFN1-xTB"
     assert output["results"]["energy"] == pytest.approx(-156.96750578831137)
+    assert np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
     job = StaticJob(method="GFN-FF").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
     assert output["parameters"]["method"] == "GFN-FF"
     assert output["results"]["energy"] == pytest.approx(-8.912667188932252)
+    assert np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
     atoms = bulk("Cu")
     job = StaticJob(method="GFN1-xTB").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
     assert output["results"]["energy"] == pytest.approx(-119.77643232313169)
+    assert np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
 
 @pytest.mark.skipif(
@@ -66,21 +71,28 @@ def test_relax_Job():
     assert output["parameters"]["method"] == "GFN2-xTB"
     assert output["name"] == "xTB-Relax"
     assert output["results"]["energy"] == pytest.approx(-137.9764670127011)
+    assert not np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
     job = RelaxJob(method="GFN1-xTB").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
     assert output["parameters"]["method"] == "GFN1-xTB"
     assert output["results"]["energy"] == pytest.approx(-156.9763496338962)
+    assert not np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
     job = RelaxJob(method="GFN-FF").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
     assert output["parameters"]["method"] == "GFN-FF"
     assert output["results"]["energy"] == pytest.approx(-8.915974748299963)
+    assert not np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
 
-    atoms = bulk("Cu")
+    atoms = molecule("H2O")
+    atoms.center(vacuum=5)
+    atoms.pbc = True
+    atoms[0].position += 0.01
     job = RelaxJob(method="GFN1-xTB").make(atoms)
     responses = run_locally(job, ensure_success=True)
     output = responses[job.uuid][1].output
-    assert output["results"]["energy"] == pytest.approx(-119.77643232313169)
+    assert output["results"]["energy"] == pytest.approx(-156.97441169886613)
+    assert not np.array_equal(output["atoms"].get_positions(), atoms.get_positions())
