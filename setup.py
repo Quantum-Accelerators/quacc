3c34ec9ce9d81fded2f8fc6efb9c634753b8c9ca
Andrew S. Rosen <asrosen93@gmail.com>
2022-04-17 20:33:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         author="Andrew S. Rosen",
         author_email="asrosen93@gmail.com",
         url="https://github.com/arosen93/quacc",
-        python_requires=">=3.10.0",
+        python_requires=">=3.8.0",
         version="0.0.3",
         packages=find_packages(),
         license="modified BSD",
