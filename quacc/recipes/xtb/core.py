231c47751bf0e23596cbaeef24dda27ccda6a74c
Andrew S. Rosen <asrosen93@gmail.com>
2022-07-01 13:47:24 -0700

    Update name for thermojob

--- quacc/recipes/xtb/core.py
+++ quacc/recipes/xtb/core.py
@@ -147,7 +147,7 @@ class ThermoJob(Maker):
         Dictionary of custom kwargs for the xTB calculator.
     """
 
-    name: str = "xTB-Relax"
+    name: str = "xTB-freq"
     method: str = "GFN2-xTB"
     temperature: float = 298.15
     pressure: float = 1.0
