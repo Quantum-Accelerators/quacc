b5ee13bbd5e7b51322e0df93c4f6e4755f58f3d4
Andrew <asrosen93@gmail.com>
2023-04-06 17:39:10 -0700

    Add missing conftest

---
+++ tests/recipes/gulp/conftest.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pytest
+from ase.atoms import Atoms
+from ase.calculators.gulp import GULP
+
+
+def mock_get_potential_energy(self, **kwargs):
+    # Instead of running .get_potential_energy(), we mock it by attaching
+    # dummy results to the atoms object and returning a fake energy. This
+    # works because the real atoms.get_potential_energy() takes one argument
+    # (i.e. self) and that self object is the atoms object.
+    e = -1.0
+    self.calc.results = {
+        "energy": e,
+        "forces": np.array([[0.0, 0.0, 0.0]] * len(self)),
+    }
+    return e
+
+
+@pytest.fixture(autouse=True)
+def patch_get_potential_energy(monkeypatch):
+    # Monkeypatch the .get_potential_energy() method of the .Atoms object so
+    # we aren't running the actual calculation during testing.
+    monkeypatch.setattr(Atoms, "get_potential_energy", mock_get_potential_energy)
+
+def mock_get_opt_state(self, **kwargs):
+    # Instead of running GULP and getting the opt state, we'll just mock
+    # it as true
+
+    self.get_opt_state = True
+
+    return True
+
+@pytest.fixture(autouse=True)
+def patch_get_opt_state(monkeypatch):
+    # Monkeypatch the .get_opt_state() method of the GULP calculator object so
+    # we aren't fetching the actual state
+    monkeypatch.setattr(GULP, "get_opt_state", mock_get_opt_state)
--- tests/recipes/gulp/test_gulp_recipes.py
+++ tests/recipes/gulp/test_gulp_recipes.py
@@ -2,8 +2,6 @@ import os
 from pathlib import Path
 from shutil import copy, rmtree
 
-import numpy as np
-import pytest
 from ase.build import bulk, molecule
 from jobflow.managers.local import run_locally
 
