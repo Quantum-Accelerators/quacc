97e043f6d72307ff4e3c37bc38b6311fe1884a38
Andrew <asrosen93@gmail.com>
2023-04-08 23:20:57 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ if __name__ == "__main__":
         zip_safe=False,
         install_requires=[
             "ase @ git+https://gitlab.com/ase/ase.git",
-            "atomate2 @ git+https://github.com/arosen93/atomate2.git@molecule",
+            "atomate2 @ git+https://github.com/arosen93/atomate2.git@cclib2emmet",
             "pymatgen==2023.3.23",
             "jobflow==0.1.11",
-            "emmet-core==0.51.11",
+            "@emmet-core git+https://github.com/arosen93/emmet.git@cclib",
             "cclib==1.7.2",
             "custodian==2023.3.10",
             "fireworks==2.0.3",
