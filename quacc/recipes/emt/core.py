90ec777e1e7325eaa2df7cfad1058b03881a4452
Andrew <asrosen93@gmail.com>
2023-04-20 23:06:25 -0700

    Start use of covalent recipes

--- quacc/recipes/emt/core.py
+++ quacc/recipes/emt/core.py
@@ -2,104 +2,77 @@
 from __future__ import annotations
 
 from copy import deepcopy
-from dataclasses import dataclass, field
 from typing import Any, Dict
 
 from ase.atoms import Atoms
 from ase.calculators.emt import EMT
 from ase.io import read
 from ase.optimize import FIRE
-from jobflow import Maker, job
+import covalent as ct
 
 from quacc.schemas.calc import summarize_opt_run, summarize_run
 
 # NOTE: This set of minimal recipes is mainly for demonstration purposes
 
 
-@dataclass
-class StaticJob(Maker):
+@ct.electron
+def StaticJob(atoms: Atoms, asap_cutoff: bool = False) -> Dict[str, Any]:
     """
-    Class to carry out a single-point calculation.
+    Function to carry out a static calculation.
 
     Parameters
     ----------
-    name
-        Name of the job.
+    atoms
+        .Atoms object
     asap_cutoff
         If an ASAP-style cutoff should be used.
+
+    Returns
+    -------
+    Dict
+        Summary of the run.
     """
+    input_atoms = deepcopy(atoms)
+    atoms.calc = EMT(asap_cutoff=asap_cutoff)
+    atoms.get_potential_energy()
+    summary = summarize_run(atoms, input_atoms=input_atoms)
 
-    name: str = "EMT-Static"
-    asap_cutoff: bool = False
-
-    @job
-    def make(self, atoms: Atoms) -> Dict[str, Any]:
-        """
-        Make the run.
-
-        Parameters
-        ----------
-        atoms
-            .Atoms object`
-
-        Returns
-        -------
-        Dict
-            Summary of the run.
-        """
-        input_atoms = deepcopy(atoms)
-        atoms.calc = EMT(asap_cutoff=self.asap_cutoff)
-        atoms.get_potential_energy()
-        summary = summarize_run(
-            atoms, input_atoms=input_atoms, additional_fields={"name": self.name}
-        )
-
-        return summary
+    return summary
 
 
-@dataclass
-class RelaxJob(Maker):
+@ct.electron
+def RelaxJob(
+    atoms: Atoms,
+    asap_cutoff: bool = False,
+    fmax: float = 0.03,
+    opt_kwargs: Dict[str, Any] | None = None,
+) -> Dict[str, Any]:
     """
-    Class to carry out a geometry optimization.
+    Function to carry out a geometry optimization.
 
     Parameters
     ----------
-    name
-        Name of the job.
+    atoms
+        .Atoms object
     asap_cutoff
         If an ASAP-style cutoff should be used.
     fmax
         Tolerance for the force convergence (in eV/A).
     opt_kwargs
         Dictionary of kwargs for the optimizer.
+
+    Returns
+    -------
+    Dict
+        Summary of the run.
     """
+    if opt_kwargs is None:
+        opt_kwargs = {}
 
-    name: str = "EMT-Relax"
-    asap_cutoff: bool = False
-    fmax: float = 0.03
-    opt_kwargs: Dict[str, Any] = field(default_factory=dict)
+    atoms.calc = EMT(asap_cutoff=asap_cutoff)
+    dyn = FIRE(atoms, logfile="opt.log", trajectory="opt.traj", **opt_kwargs)
+    dyn.run(fmax=fmax)
+    traj = read("opt.traj", index=":")
+    summary = summarize_opt_run(traj, atoms.calc.parameters)
 
-    @job
-    def make(self, atoms: Atoms) -> Dict[str, Any]:
-        """
-        Make the run.
-
-        Parameters
-        ----------
-        atoms
-            .Atoms object
-
-        Returns
-        -------
-        Dict
-            Summary of the run.
-        """
-        atoms.calc = EMT(asap_cutoff=self.asap_cutoff)
-        dyn = FIRE(atoms, logfile="opt.log", trajectory="opt.traj", **self.opt_kwargs)
-        dyn.run(fmax=self.fmax)
-        traj = read("opt.traj", index=":")
-        summary = summarize_opt_run(
-            traj, atoms.calc.parameters, additional_fields={"name": self.name}
-        )
-
-        return summary
+    return summary
