d7d3d402fb8f879a4d755a917f50c000745b1b2d
Andrew <asrosen93@gmail.com>
2023-04-21 00:26:58 -0700

    Gaussian

--- quacc/recipes/gaussian/core.py
+++ quacc/recipes/gaussian/core.py
@@ -2,12 +2,11 @@
 from __future__ import annotations
 
 import multiprocessing
-from dataclasses import dataclass, field
-from typing import Any, Dict
 
+from typing import Any, Dict
+import covalent as ct
 from ase.atoms import Atoms
 from ase.calculators.gaussian import Gaussian
-from jobflow import Maker, job
 
 from quacc.schemas.cclib import summarize_run
 from quacc.util.basics import merge_dicts
@@ -17,15 +16,30 @@ LOG_FILE = Gaussian().label + ".log"
 GEOM_FILE = LOG_FILE
 
 
-@dataclass
-class StaticJob(Maker):
+@ct.electron
+def StaticJob(
+    atoms: Atoms,
+    charge: int = None,
+    mult: int = None,
+    xc: str = "wb97x-d",
+    basis: str = "def2-tzvp",
+    pop: str = "hirshfeld",
+    molden: bool = True,
+    swaps: Dict[str, Any] | None = None,
+) -> Dict[str, Any]:
     """
-    Class to carry out a single-point calculation.
+    Function to carry out a single-point calculation.
 
     Parameters
     ----------
-    name
-        Name of the job.
+    atoms
+        .Atoms object
+    charge
+        Charge of the system. If None, this is determined from the sum of
+        atoms.get_initial_charges().
+    mult
+        Multiplicity of the system. If None, this is determined from 1+ the sum
+        of atoms.get_initial_magnetic_moments().
     xc
         Exchange-correlation functional
     basis
@@ -36,72 +50,63 @@ class StaticJob(Maker):
         Whether to write a molden file for orbital visualization
     swaps
         Dictionary of custom kwargs for the calculator.
+
+    Returns
+    -------
+    summary
+        Dictionary of results from the calculation.
     """
 
-    name: str = "Gaussian-Static"
-    xc: str = "wb97x-d"
-    basis: str = "def2-tzvp"
-    pop: str = "hirshfeld"
-    molden: bool = True
-    swaps: Dict[str, Any] = field(default_factory=dict)
+    swaps = swaps or {}
 
-    @job
-    def make(
-        self, atoms: Atoms, charge: int = None, mult: int = None
-    ) -> Dict[str, Any]:
-        """
-        Make the run.
+    defaults = {
+        "mem": "16GB",
+        "chk": "Gaussian.chk",
+        "nprocshared": multiprocessing.cpu_count(),
+        "xc": xc,
+        "basis": basis,
+        "charge": charge,
+        "mult": mult,
+        "sp": "",
+        "scf": ["maxcycle=250", "xqc"],
+        "integral": "ultrafine",
+        "nosymmetry": "",
+        "pop": pop,
+        "gfinput": "" if molden else None,
+        "ioplist": ["6/7=3"] if molden else None,
+    }
+    flags = merge_dicts(defaults, swaps, remove_none=True)
 
-        Parameters
-        ----------
-        atoms
-            .Atoms object
-        charge
-            Charge of the system. If None, this is determined from the sum of
-            atoms.get_initial_charges().
-        mult
-            Multiplicity of the system. If None, this is determined from 1+ the sum
-            of atoms.get_initial_magnetic_moments().
+    atoms.calc = Gaussian(**flags)
+    atoms = run_calc(atoms, geom_file=GEOM_FILE)
+    summary = summarize_run(atoms, LOG_FILE)
 
-        Returns
-        -------
-        Dict
-            Summary of the run.
-        """
-        defaults = {
-            "mem": "16GB",
-            "chk": "Gaussian.chk",
-            "nprocshared": multiprocessing.cpu_count(),
-            "xc": self.xc,
-            "basis": self.basis,
-            "charge": charge,
-            "mult": mult,
-            "sp": "",
-            "scf": ["maxcycle=250", "xqc"],
-            "integral": "ultrafine",
-            "nosymmetry": "",
-            "pop": self.pop,
-            "gfinput": "" if self.molden else None,
-            "ioplist": ["6/7=3"] if self.molden else None,
-        }
-        flags = merge_dicts(defaults, self.swaps, remove_none=True)
-
-        atoms.calc = Gaussian(**flags)
-        atoms = run_calc(atoms, geom_file=GEOM_FILE)
-        summary = summarize_run(atoms, LOG_FILE, additional_fields={"name": self.name})
-
-        return summary
+    return summary
 
 
-@dataclass
-class RelaxJob(Maker):
+@ct.electron
+def RelaxJob(
+    atoms: Atoms,
+    charge: int = None,
+    mult: int = None,
+    xc: str = "wb97x-d",
+    basis: str = "def2-tzvp",
+    freq: bool = False,
+    swaps: Dict[str, Any] | None = None,
+) -> Dict[str, Any]:
     """
-    Class to carry out a geometry optimization.
+    Function to carry out a geometry optimization.
 
     Parameters
     ----------
-    name
-        Name of the job.
+    atoms
+        .Atoms object
+    charge
+        Charge of the system. If None, this is determined from the sum of
+        atoms.get_initial_charges().
+    mult
+        Multiplicity of the system. If None, this is determined from 1+ the sum
+        of atoms.get_initial_magnetic_moments().
     xc
         Exchange-correlation functional
     basis
@@ -110,55 +115,33 @@ class RelaxJob(Maker):
         If a requency calculation should be carried out.
     swaps
         Dictionary of custom kwargs for the calculator.
+
+    Returns
+    -------
+    summary
+        Dictionary of results from the calculation.
     """
 
-    name: str = "Gaussian-Relax"
-    xc: str = "wb97x-d"
-    basis: str = "def2-tzvp"
-    freq: bool = False
-    swaps: Dict[str, Any] = field(default_factory=dict)
+    swaps = swaps or {}
 
-    @job
-    def make(
-        self, atoms: Atoms, charge: int = None, mult: int = None
-    ) -> Dict[str, Any]:
-        """
-        Make the run.
+    defaults = {
+        "mem": "16GB",
+        "chk": "Gaussian.chk",
+        "nprocshared": multiprocessing.cpu_count(),
+        "xc": xc,
+        "basis": basis,
+        "charge": charge,
+        "mult": mult,
+        "opt": "",
+        "scf": ["maxcycle=250", "xqc"],
+        "integral": "ultrafine",
+        "nosymmetry": "",
+        "freq": "" if freq else None,
+    }
+    flags = merge_dicts(defaults, swaps, remove_none=True)
 
-        Parameters
-        ----------
-        atoms
-            .Atoms object
-        charge
-            Charge of the system. If None, this is determined from the sum of
-            atoms.get_initial_charges().
-        mult
-            Multiplicity of the system. If None, this is determined from 1+ the sum
-            of atoms.get_initial_magnetic_moments().
+    atoms.calc = Gaussian(**flags)
+    atoms = run_calc(atoms, geom_file=GEOM_FILE)
+    summary = summarize_run(atoms, LOG_FILE)
 
-        Returns
-        -------
-        Dict
-            Summary of the run.
-        """
-        defaults = {
-            "mem": "16GB",
-            "chk": "Gaussian.chk",
-            "nprocshared": multiprocessing.cpu_count(),
-            "xc": self.xc,
-            "basis": self.basis,
-            "charge": charge,
-            "mult": mult,
-            "opt": "",
-            "scf": ["maxcycle=250", "xqc"],
-            "integral": "ultrafine",
-            "nosymmetry": "",
-            "freq": "" if self.freq else None,
-        }
-        flags = merge_dicts(defaults, self.swaps, remove_none=True)
-
-        atoms.calc = Gaussian(**flags)
-        atoms = run_calc(atoms, geom_file=GEOM_FILE)
-        summary = summarize_run(atoms, LOG_FILE, additional_fields={"name": self.name})
-
-        return summary
+    return summary
--- tests/recipes/gaussian/test_gaussian_recipes.py
+++ tests/recipes/gaussian/test_gaussian_recipes.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from shutil import copy, rmtree
 
 from ase.build import molecule
-from jobflow.managers.local import run_locally
-
+import covalent as ct
 from quacc.recipes.gaussian.core import RelaxJob, StaticJob
 
 FILE_DIR = Path(__file__).resolve().parent
@@ -31,11 +30,8 @@ def teardown_module():
 def test_static_Job():
     atoms = molecule("H2")
 
-    job = StaticJob().make(atoms)
-    responses = run_locally(job, ensure_success=True)
-    output = responses[job.uuid][1].output
+    output = StaticJob(atoms)
     assert output["natoms"] == len(atoms)
-    assert output["name"] == "Gaussian-Static"
     assert "charge" not in output["parameters"]
     assert "mult" not in output["parameters"]
     assert output["parameters"]["sp"] == ""
@@ -45,19 +41,19 @@ def test_static_Job():
     assert output["parameters"]["gfinput"] == ""
     assert output["parameters"]["ioplist"] == ["6/7=3"]
 
-    job = StaticJob(
+    output = StaticJob(
+        atoms,
+        charge=-2,
+        mult=3,
         xc="m06l",
         basis="def2-svp",
         pop="regular",
         molden=False,
         swaps={"integral": "superfinegrid"},
-    ).make(atoms, charge=-2, mult=3)
-    responses = run_locally(job, ensure_success=True)
-    output = responses[job.uuid][1].output
+    )
     assert output["natoms"] == len(atoms)
     assert output["parameters"]["charge"] == -2
     assert output["parameters"]["mult"] == 3
-    assert output["name"] == "Gaussian-Static"
     assert output["parameters"]["sp"] == ""
     assert output["parameters"]["xc"] == "m06l"
     assert output["parameters"]["basis"] == "def2-svp"
@@ -70,11 +66,8 @@ def test_static_Job():
 def test_relax_Job():
     atoms = molecule("H2")
 
-    job = RelaxJob().make(atoms)
-    responses = run_locally(job, ensure_success=True)
-    output = responses[job.uuid][1].output
+    output = RelaxJob(atoms)
     assert output["natoms"] == len(atoms)
-    assert output["name"] == "Gaussian-Relax"
     assert "charge" not in output["parameters"]
     assert "mult" not in output["parameters"]
     assert output["parameters"]["opt"] == ""
@@ -84,15 +77,18 @@ def test_relax_Job():
     assert "freq" not in output["parameters"]
     assert "sp" not in output["parameters"]
 
-    job = RelaxJob(
-        xc="m06l", basis="def2-svp", freq=True, swaps={"integral": "superfinegrid"}
-    ).make(atoms, charge=-2, mult=3)
-    responses = run_locally(job, ensure_success=True)
-    output = responses[job.uuid][1].output
+    output = RelaxJob(
+        atoms,
+        charge=-2,
+        mult=3,
+        xc="m06l",
+        basis="def2-svp",
+        freq=True,
+        swaps={"integral": "superfinegrid"},
+    )
     assert output["natoms"] == len(atoms)
     assert output["parameters"]["charge"] == -2
     assert output["parameters"]["mult"] == 3
-    assert output["name"] == "Gaussian-Relax"
     assert output["parameters"]["opt"] == ""
     assert output["parameters"]["freq"] == ""
     assert output["parameters"]["xc"] == "m06l"
