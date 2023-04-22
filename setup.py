0206fa8b2dac91667ab713884c77b9d149acea3e
Andrew <asrosen93@gmail.com>
2023-04-21 18:45:28 -0700

    Fix install

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
         ],
         extras_require={
             "covalent": ["covalent==0.209.1", "covalent-slurm-plugin==0.8.0"],
-            "fireworks": ["fireworks==2.0.2", "jobflow==0.1.11"],
+            "jobflow": ["fireworks==2.0.2", "jobflow==0.1.11"],
             "vasp": ["custodian==2023.3.10"],
             "xtb": ["tblite[ase]==0.3.0", "xtb==22.1"],
             "docs": [
