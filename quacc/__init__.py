901b9034c821566ac410b1205e1a233af128f95e
Andrew S. Rosen <asrosen93@gmail.com>
2023-05-06 18:02:52 -0700

    Update __init__.py

--- quacc/__init__.py
+++ quacc/__init__.py
@@ -3,7 +3,7 @@ from ase.atoms import Atoms
 from ase.io.jsonio import decode, encode
 
 from quacc.settings import QuaccSettings
-
+from quacc._version import __version__
 
 def atoms_as_dict(self):
     # Uses Monty's MSONable spec
