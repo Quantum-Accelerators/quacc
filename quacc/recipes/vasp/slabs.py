e6e157023665f20a12e2c5b63d4f40474dc02ba4
Andrew Rosen <asrosen93@gmail.com>
2023-05-11 19:42:32 -0700

    Fix function

--- quacc/recipes/vasp/slabs.py
+++ quacc/recipes/vasp/slabs.py
@@ -3,23 +3,17 @@ from __future__ import annotations
 
 import warnings
 from dataclasses import dataclass
-from typing import Any, List
+from typing import Any
 
 import covalent as ct
 from ase.atoms import Atoms
 from covalent._workflow.electron import Electron
 
 from quacc.calculators.vasp import Vasp
-from quacc.recipes.vasp.core import relax_job, static_job
 from quacc.schemas.vasp import summarize_run
 from quacc.util.basics import merge_dicts
 from quacc.util.calc import run_calc
-from quacc.util.slabs import (
-    get_surface_energy,
-    make_adsorbate_structures,
-    make_max_slabs_from_bulk,
-    slab_to_adsorbates,
-)
+from quacc.util.slabs import make_max_slabs_from_bulk, slab_to_adsorbates
 
 
 def slab_static_job(
@@ -217,7 +211,7 @@ class SlabToAdsFlow:
                 for slab in slabs
             ]
 
-        ads_slabs = make_adsorbate_structures(slab, adsorbate, **make_ads_kwargs)
+        ads_slabs = slab_to_adsorbates(slab, adsorbate, **make_ads_kwargs)
 
         if self.relax_electron and self.static_electron:
             return _relax_and_static_distributed(ads_slabs)
@@ -225,77 +219,3 @@ class SlabToAdsFlow:
             return _relax_distributed(ads_slabs)
         else:
             return ads_slabs
-
-
-def _get_slab_stability(
-    bulk_summary: dict[str, Any],
-    slab_summaries: dict[str, Any],
-    n_stable_slabs: int = 1,
-) -> dict[str, Any]:
-    """
-    A function that determine the most stable surface slabs (based on surface energy) for
-    a given bulk summary and list of slab summaries.
-
-    Parameters
-    ----------
-    bulk_summary
-        Output of a VASP job corresponding to the bulk structure.
-    slab_summaries
-        list of outputs of VASP jobs corresponding to the slab structures.
-    n_stable_slabs
-        The n most stable slabs are returned.
-
-    Returns
-    -------
-    dict
-        VASP output summaries for the stable and unstable slabs formatted as
-        {
-            {"stable_slabs": {"all_atoms": [.Atoms, .Atoms, ...], "all_outputs": [...],
-            {"unstable_slabs": {"all_atoms": [.Atoms, .Atoms, ...], "all_outputs": [...]
-        }
-    """
-    bulk = bulk_summary["atoms"]
-    bulk_energy = bulk_summary["output"]["energy"]
-    surface_energies = []
-
-    if n_stable_slabs > len(slab_summaries):
-        warnings.warn(
-            "n_stable_slabs is larger than the number of slabs. Setting n_stable_slabs to the number of slabs."
-        )
-        n_stable_slabs = len(slab_summaries)
-
-    # Iterate through each slab summary and determine the most stable slab
-    for slab_summary in slab_summaries:
-        slab = slab_summary["atoms"]
-        slab_energy = slab_summary["output"]["energy"]
-
-        # Calculate the surface energy
-        surface_energy = get_surface_energy(bulk, slab, bulk_energy, slab_energy)
-
-        # Insert the surface energy into the slab summary
-        slab_summary["avg_surface_energy"] = surface_energy
-
-        # Store the slab energy in a convenient dict
-        surface_energies.append(surface_energy)
-
-    slab_summaries_sorted = [
-        slab_summary
-        for _, slab_summary in sorted(zip(surface_energies, slab_summaries))
-    ]
-    stable_slab_summaries = slab_summaries_sorted[0:n_stable_slabs]
-    unstable_slab_summaries = slab_summaries_sorted[n_stable_slabs:]
-
-    output = {
-        "stable_slabs": {
-            "all_atoms": [summary["atoms"] for summary in stable_slab_summaries],
-            "all_outputs": stable_slab_summaries,
-        },
-        "unstable_slabs": {
-            "all_atoms": [summary["atoms"] for summary in unstable_slab_summaries]
-            if unstable_slab_summaries
-            else [],
-            "all_outputs": unstable_slab_summaries,
-        },
-    }
-
-    return output
