fb0b7f0ccf484547f105a90535bd0ca771adacf3
Andrew Rosen <asrosen93@gmail.com>
2023-05-13 23:04:11 -0700

    Update unit tests

--- tests/covalent/test_covalent.py
+++ tests/covalent/test_covalent.py
@@ -1,5 +1,9 @@
+import os
+
 import pytest
-import quacc
+from ase.build import bulk
+from quacc.recipes.emt.core import relax_job, static_job
+from quacc.recipes.emt.slabs import BulkToSlabsFlow
 
 try:
     import covalent as ct
@@ -13,10 +17,21 @@ except ImportError:
 )
 def test_covalent_config():
     ct_config = ct.get_config()
-    for executor in ct_config["executors"]:
-        assert (
-            ct_config["executors"][executor].get("create_unique_workdir", "false")
-            == "true"
-        )
     if ct_config["executors"].get("slurm", None):
         assert ct_config["executors"]["slurm"].get("use_srun", "true") == "false"
+        assert (
+            ct_config["executors"]["slurm"].get("create_unique_workdir", "false")
+            == "true"
+        )
+
+
+@pytest.mark.skipif(
+    ct is None,
+    reason="covalent must be installed. Try pip install covalent",
+)
+def test_emt():
+    atoms = bulk("Cu")
+    workflow = ct.lattice(ct.electron(static_job))
+    dispatch_id = ct.dispatch(workflow)(atoms)
+    result = ct.get_result(dispatch_id, wait=True)
+    assert result.status == "COMPLETED"
