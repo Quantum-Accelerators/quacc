7898fbc00677de7291ba793c38535d937af88591
Andrew Rosen <asrosen93@gmail.com>
2023-05-13 21:07:36 -0700

    black

--- tests/covalent/test_covalent.py
+++ tests/covalent/test_covalent.py
@@ -6,14 +6,17 @@ try:
 except ImportError:
     ct = None
 
+
 @pytest.mark.skipif(
     ct is None,
     reason="covalent must be installed. Try pip install covalent",
 )
-
 def test_covalent_config():
     ct_config = ct.get_config()
     for executor in ct_config["executors"]:
-        assert ct_config["executors"][executor].get("create_unique_workdir", "false")  == "true"
+        assert (
+            ct_config["executors"][executor].get("create_unique_workdir", "false")
+            == "true"
+        )
     if ct_config["executors"].get("slurm", None):
         assert ct_config["executors"]["slurm"].get("use_srun", "true") == "false"
