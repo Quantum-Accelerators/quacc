331f68c93476c7c475454227461bf9be86784660
Andrew Rosen <asrosen93@gmail.com>
2023-05-14 12:23:37 -0700

    update tests

--- tests/covalent/test_covalent.py
+++ tests/covalent/test_covalent.py
@@ -33,6 +33,11 @@ def teardown_module():
 )
 def test_covalent_config():
     ct_config = ct.get_config()
+    for executor in ["dask", "local"]:
+        assert (
+            ct_config["executors"][executor].get("create_unique_workdir", "false")
+            == "true"
+        )
     if ct_config["executors"].get("slurm", None):
         assert ct_config["executors"]["slurm"].get("use_srun", "true") == "false"
         assert (
