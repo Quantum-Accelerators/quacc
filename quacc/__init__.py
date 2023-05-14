e628369d18fe9c77f4f57a23fad227772c33a5a8
Andrew Rosen <asrosen93@gmail.com>
2023-05-14 13:43:28 -0700

    Update init

--- quacc/__init__.py
+++ quacc/__init__.py
@@ -40,6 +40,7 @@ Atoms.from_dict = atoms_from_dict
 SETTINGS = QuaccSettings()
 
 if ct:
+    ct_config = ct.get_config()
     # Ensure that the create_unique_workdir is set to True
     # These are the executors where we know this parameter exists
     for executor in ["dask", "local", "slurm"]:
@@ -56,6 +57,7 @@ if ct:
                 UserWarning,
             )
             ct.set_config({f"executors.{executor}.create_unique_workdir": True})
+            ct_config = ct.get_config()
 
     # Make sure that the create_unique_workdir is set to True for any executor
     # where this option exists, in case we missed any above
@@ -70,6 +72,7 @@ if ct:
                 UserWarning,
             )
             ct.set_config({f"executors.{executor}.create_unique_workdir": True})
+            ct_config = ct.get_config()
 
     # Ensure that use_srun is False in Slurm executor if the plugin is installed
     if "slurm" in ct.get_config()["executors"]:
@@ -79,3 +82,4 @@ if ct:
                 UserWarning,
             )
             ct.set_config({"executors.slurm.use_srun": False})
+            ct_config = ct.get_config()
