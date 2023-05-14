8d68736518f87b24dd7ac43e3b07756cd87bc14c
Andrew Rosen <asrosen93@gmail.com>
2023-05-13 21:07:01 -0700

    Add covalent tests

---
+++ tests/covalent/test_covalent.py
@@ -0,0 +1,19 @@
+import pytest
+import quacc
+
+try:
+    import covalent as ct
+except ImportError:
+    ct = None
+
+@pytest.mark.skipif(
+    ct is None,
+    reason="covalent must be installed. Try pip install covalent",
+)
+
+def test_covalent_config():
+    ct_config = ct.get_config()
+    for executor in ct_config["executors"]:
+        assert ct_config["executors"][executor].get("create_unique_workdir", "false")  == "true"
+    if ct_config["executors"].get("slurm", None):
+        assert ct_config["executors"]["slurm"].get("use_srun", "true") == "false"
