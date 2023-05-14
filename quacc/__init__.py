847ef12ea99d4c9e42c6a387076c316897dea78d
Andrew Rosen <asrosen93@gmail.com>
2023-05-14 13:42:41 -0700

    fix init

--- quacc/__init__.py
+++ quacc/__init__.py
@@ -40,13 +40,11 @@ Atoms.from_dict = atoms_from_dict
 SETTINGS = QuaccSettings()
 
 if ct:
-    ct_config = ct.get_config()
-
     # Ensure that the create_unique_workdir is set to True
     # These are the executors where we know this parameter exists
     for executor in ["dask", "local", "slurm"]:
         try:
-            create_unique_workdir = ct_config["executors"][executor][
+            create_unique_workdir = ct.get_config()["executors"][executor][
                 "create_unique_workdir"
             ]
         except:
@@ -61,10 +59,11 @@ if ct:
 
     # Make sure that the create_unique_workdir is set to True for any executor
     # where this option exists, in case we missed any above
-    for executor in ct_config["executors"]:
+    for executor in ct.get_config()["executors"]:
         if (
-            "create_unique_workdir" in ct_config["executors"][executor]
-            and ct_config["executors"][executor]["create_unique_workdir"] is not True
+            "create_unique_workdir" in ct.get_config()["executors"][executor]
+            and ct.get_config()["executors"][executor]["create_unique_workdir"]
+            is not True
         ):
             warnings.warn(
                 f"Updating Covalent configuration... setting executors.{executor}.create_unique_workdir: True",
@@ -73,8 +72,8 @@ if ct:
             ct.set_config({f"executors.{executor}.create_unique_workdir": True})
 
     # Ensure that use_srun is False in Slurm executor if the plugin is installed
-    if "slurm" in ct_config["executors"]:
-        if ct_config["executors"]["slurm"].get("use_srun", True) is not False:
+    if "slurm" in ct.get_config()["executors"]:
+        if ct.get_config()["executors"]["slurm"].get("use_srun", True) is not False:
             warnings.warn(
                 "Updating Covalent configuration... setting executors.slurm.use_srun: False",
                 UserWarning,
