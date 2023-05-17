c8684443472ae73c6837e7a64853be2fa89166ed
Andrew Rosen <asrosen93@gmail.com>
2023-05-17 14:42:36 -0700

    push cclib update

--- setup.py
+++ setup.py
@@ -24,9 +24,10 @@ if __name__ == "__main__":
         data_files=["LICENSE.md"],
         zip_safe=False,
         install_requires=[
-            "ase @ git+https://gitlab.com/ase/ase.git",
-            "atomate2[cclib] @ git+https://github.com/materialsproject/atomate2.git",
-            "covalent @ git+https://github.com/arosen93/covalent.git@rosen-unique-workdir",
+            "ase @ git+https://gitlab.com/ase/ase.git",  # waiting on >3.22.1
+            "atomate2 @ git+https://github.com/materialsproject/atomate2.git",  # waiting on >0.0.10
+            "cclib @ git+https://github.com/cclib/cclib.git",  # waiting on >1.7.2
+            "covalent @ git+https://github.com/arosen93/covalent.git@rosen-unique-workdir",  # waiting on PR 1644
             "emmet-core>=0.55.1",
             "jobflow>=0.1.11",
             "maggma>=0.50.4",
@@ -51,7 +52,8 @@ if __name__ == "__main__":
             "dev": ["black==23.3.0", "isort==5.12.0", "pytest==7.3.1"],
             "strict": [
                 "ase @ git+https://gitlab.com/ase/ase.git",
-                "atomate2[cclib] @ git+https://github.com/materialsproject/atomate2.git",
+                "atomate2 @ git+https://github.com/materialsproject/atomate2.git",
+                "cclib @ git+https://github.com/cclib/cclib.git",
                 "covalent @ git+https://github.com/arosen93/covalent.git@rosen-unique-workdir",
                 # "covalent-slurm-plugin==0.16.0rc0",
                 "custodian==2023.5.7",
