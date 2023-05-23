2e0f2786e7b1cce2e311a5e799dcfdc1f54cd320
Andrew Rosen <asrosen93@gmail.com>
2023-05-23 10:54:50 -0700

    Better DB handling

--- quacc/util/db.py
+++ quacc/util/db.py
@@ -46,12 +46,8 @@ def covalent_to_db(store: Store, dispatch_id: str = None, results_dir: str = Non
     # Populate the docs
     docs = []
     for d_id in dispatch_ids:
-        try:
-            result_obj = ct.get_result(d_id)
-        except MissingLatticeRecordError:
-            warnings.warn(f"Could not find dispatch_id: {d_id}", UserWarning)
-            continue
-        if result_obj.status == "COMPLETED":
+        result_obj = ct.get_result(d_id)
+        if result_obj and result_obj.status == "COMPLETED":
             docs.append({"dispatch_id": d_id, "result": result_obj.result})
 
     # Store the results
