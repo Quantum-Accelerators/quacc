e01120c518a55d2cd6bee01ac46743d807d89588
Andrew Rosen <asrosen93@gmail.com>
2023-05-24 22:55:46 -0700

    Fix init

--- quacc/__init__.py
+++ quacc/__init__.py
@@ -27,27 +27,26 @@ Atoms.as_dict = atoms_as_dict
 Atoms.from_dict = atoms_from_dict
 SETTINGS = QuaccSettings()
 
-if ct:
-    ct_config = ct.get_config()
+ct_config = ct.get_config()
 
-    # Make sure that the create_unique_workdir is set to True for any plugin
-    # where this option exists
-    for executor in ct_config["executors"]:
-        if (
-            "create_unique_workdir" in ct_config["executors"][executor]
-            and ct_config["executors"][executor]["create_unique_workdir"] is not True
-        ):
-            print(
-                f"Configuring Covalent... Setting executors.{executor}.create_unique_workdir: True"
-            )
-            ct.set_config({f"executors.{executor}.create_unique_workdir": True})
-            ct_config = ct.get_config()
-
-    # Ensure that use_srun is False in Slurm executor if the plugin is installed
+# Make sure that the create_unique_workdir is set to True for any plugin
+# where this option exists
+for executor in ct_config["executors"]:
     if (
-        "slurm" in ct_config["executors"]
-        and ct_config["executors"]["slurm"].get("use_srun", True) is not False
+        "create_unique_workdir" in ct_config["executors"][executor]
+        and ct_config["executors"][executor]["create_unique_workdir"] is not True
     ):
-        print("Configuring Covalent... Setting executors.slurm.use_srun: False")
-        ct.set_config({"executors.slurm.use_srun": False})
+        print(
+            f"Configuring Covalent... Setting executors.{executor}.create_unique_workdir: True"
+        )
+        ct.set_config({f"executors.{executor}.create_unique_workdir": True})
         ct_config = ct.get_config()
+
+# Ensure that use_srun is False in Slurm executor if the plugin is installed
+if (
+    "slurm" in ct_config["executors"]
+    and ct_config["executors"]["slurm"].get("use_srun", True) is not False
+):
+    print("Configuring Covalent... Setting executors.slurm.use_srun: False")
+    ct.set_config({"executors.slurm.use_srun": False})
+    ct_config = ct.get_config()
