bcf6bbfa477c9958e6f1d66ccde650b0526a464f
Andrew Rosen <asrosen93@gmail.com>
2023-05-24 22:36:34 -0700

    Update tests

--- tests/util/test_thermo.py
+++ tests/util/test_thermo.py
@@ -6,9 +6,12 @@ from quacc.util.thermo import ideal_gas
 
 def test_ideal_gas():
     co2 = molecule("CO2")
-    igt = ideal_gas(co2, [526, 526, 1480, 2565], spin_multiplicity=3)
+    igt = ideal_gas(co2, [526, 526, 1480, 2565], spin_multiplicity=2)
+    assert igt.geometry == "linear"
+    assert igt.spin == 0.5
 
     co2 = molecule("CO2")
     co2.calc = EMT()
     co2.calc.results["magmom"] = 1.0
     igt = ideal_gas(co2, [526, 526, 1480, 2565])
+    assert igt.spin == 0.5
