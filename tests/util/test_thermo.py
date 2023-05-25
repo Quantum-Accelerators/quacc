d159810c33e48cada87b599af2cce738234c5277
Andrew Rosen <asrosen93@gmail.com>
2023-05-24 22:49:14 -0700

    add tests

--- tests/util/test_thermo.py
+++ tests/util/test_thermo.py
@@ -1,5 +1,7 @@
+import pytest
 from ase.build import molecule
 from ase.calculators.emt import EMT
+from ase.units import invcm
 
 from quacc.util.thermo import ideal_gas
 
@@ -15,3 +17,10 @@ def test_ideal_gas():
     co2.calc.results["magmom"] = 1.0
     igt = ideal_gas(co2, [526, 526, 1480, 2565])
     assert igt.spin == 0.5
+    assert igt.get_ZPE_correction() == pytest.approx(2548.5 * invcm)
+
+    co2 = molecule("CO2")
+    co2.calc = EMT()
+    co2.calc.results["magmom"] = 1.0
+    igt = ideal_gas(co2, [-12, 526, 526, 1480, 2565])
+    assert igt.get_ZPE_correction() == pytest.approx(2548.5 * invcm)
