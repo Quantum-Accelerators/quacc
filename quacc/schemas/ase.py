109e6d2d1a1196ce353fd3511ff5e8b9c83c1544
Andrew Rosen <asrosen93@gmail.com>
2023-05-26 10:49:50 -0700

    Update n_modes

--- quacc/schemas/ase.py
+++ quacc/schemas/ase.py
@@ -403,17 +403,16 @@ def summarize_vib_run(
     # Get the true vibrational modes if it's a molecule
     natoms = len(atoms)
     if natoms == 1:
-        true_vib_freqs = []
-        true_vib_energies = []
+        n_modes = 0
     elif atoms.pbc.any():
-        true_vib_freqs = vib_freqs[-(3 * natoms - 3) :]
-        true_vib_energies = vib_energies[-(3 * natoms - 3) :]
+        n_modes = 3 * natoms - 3
     elif atoms_db["symmetry"]["linear"]:
-        true_vib_freqs = vib_freqs[-(3 * natoms - 5) :]
-        true_vib_energies = vib_energies[-(3 * natoms - 5) :]
+        n_modes = 3 * natoms - 5
     else:
-        true_vib_freqs = vib_freqs[-(3 * natoms - 6) :]
-        true_vib_energies = vib_energies[-(3 * natoms - 6) :]
+        n_modes = 3 * natoms - 6
+
+    true_vib_freqs = vib_freqs[-n_modes:]
+    true_vib_energies = vib_energies[-n_modes:]
 
     results = {
         "results": {
