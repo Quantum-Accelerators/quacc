68aa1bfac753123051bc8b656cf324d9d7b1eb22
Andrew Rosen <asrosen93@gmail.com>
2023-06-03 11:01:08 -0700

    Add a requirements.txt

--- setup.py
+++ setup.py
@@ -33,19 +33,19 @@ if __name__ == "__main__":
         data_files=["LICENSE.md"],
         zip_safe=False,
         install_requires=[
-            "ase @ git+https://gitlab.com/ase/ase.git",  # waiting on >3.22.1
-            "atomate2[cclib] @ git+https://github.com/materialsproject/atomate2.git",  # waiting on >0.0.10
-            "covalent @ git+https://github.com/AgnostiqHQ/covalent.git",  # waiting on > 0.222.0
+            "ase",  # waiting on >3.22.1
+            "atomate2[cclib]",  # waiting on >0.0.10
+            "covalent",  # waiting on > 0.222.0
             "custodian>=2023.5.12",
             "emmet-core>=0.51.11",
             "pymatgen>=2023.5.10",
             "monty>=2023.4.10",
-            "numpy>=1.24.3",
+            "numpy",
         ],
         extras_require={
             "fireworks": ["fireworks>=2.0.3"],
             "tblite": ["tblite[ase]>=0.3.0"],
-            "dev": ["black>=23.3.0", "isort>=5.12.0", "pytest>=7.3.1"],
+            "dev": ["black==23.3.0", "isort==5.12.0", "pytest"],
             "docs": [
                 "autodoc_pydantic==1.8.0",
                 "furo==2023.5.20",
@@ -54,24 +54,6 @@ if __name__ == "__main__":
                 "myst_parser==1.0.0",
                 "numpydoc==1.5.0",
                 "sphinx_design==0.4.1",
-            ],
-            "strict": [
-                "ase @ git+https://gitlab.com/ase/ase.git",  # waiting on >3.22.1
-                "atomate2[cclib] @ git+https://github.com/materialsproject/atomate2.git",  # waiting on >0.0.10
-                "black==23.3.0",
-                "cclib==1.7.2",
-                "covalent @ git+https://github.com/AgnostiqHQ/covalent.git",  # waiting on > 0.222.0
-                "custodian==2023.5.12",
-                "emmet-core==0.55.2",
-                "fireworks==2.0.3",
-                "isort==5.12.0",
-                "jobflow==0.1.11",
-                "maggma==0.51.3",
-                "monty==2023.5.8",
-                "numpy==1.24.3",
-                "pydantic==1.10.2",
-                "pymatgen==2023.5.10",
-                "pytest==7.3.1",
             ],
         },
         tests_require=["pytest"],
