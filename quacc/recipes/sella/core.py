61696f7719998785da3501407045009415cae5fa
Andrew Rosen <asrosen93@gmail.com>
2023-06-10 12:37:21 -0700

    dev requires on sella

--- quacc/recipes/sella/core.py
+++ quacc/recipes/sella/core.py
@@ -10,14 +10,16 @@ import numpy as np
 from ase.atoms import Atoms
 from ase.io import read
 from monty.dev import requires
-from sella import Sella
 
+try:
+    from sella import Sella
+except ImportError:
+    Sella = None
 try:
     from newtonnet.utils.ase_interface import MLAseCalculator as NewtonNet
 except ImportError:
     NewtonNet = None
 
-# TODO: Make sure code doesn't crash if sella isn't installed
 # TODO: Don't hardcode fmax or steps
 # TODO: Add docstrings
 # TODO: Add all path-related variables to the global Quacc settings and remove from kwargs.
@@ -25,6 +27,7 @@ except ImportError:
 
 @ct.electron
 @requires(NewtonNet, "NewtonNet must be installed. Try pip install quacc[newtonnet]")
+@requires(Sella, "Sella must be installed. Try pip install quacc[sella]")
 def optimize_and_analyze(atoms: Atoms, ml_path: str, config_path: str) -> dict:
     mlcalculator = NewtonNet(model_path=ml_path, settings_path=config_path)
     atoms.calc = mlcalculator
