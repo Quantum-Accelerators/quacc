3498f348e297c4a80ebfc53178fb6cbe08a00412
Andrew S. Rosen <asrosen93@gmail.com>
2023-06-15 14:26:09 -0700

    Remove sorting

--- quacc/recipes/newtonnet/core.py
+++ quacc/recipes/newtonnet/core.py
@@ -388,11 +388,7 @@ def freq_job(
     '''
     #print('vib.get_frequencies():', vib.get_frequencies())
 
-    ## Sort the frequencies (can remove after ASE MR 2906 is merged)
-    #freqs_cm_inv = list(freqs_cm_inv)
-    freqs_cm_inv = list(vib.get_frequencies())
-    freqs_cm_inv.sort(key=np.imag, reverse=True)
-    freqs_cm_inv.sort(key=np.real)
+    freqs_cm_inv = list(freqs_cm_inv)
 
     ## Make IdealGasThermo object
     igt = ideal_gas(atoms, freqs_cm_inv, energy=mlcalculator.results["energy"])
