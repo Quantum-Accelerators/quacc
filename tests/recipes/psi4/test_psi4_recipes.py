128da70bd1744200a6e46df05dd2ea0d7329552d
Andrew Rosen <asrosen93@gmail.com>
2023-06-16 12:14:04 -0700

    switch to pbe default

--- tests/recipes/psi4/test_psi4_recipes.py
+++ tests/recipes/psi4/test_psi4_recipes.py
@@ -45,14 +45,14 @@ def test_static_maker():
         atoms,
         charge=-2,
         multiplicity=3,
-        method="m06l",
+        method="pbe",
         basis="def2-svp",
         swaps={"num_threads": 1, "mem": None, "pop": "regular"},
     )
     assert output["natoms"] == len(atoms)
     assert output["parameters"]["charge"] == -2
     assert output["parameters"]["multiplicity"] == 3
-    assert output["parameters"]["method"] == "m06l"
+    assert output["parameters"]["method"] == "pbe"
     assert output["parameters"]["basis"] == "def2-svp"
     assert output["parameters"]["num_threads"] == 1
     assert output["parameters"]["pop"] == "regular"
