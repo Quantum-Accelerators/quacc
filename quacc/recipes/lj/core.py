38ee6a6fde17602c87bffbd2f1d028d3cdc98ebd
Andrew Rosen <asrosen93@gmail.com>
2023-06-19 00:05:56 -0700

    Add minimal LJ recipe

--- quacc/recipes/lj/core.py
+++ quacc/recipes/lj/core.py
@@ -12,8 +12,14 @@ from ase.atoms import Atoms
 from ase.calculators.lj import LennardJones
 from ase.optimize import FIRE
 
-from quacc.schemas.ase import summarize_opt_run, summarize_run
-from quacc.util.calc import run_ase_opt, run_calc
+from quacc.schemas.ase import (
+    summarize_opt_run,
+    summarize_run,
+    summarize_thermo_run,
+    summarize_vib_run,
+)
+from quacc.util.calc import run_ase_opt, run_ase_vib, run_calc
+from quacc.util.thermo import ideal_gas
 
 
 @ct.electron
@@ -82,3 +88,58 @@ def relax_job(
     dyn = run_ase_opt(atoms, **opt_flags)
 
     return summarize_opt_run(dyn, additional_fields={"name": "LJ Relax"})
+
+
+@ct.electron
+def freq_job(
+    atoms: Atoms,
+    energy: float = 0.0,
+    temperature: float = 298.15,
+    pressure: float = 1.0,
+    calc_kwargs: dict | None = None,
+    vib_kwargs: dict | None = None,
+) -> dict:
+    """
+    Run a frequency job and calculate thermochemistry.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    energy
+        Potential energy in eV. If 0, then the output is just the correction.
+    temperature
+        Temperature in Kelvins.
+    pressure
+        Pressure in bar.
+    calc_kwargs
+        dictionary of custom kwargs for the LJ calculator.
+    vib_kwargs
+        dictionary of custom kwargs for the Vibrations object
+
+    Returns
+    -------
+    dict
+        Dictionary of results from quacc.schemas.ase.summarize_vib_run and
+        quacc.schemas.ase.summarize_thermo_run
+    """
+
+    calc_kwargs = calc_kwargs or {}
+    vib_kwargs = vib_kwargs or {}
+
+    atoms.calc = LennardJones(**calc_kwargs)
+    vibrations = run_ase_vib(atoms, vib_kwargs=vib_kwargs)
+
+    igt = ideal_gas(atoms, vibrations.get_frequencies(), energy=energy)
+
+    return {
+        "vib": summarize_vib_run(
+            vibrations, additional_fields={"name": "LJ Vibrations"}
+        ),
+        "thermo": summarize_thermo_run(
+            igt,
+            temperature=temperature,
+            pressure=pressure,
+            additional_fields={"name": "LJ Thermo"},
+        ),
+    }
