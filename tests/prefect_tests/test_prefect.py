1d73013a7c87e6f3a45882a6a4cdd336d22bc630
Andrew Rosen <asrosen93@gmail.com>
2023-06-29 01:01:51 -0700

    Refactor tests

---
+++ tests/prefect_tests/test_prefect.py
@@ -0,0 +1,68 @@
+import os
+from shutil import rmtree
+
+import pytest
+from ase.build import bulk, molecule
+
+try:
+    import prefect
+    from prefect import flow, task
+except ImportError:
+    prefect = None
+
+
+def teardown_module():
+    for f in os.listdir(os.getcwd()):
+        if (
+            f.endswith(".log")
+            or f.endswith(".pckl")
+            or f.endswith(".traj")
+            or f.endswith(".out")
+            or ".gz" in f
+        ):
+            os.remove(f)
+        if "quacc-tmp" in f or "job_" in f or f == "tmp_dir" or f == "runinfo":
+            rmtree(f)
+
+
+@pytest.mark.skipif(prefect is None, reason="Prefect is not installed")
+def test_tutorial1():
+    from ase.build import bulk
+
+    from quacc.recipes.emt.core import relax_job, static_job
+
+    # Define the workflow
+    @flow
+    def workflow(atoms):
+        # Call Task 1
+        result1 = task(relax_job)(atoms)
+
+        # Call Task 2, which takes the output of Task 1 as input
+        result2 = task(static_job)(result1["atoms"])
+
+        return result2
+
+    # Make an Atoms object of a bulk Cu structure
+    atoms = bulk("Cu")
+
+    # Run the workflow with Prefect tracking
+    workflow(atoms)
+
+
+@pytest.mark.skipif(prefect is None, reason="Prefect is not installed")
+def test_tutorial2():
+    # Define workflow
+    @flow
+    def workflow(atoms1, atoms2):
+        # Define two independent relaxation jobs
+        result1 = task(relax_job)(atoms1)
+        result2 = task(relax_job)(atoms2)
+
+        return {"result1": result1, "result2": result2}
+
+    # Define two Atoms objects
+    atoms1 = bulk("Cu")
+    atoms2 = molecule("N2")
+
+    # Run the workflow with Prefect tracking
+    workflow(atoms1, atoms2)
