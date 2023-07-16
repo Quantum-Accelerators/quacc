2efa07e9baede9cd2ffbb26a9676e5852f3ef3a9
Andrew Rosen <asrosen93@gmail.com>
2023-07-16 14:07:29 -0700

    fix test

--- tests/recipes/compiled/test_real_gulp_recipes.py
+++ tests/recipes/compiled/test_real_gulp_recipes.py
@@ -6,7 +6,7 @@ from ase.build import bulk, molecule
 
 from quacc.recipes.gulp.core import relax_job, static_job
 
-has_gulp = bool(which("gulp") or os.environ.get("ASE_GULP_COMMAND"))
+has_gulp = bool(os.environ.get("ASE_GULP_COMMAND"))
 
 
 @pytest.mark.skipif(has_gulp is False, reason="GULP not installed")
