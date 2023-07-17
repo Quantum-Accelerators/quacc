129eaef0fd3157e817be28a63664852965c2f700
Andrew Rosen <asrosen93@gmail.com>
2023-07-16 21:45:24 -0700

    fix

--- tests/util/test_sella.py
+++ tests/util/test_sella.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 from ase.build import bulk, molecule
 from ase.calculators.emt import EMT
@@ -24,26 +22,14 @@ def test_sella(tmpdir):
     atoms = bulk("Cu") * (2, 1, 1)
     atoms[0].position += 0.1
     atoms.calc = EMT()
-    dyn = run_ase_opt(
-        atoms,
-        optimizer=Sella,
-        scratch_dir="test_calc",
-        gzip=False,
-        optimizer_kwargs={"restart": None},
-    )
+    dyn = run_ase_opt(atoms, optimizer=Sella, optimizer_kwargs={"restart": None})
     traj = dyn.traj_atoms
     assert traj[-1].calc.results is not None
     assert dyn.user_internal is False
 
     atoms = molecule("H2O")
     atoms.calc = LennardJones()
-    dyn = run_ase_opt(
-        atoms,
-        optimizer=Sella,
-        scratch_dir="test_calc2",
-        gzip=False,
-        optimizer_kwargs={"restart": None},
-    )
+    dyn = run_ase_opt(atoms, optimizer=Sella, optimizer_kwargs={"restart": None})
     traj = dyn.traj_atoms
     assert traj[-1].calc.results is not None
     assert dyn.user_internal is True
