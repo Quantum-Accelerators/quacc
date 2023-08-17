dff7a9886954afd85b90abfe5207069115faebd5
Andrew Rosen <asrosen93@gmail.com>
2023-08-16 19:33:13 -0700

    remove unused param

--- src/quacc/settings.py
+++ src/quacc/settings.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 import os
 from shutil import which
-from typing import List, Literal, Optional, Union
+from typing import List, Literal, Optional
 
 from pydantic import BaseSettings, Field, root_validator
 
