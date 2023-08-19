018a6bffbff9b1099fef482db9cc3669b227a692
Andrew Rosen <asrosen93@gmail.com>
2023-08-18 17:19:23 -0700

    fix conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,12 +11,20 @@ DEFAULT_SETTINGS = SETTINGS.copy()
 test_results_dir = os.path.join(FILE_DIR, ".results_dir")
 test_scratch_dir = os.path.join(FILE_DIR, ".scratch_dir")
 
+WFLOW_ENGINE = SETTINGS.WORKFLOW_ENGINE.lower() if SETTINGS.WORKFLOW_ENGINE else None
+
 
 def pytest_sessionstart():
     os.makedirs(test_results_dir, exist_ok=True)
     os.makedirs(test_scratch_dir, exist_ok=True)
     SETTINGS.RESULTS_DIR = test_results_dir
     SETTINGS.SCRATCH_DIR = test_scratch_dir
+    if WFLOW_ENGINE == "covalent":
+        os.system("covalent start")
+    elif WFLOW_ENGINE == "parsl":
+        import parsl
+
+        parsl.load()
 
 
 def pytest_sessionfinish():
@@ -24,3 +32,5 @@ def pytest_sessionfinish():
     SETTINGS.SCRATCH_DIR = DEFAULT_SETTINGS.SCRATCH_DIR
     rmtree(test_results_dir)
     rmtree(test_scratch_dir)
+    if WFLOW_ENGINE == "covalent":
+        os.system("covalent stop")
