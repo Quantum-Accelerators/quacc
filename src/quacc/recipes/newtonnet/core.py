e373d73448d71ee6d61b242539a1a1c6aad89716
Andrew Rosen <asrosen93@gmail.com>
2023-08-23 18:44:05 -0700

    clean up newtonnet

--- src/quacc/recipes/newtonnet/core.py
+++ src/quacc/recipes/newtonnet/core.py
@@ -22,6 +22,12 @@ from quacc.schemas.atoms import fetch_atoms
 from quacc.util.calc import run_ase_opt, run_calc
 from quacc.util.thermo import ideal_gas
 
+if TYPE_CHECKING:
+    import numpy as np
+
+    from quacc.schemas.ase import OptSchema, RunSchema, ThermoSchema, VibSchema
+
+
 try:
     from sella import IRC, Sella
 except ImportError:
@@ -31,12 +37,6 @@ try:
     from newtonnet.utils.ase_interface import MLAseCalculator as NewtonNet
 except ImportError:
     NewtonNet = None
-
-
-if TYPE_CHECKING:
-    import numpy as np
-
-    from quacc.schemas.ase import OptSchema, RunSchema, ThermoSchema, VibSchema
 
 
 @job
@@ -149,9 +149,9 @@ def freq_job(
     atoms
         The atoms object representing the system.
     temperature
-        The temperature for the thermodynamic analysis (default: 298.15 K).
+        The temperature for the thermodynamic analysis.
     pressure
-        The pressure for the thermodynamic analysis (default: 1.0 atm).
+        The pressure for the thermodynamic analysis.
 
     Returns
     -------
@@ -211,13 +211,13 @@ def ts_job(
     use_custom_hessian
         Whether to use a custom Hessian matrix.
     temperature
-        The temperature for the frequency calculation (default: 298.15 K).
+        The temperature for the frequency calculation in Kelvins.
     pressure
-        The pressure for the frequency calculation (default: 1.0 atm).
+        The pressure for the frequency calculation in bar.
     check_convergence
-        Whether to check the convergence of the optimization (default: True).
+        Whether to check the convergence of the optimization.
     opt_swaps
-        Optional swaps for the optimization parameters (default: None).
+        Optional swaps for the optimization parameters.
 
     Returns
     -------
@@ -271,8 +271,10 @@ def ts_job(
     ts_summary = _add_stdev_and_hess(ts_summary)
 
     # Run a frequency calculation
-    thermo_summary = freq_job.original_func(
-        ts_summary, temperature=temperature, pressure=pressure
+    thermo_summary = freq_job(
+        ts_summary["atoms"],
+        temperature=temperature,
+        pressure=pressure,
     )
 
     return {"ts": ts_summary, "thermo": thermo_summary}
@@ -298,17 +300,17 @@ def irc_job(
     atoms
         The atoms object representing the system.
     fmax
-        Tolerance for the force convergence (in eV/A) (default: 0.01).
+        Tolerance for the force convergence (in eV/A).
     max_steps
-        Maximum number of steps to take (default: 1000).
+        Maximum number of steps to take.
     temperature
-        The temperature for the frequency calculation (default: 298.15 K).
+        The temperature for the frequency calculation in Kelvins.
     pressure
-        The pressure for the frequency calculation (default: 1.0 atm).
+        The pressure for the frequency calculation in bar.
     check_convergence
-        Whether to check the convergence of the optimization (default: False).
+        Whether to check the convergence of the optimization.
     opt_swaps
-        Optional swaps for the optimization parameters (default: None).
+        Optional swaps for the optimization parameters.
 
     Returns
     -------
@@ -378,15 +380,15 @@ def quasi_irc_job(
     atoms
         The atoms object representing the system.
     direction
-        The direction of the IRC calculation ("forward" or "reverse") (default: "forward").
+        The direction of the IRC calculation ("forward" or "reverse").
     temperature
-        The temperature for the frequency calculation (default: 298.15 K).
+        The temperature for the frequency calculation in Kelvins.
     pressure
-        The pressure for the frequency calculation (default: 1.0 atm).
+        The pressure for the frequency calculation in bar.
     irc_swaps
-        Optional swaps for the IRC optimization parameters (default: None).
+        Optional swaps for the IRC optimization parameters.
     opt_swaps
-        Optional swaps for the optimization parameters (default: None).
+        Optional swaps for the optimization parameters.
 
     Returns
     -------
