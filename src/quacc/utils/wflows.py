e68c8539e8cf1eb9fd0241d508d35f0e03a09324
Andrew Rosen <asrosen93@gmail.com>
2023-09-09 00:15:28 -0700

    fix

--- src/quacc/utils/wflows.py
+++ src/quacc/utils/wflows.py
@@ -622,7 +622,7 @@ def subflow(_func: Callable | None = None, **kwargs) -> Subflow:  # sourcery ski
 @requires(prefect_deps and dask_deps, "Need quacc[prefect] dependencies")
 def make_prefect_runner(
     cluster_kwargs: dict,
-    cluster_class: callable | None = None,
+    cluster_class: Callable | None = None,
     adapt_kwargs: dict[str, int | None] | None = None,
     client_kwargs: dict | None = None,
     temporary: bool = False,
@@ -677,7 +677,7 @@ def make_prefect_runner(
 
 @requires(dask_deps, "Need quacc[prefect] dependencies")
 def _make_dask_cluster(
-    cluster_class: callable, cluster_kwargs: dict, verbose=False
+    cluster_class: Callable, cluster_kwargs: dict, verbose=False
 ) -> DaskJob:
     """
     Make a Dask cluster for use with Prefect workflows.
