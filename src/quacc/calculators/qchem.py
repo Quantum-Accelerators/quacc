1fda0081cd5bbc680bd53cb55739e4967ccd79a8
Andrew Rosen <asrosen93@gmail.com>
2023-09-15 22:11:26 -0700

    patch

--- src/quacc/calculators/qchem.py
+++ src/quacc/calculators/qchem.py
@@ -237,7 +237,7 @@ class QChem(FileIOCalculator):
             data["enthalpy"] = data["total_enthalpy"] * (units.kcal / units.mol)
             data["entropy"] = data["total_entropy"] * (0.001 * units.kcal / units.mol)
             for k in ["total_enthalpy", "total_entropy"]:
-                data.pop(k, None)
+                data.pop(k)
         else:
             self.results["hessian"] = None
         self.results["qc_output"] = data
