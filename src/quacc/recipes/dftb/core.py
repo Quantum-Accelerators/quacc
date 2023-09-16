5bcbc6ed760f17347905a395018e29ddd7a2ca3c
Andrew Rosen <asrosen93@gmail.com>
2023-09-15 23:53:58 -0700

    refactor

--- src/quacc/recipes/dftb/core.py
+++ src/quacc/recipes/dftb/core.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 from ase.calculators.dftb import Dftb
 
-from quacc import job
+from quacc import SETTINGS, job
 from quacc.schemas import fetch_atoms
 from quacc.schemas.ase import summarize_run
 from quacc.utils.calc import run_calc
@@ -34,6 +34,18 @@ def static_job(
     """
     Carry out a single-point calculation.
 
+    ??? Note
+
+        Calculator Defaults:
+
+        ```python
+        {
+            "Hamiltonian_": "xTB" if "xtb" in method.lower()
+            else "DFTB", "Hamiltonian_Method": method if "xtb" in method.lower()
+            else None, "kpts": kpts or ((1, 1, 1) if atoms.pbc.any() else None)
+        }
+        ```
+
     Parameters
     ----------
     atoms
@@ -42,22 +54,9 @@ def static_job(
     method
         Method to use.
     kpts
-        k-point grid to use. Defaults to None for molecules and (1, 1, 1) for
-        solids.
+        k-point grid to use.
     calc_swaps
         Dictionary of custom kwargs for the calculator.
-
-        ???+ Note
-
-             Overrides the following defaults:
-
-            ```python
-            {
-                "Hamiltonian_": "xTB" if "xtb" in method.lower()
-                else "DFTB", "Hamiltonian_Method": method if "xtb" in method.lower()
-                else None, "kpts": kpts or ((1, 1, 1) if atoms.pbc.any() else None)
-            }
-            ```
     copy_files
         Files to copy to the runtime directory.
 
@@ -66,28 +65,26 @@ def static_job(
     RunSchema
         Dictionary of results, specified in [quacc.schemas.ase.summarize_run][]
     """
-    atoms = fetch_atoms(atoms)
-    calc_swaps = calc_swaps or {}
 
     defaults = {
         "Hamiltonian_": "xTB" if "xtb" in method.lower() else "DFTB",
         "Hamiltonian_Method": method if "xtb" in method.lower() else None,
-        "kpts": kpts or ((1, 1, 1) if atoms.pbc.any() else None),
+        "kpts": kpts,
     }
-    flags = merge_dicts(defaults, calc_swaps)
 
-    atoms.calc = Dftb(**flags)
-    final_atoms = run_calc(atoms, geom_file=GEOM_FILE, copy_files=copy_files)
+    summary = _base_job(
+        atoms,
+        defaults=defaults,
+        calc_swaps=calc_swaps,
+        additional_fields={"name": "DFTB+ Static"},
+        copy_files=copy_files,
+    )
 
-    if check_logfile(LOG_FILE, "SCC is NOT converged"):
+    if SETTINGS.CHECK_CONVERGENCE and check_logfile(LOG_FILE, "SCC is NOT converged"):
         msg = "SCC is not converged"
         raise ValueError(msg)
 
-    return summarize_run(
-        final_atoms,
-        input_atoms=atoms,
-        additional_fields={"name": "DFTB+ Static"},
-    )
+    return summary
 
 
 @job
@@ -102,6 +99,22 @@ def relax_job(
     """
     Carry out a structure relaxation.
 
+    ??? Note
+
+        Calculator Defaults:
+
+        ```python
+        {
+            "Hamiltonian_": "xTB" if "xtb" in method.lower() else "DFTB",
+            "Hamiltonian_Method": method if "xtb" in method.lower() else None,
+            "kpts": kpts,
+            "Driver_": "GeometryOptimization",
+            "Driver_LatticeOpt": "Yes" if relax_cell else "No",
+            "Driver_AppendGeometries": "Yes",
+            "Driver_MaxSteps": 2000,
+        }
+        ```
+
     Parameters
     ----------
     atoms
@@ -110,29 +123,12 @@ def relax_job(
     method
         Method to use.
     kpts
-        k-point grid to use. Defaults to None for molecules and (1, 1, 1) for
-        solids.
+        k-point grid to use.
     relax_cell
         Whether to relax the unit cell shape/volume in addition to the
         positions.
     calc_swaps
         Dictionary of custom kwargs for the calculator.
-
-        ???+ Note
-
-             Overrides the following defaults:
-
-            ```python
-            {
-                "Hamiltonian_": "xTB" if "xtb" in method.lower() else "DFTB",
-                "Hamiltonian_Method": method if "xtb" in method.lower() else None,
-                "kpts": kpts or ((1, 1, 1) if atoms.pbc.any() else None),
-                "Driver_": "GeometryOptimization",
-                "Driver_LatticeOpt": "Yes" if relax_cell else "No",
-                "Driver_AppendGeometries": "Yes",
-                "Driver_MaxSteps": 2000,
-            }
-            ```
     copy_files
         Files to copy to the runtime directory.
 
@@ -141,29 +137,73 @@ def relax_job(
     RunSchema
         Dictionary of results, specified in [quacc.schemas.ase.summarize_run][]
     """
-    atoms = fetch_atoms(atoms)
-    calc_swaps = calc_swaps or {}
 
     defaults = {
         "Hamiltonian_": "xTB" if "xtb" in method.lower() else "DFTB",
         "Hamiltonian_Method": method if "xtb" in method.lower() else None,
-        "kpts": kpts or ((1, 1, 1) if atoms.pbc.any() else None),
+        "kpts": kpts,
         "Driver_": "GeometryOptimization",
         "Driver_LatticeOpt": "Yes" if relax_cell else "No",
         "Driver_AppendGeometries": "Yes",
         "Driver_MaxSteps": 2000,
     }
+
+    summary = _base_job(
+        atoms,
+        defaults=defaults,
+        calc_swaps=calc_swaps,
+        additional_fields={"name": "DFTB+ Relax"},
+        copy_files=copy_files,
+    )
+
+    if SETTINGS.CHECK_CONVERGENCE and not check_logfile(LOG_FILE, "Geometry converged"):
+        msg = "Geometry did not converge"
+        raise ValueError(msg)
+
+    return summary
+
+
+def _base_job(
+    atoms: Atoms | dict,
+    defaults: dict | None = None,
+    calc_swaps: dict | None = None,
+    additional_fields: dict | None = None,
+    copy_files: list[str] | None = None,
+) -> RunSchema:
+    """
+    Base job function for DFTB+ recipes.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object or a dictionary with the key "atoms" and an Atoms object as
+        the value
+    defaults
+        The default calculator parameters to use.
+    calc_swaps
+        Dictionary of custom kwargs for the calculator to override defaults.
+    additional_fields
+        Any additional fields to supply to the summarizer.
+    copy_files
+        Files to copy to the runtime directory.
+
+    Returns
+    -------
+    RunSchema
+        Dictionary of results, specified in [quacc.schemas.ase.summarize_run][]
+    """
+
+    atoms = fetch_atoms(atoms)
+    calc_swaps = calc_swaps or {}
+    defaults = defaults or {}
+
     flags = merge_dicts(defaults, calc_swaps)
 
     atoms.calc = Dftb(**flags)
     final_atoms = run_calc(atoms, geom_file=GEOM_FILE, copy_files=copy_files)
 
-    if not check_logfile(LOG_FILE, "Geometry converged"):
-        msg = "Geometry did not converge"
-        raise ValueError(msg)
-
     return summarize_run(
         final_atoms,
         input_atoms=atoms,
-        additional_fields={"name": "DFTB+ Relax"},
+        additional_fields=additional_fields,
     )
