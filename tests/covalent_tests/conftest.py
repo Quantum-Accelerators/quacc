f4911499c9baec3faf92c40a316bacaed2c8dc31
Andrew Rosen <asrosen93@gmail.com>
2023-09-24 15:43:11 -0700

    patch

--- tests/covalent_tests/conftest.py
+++ tests/covalent_tests/conftest.py
@@ -1,8 +1,6 @@
 import importlib
-import subprocess
 
 import pytest
-import requests
 
 
 @pytest.fixture(autouse=True)
@@ -13,14 +11,3 @@ def reload_covalent_quacc(default_settings):
     SETTINGS.WORKFLOW_ENGINE = "covalent"
     SETTINGS.SCRATCH_DIR = default_settings.SCRATCH_DIR
     SETTINGS.RESULTS_DIR = default_settings.RESULTS_DIR
-
-
-@pytest.fixture(autouse=True)
-def start_covalent_server():
-    import covalent as ct
-
-    address = f"http://{ct.get_config('dispatcher.address')}:{str(ct.get_config('dispatcher.port'))}"
-    try:
-        requests.session().get(address)
-    except requests.exceptions.ConnectionError:
-        subprocess.run(["covalent", "start"], check=True)
