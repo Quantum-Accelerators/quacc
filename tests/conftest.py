f6d14d8590753639cc5e6a55ae26289b08f2d63e
Andrew Rosen <asrosen93@gmail.com>
2023-09-24 18:18:21 -0700

    test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,26 +2,18 @@ import os
 from pathlib import Path
 from shutil import rmtree
 
-import pytest
-
 FILE_DIR = Path(__file__).resolve().parent
 TEST_RESULTS_DIR = FILE_DIR / ".test_results"
 TEST_SCRATCH_DIR = FILE_DIR / ".test_scratch"
 
 
-@pytest.fixture
-def default_settings():
+def pytest_sessionstart():
     from quacc import SETTINGS
 
-    DEFAULT_SETTINGS = SETTINGS.copy()
-    DEFAULT_SETTINGS.RESULTS_DIR = TEST_RESULTS_DIR
-    DEFAULT_SETTINGS.SCRATCH_DIR = TEST_SCRATCH_DIR
-    return DEFAULT_SETTINGS
-
-
-def pytest_sessionstart():
-    os.makedirs(TEST_RESULTS_DIR, exist_ok=True)
-    os.makedirs(TEST_SCRATCH_DIR, exist_ok=True)
+    SETTINGS.RESULTS_DIR = TEST_RESULTS_DIR
+    SETTINGS.SCRATCH_DIR = TEST_SCRATCH_DIR
+    os.makedirs(SETTINGS.RESULTS_DIR, exist_ok=True)
+    os.makedirs(SETTINGS.SCRATCH_DIR, exist_ok=True)
 
 
 def pytest_sessionfinish():
