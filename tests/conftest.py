ff0d793e38a97a8eccc2e9c0944092eaa663299a
Andrew Rosen <asrosen93@gmail.com>
2023-10-09 22:45:39 -0700

    patch

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,6 @@
 import pytest
 
 
-@pytest.fixture()
-def test_results_dir():
-    from pathlib import Path
-
-    FILE_DIR = Path(__file__).resolve().parent
-    return FILE_DIR / ".test_results"
-
-
-@pytest.fixture()
-def test_scratch_dir():
-    from pathlib import Path
-
-    FILE_DIR = Path(__file__).resolve().parent
-
-    return FILE_DIR / ".test_scratch"
-
-
 @pytest.fixture
 def default_settings():
     from quacc import SETTINGS
@@ -27,17 +10,27 @@ def default_settings():
 
 def pytest_sessionstart(test_results_dir, test_scratch_dir):
     import os
+    from pathlib import Path
 
     from quacc import SETTINGS
 
+    FILE_DIR = Path(__file__).resolve().parent
+    test_results_dir = FILE_DIR / ".test_results"
+    test_scratch_dir = FILE_DIR / ".test_scratch"
+
     SETTINGS.RESULTS_DIR = test_results_dir
     SETTINGS.SCRATCH_DIR = test_scratch_dir
-    os.makedirs(SETTINGS.RESULTS_DIR, exist_ok=True)
-    os.makedirs(SETTINGS.SCRATCH_DIR, exist_ok=True)
+    os.makedirs(test_results_dir, exist_ok=True)
+    os.makedirs(test_scratch_dir, exist_ok=True)
 
 
 def pytest_sessionfinish(test_results_dir, test_scratch_dir):
+    from pathlib import Path
     from shutil import rmtree
+
+    FILE_DIR = Path(__file__).resolve().parent
+    test_results_dir = FILE_DIR / ".test_results"
+    test_scratch_dir = FILE_DIR / ".test_scratch"
 
     rmtree(test_results_dir, ignore_errors=True)
     rmtree(test_scratch_dir, ignore_errors=True)
