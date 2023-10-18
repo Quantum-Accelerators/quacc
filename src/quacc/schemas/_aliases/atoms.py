7a54f2a3c0d32980a84c1c65886b0c287fa394bd
Andrew Rosen <asrosen93@gmail.com>
2023-10-18 13:05:07 -0700

    patch

--- src/quacc/schemas/_aliases/atoms.py
+++ src/quacc/schemas/_aliases/atoms.py
@@ -1,20 +1,18 @@
 """Aliases for type hinting `quacc.schemas.atoms`"""
-from typing import TYPE_CHECKING
+from typing import Any
 
-if TYPE_CHECKING:
-    from typing import Any, TypedDict
+from ase import Atoms
+from pymatgen.core import Molecule, Structure
 
-    from ase import Atoms
-    from pymatgen.core import Molecule, Structure
+from quacc.schemas._aliases.emmet import MoleculeMetadata, StructureMetadata
 
-    from quacc.schemas._aliases.emmet import MoleculeMetadata, StructureMetadata
 
-    class AtomsSchema(TypedDict, StructureMetadata, MoleculeMetadata, total=False):
-        """
-        Type hint associated with `quacc.schemas.atoms.atoms_to_metadata`
-        """
+class AtomsSchema(StructureMetadata, MoleculeMetadata):
+    """
+    Type hint associated with `quacc.schemas.atoms.atoms_to_metadata`
+    """
 
-        atoms: Atoms
-        atoms_info: dict[str, Any]  # from atoms.info
-        structure: Structure  # if atoms.pbc.any()
-        molecule: Molecule  # if not atoms.pbc.any()
+    atoms: Atoms
+    atoms_info: dict[str, Any]  # from atoms.info
+    structure: Structure  # if atoms.pbc.any()
+    molecule: Molecule  # if not atoms.pbc.any()
