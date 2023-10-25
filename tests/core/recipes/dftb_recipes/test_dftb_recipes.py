0a2c35625bb072529f2a5baacec05e539f837787
Andrew S. Rosen <asrosen93@gmail.com>
2023-10-24 21:42:07 -0700

    Update test_dftb_recipes.py (#1099)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- tests/core/recipes/dftb_recipes/test_dftb_recipes.py
+++ tests/core/recipes/dftb_recipes/test_dftb_recipes.py
@@ -19,7 +19,6 @@ def test_static_job(tmpdir):
     tmpdir.chdir()
 
     atoms = molecule("H2O")
-
     output = static_job(atoms)
     assert output["natoms"] == len(atoms)
     assert output["parameters"]["Hamiltonian_"] == "xTB"
@@ -29,8 +28,25 @@ def test_static_job(tmpdir):
         np.array_equal(output["atoms"].get_positions(), atoms.get_positions()) is True
     )
 
-    atoms = bulk("Cu")
+    atoms = bulk("Cu") * (3, 3, 3)
+    output = static_job(atoms)
+    assert output["nsites"] == len(atoms)
+    assert output["parameters"]["Hamiltonian_"] == "xTB"
+    assert output["parameters"]["Hamiltonian_Method"] == "GFN2-xTB"
+    assert (
+        output["parameters"]["Hamiltonian_KPointsAndWeights_"].strip()
+        == "SupercellFolding"
+    )
+    assert output["parameters"]["Hamiltonian_KPointsAndWeights_empty000"] == "1 0 0"
+    assert output["parameters"]["Hamiltonian_KPointsAndWeights_empty001"] == "0 1 0"
+    assert output["parameters"]["Hamiltonian_KPointsAndWeights_empty002"] == "0 0 1"
+    assert output["results"]["energy"] == pytest.approx(-2885.417379886678)
+    assert (
+        np.array_equal(output["atoms"].get_positions(), atoms.get_positions()) is True
+    )
+    assert np.array_equal(output["atoms"].cell.array, atoms.cell.array) is True
 
+    atoms = bulk("Cu")
     output = static_job(atoms, kpts=(3, 3, 3))
     assert output["nsites"] == len(atoms)
     assert output["parameters"]["Hamiltonian_"] == "xTB"
@@ -113,12 +129,3 @@ def test_relax_job(tmpdir):
         atoms = bulk("Cu") * (2, 1, 1)
         atoms[0].position += 0.5
         relax_job(atoms, kpts=(3, 3, 3), calc_swaps={"MaxSteps": 1})
-
-
-def test_unique_workdir(tmpdir):
-    DEFAULT_SETTINGS = SETTINGS.copy()
-
-    SETTINGS.CREATE_UNIQUE_WORKDIR = True
-    test_static_job(tmpdir)
-    test_relax_job(tmpdir)
-    SETTINGS.CREATE_UNIQUE_WORKDIR = DEFAULT_SETTINGS.CREATE_UNIQUE_WORKDIR
