f530864a168ea0bf1ebbde646cf402ceaba814af
Andrew S. Rosen <asrosen93@gmail.com>
2023-10-29 10:00:22 -0700

    Refactor Q-Chem calculator (#1122)
    Refactoring of the Q-Chem calculator. This PR mainly seeks to clean up
    the calculator to help with maintenance. It changes no features.
    
    Some things that I cleaned up:
    - Moved all I/O related functions to a new module
    `quacc.calculators.qchem.io` to make the calculator easier to read.
    - Added missing type-hints and missing docstrings.
    - Created a custom type-hint for the `calc.results` object for clearer
    reporting.
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/quacc/calculators/qchem/qchem.py
+++ src/quacc/calculators/qchem/qchem.py
@@ -2,63 +2,25 @@
 from __future__ import annotations
 
 import inspect
-import logging
-import struct
-from copy import deepcopy
 from pathlib import Path
-from typing import ClassVar
+from typing import TYPE_CHECKING
 
-import numpy as np
-from ase import Atoms, units
 from ase.calculators.calculator import FileIOCalculator
-from emmet.core.tasks import _parse_custodian
-from monty.io import zopen
-from pymatgen.io.ase import AseAtomsAdaptor
-from pymatgen.io.qchem.inputs import QCInput
-from pymatgen.io.qchem.outputs import QCOutput
-from pymatgen.io.qchem.sets import QChemDictSet
 
 from quacc.calculators.qchem import custodian
+from quacc.calculators.qchem.io import read_qchem, write_qchem
 
-logger = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from typing import Any, ClassVar, Literal
+
+    from ase import Atoms
+
+    from quacc.calculators.qchem.io import Results
 
 
-# TODO: Would be more consistent to convert the Hessian to ASE units if it's being
-# stored in the first-class results.
 class QChem(FileIOCalculator):
     """
-
-    Parameters
-    ----------
-    atoms
-        The Atoms object to be used for the calculation.
-    cores
-        Number of cores to use for the Q-Chem calculation.
-    charge
-        The total charge of the molecular system.
-    spin_multiplicity
-        The spin multiplicity of the molecular system.
-    qchem_input_params
-        Dictionary of Q-Chem input parameters to be passed to
-        `pymatgen.io.qchem.sets.DictSet`.
-    **fileiocalculator_kwargs
-        Additional arguments to be passed to
-        `ase.calculators.calculator.FileIOCalculator`.
-
-    Returns
-    -------
-    Atoms
-        The ASE Atoms object with attached Q-Chem calculator. In calc.results,
-        the following properties are set:
-
-        - energy: electronic energy in eV
-        - forces: forces in eV/A
-        - hessian: Hessian in native Q-Chem units
-        - entropy: total enthalpy in eV
-        - enthalpy: total entropy in eV/K
-        - qc_output: Output from `pymatgen.io.qchem.outputs.QCOutput.data`
-        - qc_input: Input from `pymatgen.io.qchem.inputs.QCInput.as_dict()`
-        - custodian: custodian.json file metadata
+    Custom Q-Chem calculator built on Pymatgen and Custodian.
     """
 
     implemented_properties: ClassVar[list[str]] = [
@@ -71,6 +33,7 @@ class QChem(FileIOCalculator):
         "qc_input",
         "custodian",
     ]
+    results: ClassVar[Results] = {}
 
     def __init__(
         self,
@@ -79,27 +42,149 @@ class QChem(FileIOCalculator):
         spin_multiplicity: int = 1,
         method: str | None = None,
         basis_set: str = "def2-tzvpd",
-        job_type: str = "force",
+        job_type: Literal["sp", "force", "opt", "freq"] = "force",
         scf_algorithm: str = "diis",
         cores: int = 1,
-        qchem_input_params: dict | None = None,
+        qchem_input_params: dict[str, Any] | None = None,
         **fileiocalculator_kwargs,
-    ):
+    ) -> None:
+        """
+        Initialize the Q-Chem calculator.
+
+        Parameters
+        ----------
+        atoms
+            The Atoms object to be used for the calculation.
+        cores
+            Number of cores to use for the Q-Chem calculation.
+        charge
+            The total charge of the molecular system.
+        spin_multiplicity
+            The spin multiplicity of the molecular system.
+        qchem_input_params
+            Dictionary of Q-Chem input parameters to be passed to
+            `pymatgen.io.qchem.sets.DictSet`.
+        **fileiocalculator_kwargs
+            Additional arguments to be passed to
+            `ase.calculators.calculator.FileIOCalculator`.
+
+        Returns
+        -------
+        None
+        """
+
         # Assign variables to self
         self.atoms = atoms
-        self.cores = cores
         self.charge = charge
         self.spin_multiplicity = spin_multiplicity
-        self.job_type = job_type
+        self.method = method
         self.basis_set = basis_set
+        self.job_type = job_type
         self.scf_algorithm = scf_algorithm
+        self.cores = cores
         self.qchem_input_params = qchem_input_params or {}
         self.fileiocalculator_kwargs = fileiocalculator_kwargs
 
-        # Sanity checks
+        # Instantiate previous orbital coefficients
+        self._prev_orbital_coeffs = None
+
         if "directory" in self.fileiocalculator_kwargs:
             raise NotImplementedError("The directory kwarg is not supported.")
 
+        # Clean up parameters
+        self._cleanup_qchem_input_params()
+        self._set_default_params()
+
+        # Get Q-Chem executable command
+        self.command = self._manage_environment()
+
+        # Instantiate the calculator
+        FileIOCalculator.__init__(
+            self,
+            restart=None,
+            ignore_bad_restart_file=FileIOCalculator._deprecated,
+            label=None,
+            atoms=self.atoms,
+            **self.fileiocalculator_kwargs,
+        )
+
+    def write_input(
+        self,
+        atoms: Atoms,
+        properties: list[str] | None = None,
+        system_changes: list[str] | None = None,
+    ) -> None:
+        """
+        Write the Q-Chem input files.
+
+        Parameters
+        ----------
+        atoms
+            The Atoms object to be used for the calculation.
+        properties
+            List of properties to calculate.
+        system_changes
+            List of changes to the system since last calculation.
+
+        Returns
+        -------
+        None
+        """
+        FileIOCalculator.write_input(self, atoms, properties, system_changes)
+        write_qchem(
+            atoms,
+            charge=self.charge,
+            spin_multiplicity=self.spin_multiplicity,
+            basis_set=self.basis_set,
+            job_type=self.job_type,
+            scf_algorithm=self.scf_algorithm,
+            qchem_input_params=self.qchem_input_params,
+            prev_orbital_coeffs=self._prev_orbital_coeffs,
+        )
+
+    def read_results(self) -> None:
+        """
+        Read the Q-Chem output files. Update the .results and ._prev_orbital_coeffs
+        attributes.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        results, _prev_orbital_coeffs = read_qchem(job_type=self.job_type)
+        self.results = results
+        self._prev_orbital_coeffs = _prev_orbital_coeffs
+
+    def _manage_environment(self) -> str:
+        """
+        Return the command to run the Q-Chem calculator via Custodian.
+
+        Returns
+        -------
+        str
+            The command flag to run Q-Chem with Custodian.
+        """
+
+        qchem_custodian_script = Path(inspect.getfile(custodian)).resolve()
+        return f"python {qchem_custodian_script} {self.cores}"
+
+    def _cleanup_qchem_input_params(self) -> None:
+        """
+        Clean up q-chem input parameters for the Q-Chem calculator.
+        Modifies self.qchem_input_params in place.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         if "overwrite_inputs" not in self.qchem_input_params:
             self.qchem_input_params["overwrite_inputs"] = {}
 
@@ -111,13 +196,24 @@ class QChem(FileIOCalculator):
         if "rem" not in self.qchem_input_params["overwrite_inputs"]:
             self.qchem_input_params["overwrite_inputs"]["rem"] = {}
         if (
-            method
+            self.method
             and "method" not in self.qchem_input_params["overwrite_inputs"]["rem"]
         ):
-            self.qchem_input_params["overwrite_inputs"]["rem"]["method"] = method
+            self.qchem_input_params["overwrite_inputs"]["rem"]["method"] = self.method
 
-        # We will save the parameters that have been passed to the Q-Chem
-        # calculator via FileIOCalculator's self.default_parameters
+    def _set_default_params(self) -> None:
+        """
+        Store the parameters that have been passed to the Q-Chem
+        calculator in FileIOCalculator's self.default_parameters.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
         self.default_parameters = {
             "cores": self.cores,
             "charge": self.charge,
@@ -126,8 +222,8 @@ class QChem(FileIOCalculator):
             "basis_set": self.basis_set,
         }
 
-        if method:
-            self.default_parameters["method"] = method
+        if self.method:
+            self.default_parameters["method"] = self.method
 
         # We also want to save the contents of self.qchem_input_params. However,
         # the overwrite_inputs key will have a corresponding value which is
@@ -142,135 +238,3 @@ class QChem(FileIOCalculator):
                         ] = self.qchem_input_params[key][subkey][subsubkey]
             else:
                 self.default_parameters[key] = self.qchem_input_params[key]
-
-        # Get Q-Chem executable command
-        self.command = self._manage_environment()
-
-        # Instantiate previous orbital coefficients
-        self.prev_orbital_coeffs = None
-
-        # Instantiate the calculator
-        FileIOCalculator.__init__(
-            self,
-            restart=None,
-            ignore_bad_restart_file=FileIOCalculator._deprecated,
-            label=None,
-            atoms=self.atoms,
-            **self.fileiocalculator_kwargs,
-        )
-
-    def _manage_environment(self) -> str:
-        """
-        Manage the environment for the Q-Chem calculator.
-
-        Returns
-        -------
-        str
-            The command flag to pass to the Q-Chem calculator.
-        """
-
-        # Return the command flag
-        run_qchem_custodian_file = Path(inspect.getfile(custodian)).resolve()
-        return f"python {run_qchem_custodian_file} {self.cores}"
-
-    def write_input(self, atoms, properties=None, system_changes=None):
-        FileIOCalculator.write_input(self, atoms, properties, system_changes)
-        atoms = deepcopy(atoms)
-        atoms.charge = self.charge
-        atoms.spin_multiplicity = self.spin_multiplicity
-        mol = AseAtomsAdaptor.get_molecule(atoms)
-        if self.prev_orbital_coeffs is not None:
-            with Path("53.0").open(mode="wb") as file:
-                for val in self.prev_orbital_coeffs:
-                    data = struct.pack("d", val)
-                    file.write(data)
-            if "overwrite_inputs" not in self.qchem_input_params:
-                self.qchem_input_params["overwrite_inputs"] = {}
-            if "rem" not in self.qchem_input_params["overwrite_inputs"]:
-                self.qchem_input_params["overwrite_inputs"]["rem"] = {}
-            if "scf_guess" not in self.qchem_input_params["overwrite_inputs"]["rem"]:
-                self.qchem_input_params["overwrite_inputs"]["rem"]["scf_guess"] = "read"
-        qcin = QChemDictSet(
-            mol,
-            self.job_type,
-            self.basis_set,
-            self.scf_algorithm,
-            qchem_version=6,
-            **self.qchem_input_params,
-        )
-        qcin.write("mol.qin")
-
-    def read_results(self):
-        qc_input = QCInput.from_file("mol.qin").as_dict()
-        qc_output = QCOutput("mol.qout").data
-        self.results["qc_output"] = qc_output
-        self.results["qc_input"] = qc_input
-        self.results["custodian"] = _parse_custodian(Path.cwd())
-
-        self.results["energy"] = qc_output["final_energy"] * units.Hartree
-
-        if self.job_type in ["force", "opt"]:
-            tmp_grad_data = []
-            # Read the gradient scratch file in 8 byte chunks
-            with zopen("131.0", mode="rb") as file:
-                binary = file.read()
-            tmp_grad_data.extend(
-                struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0]
-                for ii in range(len(binary) // 8)
-            )
-            grad = [
-                [
-                    float(tmp_grad_data[ii * 3]),
-                    float(tmp_grad_data[ii * 3 + 1]),
-                    float(tmp_grad_data[ii * 3 + 2]),
-                ]
-                for ii in range(len(tmp_grad_data) // 3)
-            ]
-            # Ensure that the scratch values match the correct values from the
-            # output file but with higher precision
-            if qc_output["pcm_gradients"] is not None:
-                gradient = qc_output["pcm_gradients"][0]
-            else:
-                gradient = qc_output["gradients"][0]
-            for ii, subgrad in enumerate(grad):
-                for jj, val in enumerate(subgrad):
-                    if abs(gradient[ii, jj] - val) > 1e-6:
-                        raise ValueError(
-                            "Difference between gradient value in scratch file vs. output file should not be this large."
-                        )
-                    gradient[ii, jj] = val
-            # Convert gradient to force + deal with units
-            self.results["forces"] = gradient * (-units.Hartree / units.Bohr)
-        else:
-            self.results["forces"] = None
-
-        # Read orbital coefficients scratch file in 8 byte chunks
-        self.prev_orbital_coeffs = []
-        with zopen("53.0", mode="rb") as file:
-            binary = file.read()
-        self.prev_orbital_coeffs.extend(
-            struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0]
-            for ii in range(len(binary) // 8)
-        )
-
-        if self.job_type == "freq":
-            tmp_hess_data = []
-            # Read Hessian scratch file in 8 byte chunks
-            with zopen("132.0", mode="rb") as file:
-                binary = file.read()
-            tmp_hess_data.extend(
-                struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0]
-                for ii in range(len(binary) // 8)
-            )
-            self.results["hessian"] = np.reshape(
-                np.array(tmp_hess_data),
-                (len(qc_output["species"]) * 3, len(qc_output["species"]) * 3),
-            )
-            self.results["enthalpy"] = qc_output["total_enthalpy"] * (
-                units.kcal / units.mol
-            )
-            self.results["entropy"] = qc_output["total_entropy"] * (
-                0.001 * units.kcal / units.mol
-            )
-        else:
-            self.results["hessian"] = None
--- src/quacc/calculators/vasp/vasp.py
+++ src/quacc/calculators/vasp/vasp.py
@@ -35,73 +35,6 @@ class Vasp(Vasp_):
     This is a wrapper around the ASE Vasp calculator that adjusts INCAR
     parameters on-the-fly, allows for ASE to run VASP via Custodian, and
     supports several automatic k-point generation schemes from Pymatgen.
-
-    Parameters
-    ----------
-    input_atoms
-        The input Atoms object to be used for the calculation.
-    preset
-        The name of a YAML file containing a list of INCAR parameters to use as
-        a "preset" for the calculator. quacc will automatically look in the
-        `VASP_PRESET_DIR` (default: quacc/calculators/vasp/presets) for the
-        file, such that preset="BulkSet" is supported, for instance. The .yaml
-        extension is not necessary. Any user-supplied calculator **kwargs will
-        override any corresponding preset values.
-    use_custodian
-        Whether to use Custodian to run VASP. Default is True in settings.
-    incar_copilot
-        If True, the INCAR parameters will be adjusted if they go against the
-        VASP manual. Default is True in settings.
-    force_copilot
-        If False, INCAR swaps enabled by the INCAR co-pilot will not override
-        the user's chosen value. It will only override values that aren't set.
-    copy_magmoms
-        If True, any pre-existing `atoms.get_magnetic_moments()` will be set in
-        `atoms.set_initial_magnetic_moments()`. Set this to False if you want to
-        use a preset's magnetic moments every time.
-    preset_mag_default
-        Default magmom value for sites without one explicitly specified in the
-        preset. Only used if a preset is specified with an elemental_mags_dict
-        key-value pair. Default is 1.0 in settings.
-    mag_cutoff
-        Set all initial magmoms to 0 if all have a magnitude below this value.
-        Default is 0.05 in settings.
-    elemental_magmoms
-        A dictionary of elemental initial magnetic moments to pass to
-        [quacc.runners.prep.set_magmoms][], e.g. `{"Fe": 5, "Ni": 4}`.
-    auto_kpts
-        An automatic k-point generation scheme from Pymatgen. Options include:
-
-        - {"line_density": float}. This will call
-          `pymatgen.symmetry.bandstructure.HighSymmKpath`
-            with `path_type="latimer_munro"`. The `line_density` value will be
-            set in the `.get_kpoints` attribute.
-
-        - {"kppvol": float}. This will call
-          `pymatgen.io.vasp.inputs.Kpoints.automatic_density_by_vol`
-            with the given value for `kppvol`.
-
-        - {"kppa": float}. This will call
-          `pymatgen.io.vasp.inputs.Kpoints.automatic_density`
-            with the given value for `kppa`.
-
-        - {"length_densities": [float, float, float]}. This will call
-          `pymatgen.io.vasp.inputs.Kpoints.automatic_density_by_lengths`
-            with the given value for `length_densities`.
-
-        If multiple options are specified, the most dense k-point scheme will be
-        chosen.
-    auto_dipole
-        If True, will automatically set dipole moment correction parameters
-        based on the center of mass (in the c dimension by default).
-    **kwargs
-        Additional arguments to be passed to the VASP calculator, e.g.
-        `xc='PBE'`, `encut=520`. Takes all valid ASE calculator arguments.
-
-    Returns
-    -------
-    Atoms
-        The ASE Atoms object with attached VASP calculator.
     """
 
     def __init__(
@@ -120,7 +53,76 @@ class Vasp(Vasp_):
         | None = None,
         auto_dipole: bool | None = None,
         **kwargs,
-    ):
+    ) -> None:
+        """
+        Initialize the VASP calculator.
+
+        Parameters
+        ----------
+        input_atoms
+            The input Atoms object to be used for the calculation.
+        preset
+            The name of a YAML file containing a list of INCAR parameters to use as
+            a "preset" for the calculator. quacc will automatically look in the
+            `VASP_PRESET_DIR` (default: quacc/calculators/vasp/presets) for the
+            file, such that preset="BulkSet" is supported, for instance. The .yaml
+            extension is not necessary. Any user-supplied calculator **kwargs will
+            override any corresponding preset values.
+        use_custodian
+            Whether to use Custodian to run VASP. Default is True in settings.
+        incar_copilot
+            If True, the INCAR parameters will be adjusted if they go against the
+            VASP manual. Default is True in settings.
+        force_copilot
+            If False, INCAR swaps enabled by the INCAR co-pilot will not override
+            the user's chosen value. It will only override values that aren't set.
+        copy_magmoms
+            If True, any pre-existing `atoms.get_magnetic_moments()` will be set in
+            `atoms.set_initial_magnetic_moments()`. Set this to False if you want to
+            use a preset's magnetic moments every time.
+        preset_mag_default
+            Default magmom value for sites without one explicitly specified in the
+            preset. Only used if a preset is specified with an elemental_mags_dict
+            key-value pair. Default is 1.0 in settings.
+        mag_cutoff
+            Set all initial magmoms to 0 if all have a magnitude below this value.
+            Default is 0.05 in settings.
+        elemental_magmoms
+            A dictionary of elemental initial magnetic moments to pass to
+            [quacc.runners.prep.set_magmoms][], e.g. `{"Fe": 5, "Ni": 4}`.
+        auto_kpts
+            An automatic k-point generation scheme from Pymatgen. Options include:
+
+            - {"line_density": float}. This will call
+            `pymatgen.symmetry.bandstructure.HighSymmKpath`
+                with `path_type="latimer_munro"`. The `line_density` value will be
+                set in the `.get_kpoints` attribute.
+
+            - {"kppvol": float}. This will call
+            `pymatgen.io.vasp.inputs.Kpoints.automatic_density_by_vol`
+                with the given value for `kppvol`.
+
+            - {"kppa": float}. This will call
+            `pymatgen.io.vasp.inputs.Kpoints.automatic_density`
+                with the given value for `kppa`.
+
+            - {"length_densities": [float, float, float]}. This will call
+            `pymatgen.io.vasp.inputs.Kpoints.automatic_density_by_lengths`
+                with the given value for `length_densities`.
+
+            If multiple options are specified, the most dense k-point scheme will be
+            chosen.
+        auto_dipole
+            If True, will automatically set dipole moment correction parameters
+            based on the center of mass (in the c dimension by default).
+        **kwargs
+            Additional arguments to be passed to the VASP calculator, e.g.
+            `xc='PBE'`, `encut=520`. Takes all valid ASE calculator arguments.
+
+        Returns
+        -------
+        None
+        """
         from quacc import SETTINGS
 
         # Set defaults
@@ -245,6 +247,10 @@ class Vasp(Vasp_):
     def _manage_environment(self) -> str:
         """
         Manage the environment for the VASP calculator.
+
+        Parameters
+        ----------
+        None
 
         Returns
         -------
