fed10eb6766164ba16ce0dcfd1e2defcaeee06ed
Andrew Rosen <asrosen93@gmail.com>
2023-10-28 19:15:46 -0700

    fix

--- tests/core/calculators/custodian/test_qchem_custodian.py
+++ tests/core/calculators/custodian/test_qchem_custodian.py
@@ -1,7 +1,7 @@
 import pytest
 from custodian import Custodian
 
-from quacc.calculators.custodian.qchem import run_custodian
+from quacc.calculators.qchem.custodian import run_custodian
 
 pytest.importorskip("openbabel")
 
