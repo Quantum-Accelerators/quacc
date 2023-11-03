c9665cfa91ea5e6b129994b757fda84744a6ce38
Andrew S. Rosen <asrosen93@gmail.com>
2023-11-03 14:22:49 -0700

    Create core.py

---
+++ src/quacc/recipes/common/core.py
@@ -0,0 +1,33 @@
+"""Core common workflows"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from quacc import job
+from quacc.runners.ase import run_calc
+
+if TYPE_CHECKING:
+    from ase import Atoms
+    from ase.calculators.calculator import Calculator
+    from numpy.typing import NDArray
+
+
+@job
+def force_job(atoms: Atoms, calculator: Calculator) -> NDArray:
+    """
+    Calculate the forces.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    calculator
+        Calculator to use.
+
+    Returns
+    -------
+    NDArray
+        Forces
+    """
+    atoms.calc = calculator
+    return run_calc(atoms).get_forces()
