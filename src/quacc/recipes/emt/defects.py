02ef0d17dd88e1a922f084aef1b04c97125a56e8
Andrew Rosen <asrosen93@gmail.com>
2023-11-03 10:10:15 -0700

    fix

--- src/quacc/recipes/emt/defects.py
+++ src/quacc/recipes/emt/defects.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 from pymatgen.analysis.defects.generators import VacancyGenerator
 
-from quacc import flow, job, subflow
+from quacc import flow, subflow
 from quacc.atoms.defects import make_defects_from_bulk
 from quacc.recipes.emt.core import relax_job, static_job
 
