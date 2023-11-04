7a077830372d77ebb74d192a05338c05c0835588
Andrew Rosen <asrosen93@gmail.com>
2023-11-03 19:08:12 -0700

    add more tests

---
+++ tests/parsl/test_parsl_recipes.py
@@ -0,0 +1,16 @@
+import pytest
+from ase.build import bulk
+
+parsl = pytest.importorskip("parsl")
+
+
+def test_phonon_flow(tmpdir):
+    pytest.importorskip("phonopy")
+    from quacc.recipes.emt.phonons import phonon_flow
+
+    tmpdir.chdir()
+    atoms = bulk("Cu")
+    output = phonon_flow(atoms)
+    assert output.result()["results"]["thermal_properties"]["temperatures"].shape == (
+        101,
+    )
