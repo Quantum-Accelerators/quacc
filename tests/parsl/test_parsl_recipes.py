51d8c9cfa0d5778c650f5b26b1190e3a8fd962d3
Andrew Rosen <asrosen93@gmail.com>
2023-11-03 19:13:35 -0700

    fix

--- tests/parsl/test_parsl_recipes.py
+++ tests/parsl/test_parsl_recipes.py
@@ -4,6 +4,17 @@ from ase.build import bulk
 parsl = pytest.importorskip("parsl")
 
 
+def setup_module():
+    import contextlib
+
+    with contextlib.suppress(Exception):
+        parsl.load()
+
+
+def teardown_module():
+    parsl.clear()
+
+
 def test_phonon_flow(tmpdir):
     pytest.importorskip("phonopy")
     from quacc.recipes.emt.phonons import phonon_flow
