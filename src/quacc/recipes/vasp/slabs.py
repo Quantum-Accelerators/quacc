8000f8c90584b840550b15ccd0a483cd730322db
Andrew S. Rosen <asrosen93@gmail.com>
2023-11-07 19:42:34 -0500

    Refactor VASP slab workflow (#1172)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/quacc/recipes/vasp/slabs.py
+++ src/quacc/recipes/vasp/slabs.py
@@ -184,17 +184,14 @@ def bulk_to_slabs_flow(
     slab_static_kwargs = slab_static_kwargs or {}
     make_slabs_kwargs = make_slabs_kwargs or {}
 
-    def _make_slabs(atoms: Atoms) -> list[Atoms]:
-        return make_slabs_from_bulk(atoms, **make_slabs_kwargs)
-
     @subflow
     def _relax_job_distributed(atoms: Atoms) -> list[VaspSchema]:
-        slabs = _make_slabs(atoms)
+        slabs = make_slabs_from_bulk(atoms, **make_slabs_kwargs)
         return [slab_relax_job(slab, **slab_relax_kwargs) for slab in slabs]
 
     @subflow
     def _relax_and_static_job_distributed(atoms: Atoms) -> list[VaspSchema]:
-        slabs = _make_slabs(atoms)
+        slabs = make_slabs_from_bulk(atoms, **make_slabs_kwargs)
         return [
             slab_static_job(
                 slab_relax_job(slab, **slab_relax_kwargs)["atoms"],
@@ -253,17 +250,14 @@ def slab_to_ads_flow(
     slab_static_kwargs = slab_static_kwargs or {}
     make_ads_kwargs = make_ads_kwargs or {}
 
-    def _make_ads_slabs(slab: Atoms) -> list[Atoms]:
-        return make_adsorbate_structures(slab, adsorbate, **make_ads_kwargs)
-
     @subflow
     def _relax_job_distributed(slab: Atoms) -> list[VaspSchema]:
-        slabs = _make_ads_slabs(slab)
+        slabs = make_adsorbate_structures(slab, adsorbate, **make_ads_kwargs)
         return [slab_relax_job(slab, **slab_relax_kwargs) for slab in slabs]
 
     @subflow
     def _relax_and_static_job_distributed(slab: Atoms) -> list[VaspSchema]:
-        slabs = _make_ads_slabs(slab)
+        slabs = make_adsorbate_structures(slab, adsorbate, **make_ads_kwargs)
         return [
             slab_static_job(
                 slab_relax_job(slab, **slab_relax_kwargs)["atoms"],
