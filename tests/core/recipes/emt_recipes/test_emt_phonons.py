3ab86a0cd0b1d43eb3044bf8c689243595e8324d
Andrew Rosen <asrosen93@gmail.com>
2023-11-08 00:52:55 -0500

    Add tests for phonons

--- tests/core/recipes/emt_recipes/test_emt_phonons.py
+++ tests/core/recipes/emt_recipes/test_emt_phonons.py
@@ -1,6 +1,8 @@
 import pytest
 from ase.build import bulk
 
+from quacc.recipes.emt.core import relax_job
+
 pytest.importorskip("phonopy")
 
 from quacc.recipes.emt.phonons import phonon_flow
@@ -10,4 +12,12 @@ def test_phonon_flow(tmpdir):
     tmpdir.chdir()
     atoms = bulk("Cu")
     output = phonon_flow(atoms)
+    assert output["results"]["thermal_properties"]["temperatures"].shape == (101,)
+
+
+def test_phonon_flow_multistep(tmpdir):
+    tmpdir.chdir()
+    atoms = bulk("Cu")
+    relaxed = relax_job(atoms)
+    output = phonon_flow(relaxed["atoms"])
     assert output["results"]["thermal_properties"]["temperatures"].shape == (101,)
--- tests/parsl/test_parsl_recipes.py
+++ tests/parsl/test_parsl_recipes.py
@@ -3,7 +3,7 @@ import contextlib
 import pytest
 from ase.build import bulk
 
-parsl = pytest.importorskip("parsl")
+from quacc.recipes.emt.core import relax_job
 
 
 def setup_module():
@@ -22,6 +22,19 @@ def test_phonon_flow(tmpdir):
     tmpdir.chdir()
     atoms = bulk("Cu")
     output = phonon_flow(atoms)
+    assert output.result()["results"]["thermal_properties"]["temperatures"].shape == (
+        101,
+    )
+
+
+def test_phonon_flow_multistep(tmpdir):
+    pytest.importorskip("phonopy")
+    from quacc.recipes.emt.phonons import phonon_flow
+
+    tmpdir.chdir()
+    atoms = bulk("Cu")
+    relaxed = relax_job(atoms)
+    output = phonon_flow(relaxed["atoms"])
     assert output.result()["results"]["thermal_properties"]["temperatures"].shape == (
         101,
     )
