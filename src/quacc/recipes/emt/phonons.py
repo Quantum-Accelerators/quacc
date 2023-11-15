b6dbb6d1d2b160667d551b005727944fda87c2cf
Andrew S. Rosen <asrosen93@gmail.com>
2023-11-15 14:16:22 -0800

    Update phonons.py

--- src/quacc/recipes/emt/phonons.py
+++ src/quacc/recipes/emt/phonons.py
@@ -21,7 +21,7 @@ if TYPE_CHECKING:
 def phonon_flow(
     atoms: Atoms,
     supercell_matrix: ArrayLike = ((2, 0, 0), (0, 2, 0), (0, 0, 2)),
-    atom_disp: float = 0.015,
+    atom_disp: float = 0.01,
     t_step: float = 10,
     t_min: float = 0,
     t_max: float = 1000,
