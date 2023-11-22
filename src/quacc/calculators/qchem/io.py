72c08293d051a60a59117221b23a5691256b830c
Andrew S. Rosen <asrosen93@gmail.com>
2023-11-21 23:51:51 -0800

    Make units on Q-Chem Hessian ASE-compatible (#1237)
    - [x] I have read the [Developer
    Guide](https://quantum-accelerators.github.io/quacc/dev/contributing.html).
    Don't lie! 😉
    - [x] My PR is on a custom branch and is _not_ named `main`.
    - [ ] I have added relevant unit tests. Note: Your PR will likely not be
    merged without proper tests.
    
    Closes #1120.
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- src/quacc/calculators/qchem/io.py
+++ src/quacc/calculators/qchem/io.py
@@ -15,24 +15,11 @@ from pymatgen.io.qchem.outputs import QCOutput
 from pymatgen.io.qchem.sets import QChemDictSet
 
 if TYPE_CHECKING:
-    from typing import Any, Literal, TypedDict
+    from typing import Any, Literal
 
     from ase import Atoms
-    from numpy.typing import NDArray
 
-    class Results(TypedDict, total=False):
-        energy: float  # electronic energy in eV
-        forces: NDArray  # forces in eV/A
-        hessian: NDArray  # Hessian in native Q-Chem units
-        enthalpy: float  # total enthalpy in eV
-        entropy: float  # total entropy in eV/K
-        qc_output: dict[
-            str, Any
-        ]  # Output from `pymatgen.io.qchem.outputs.QCOutput.data`
-        qc_input: dict[
-            str, Any
-        ]  # Input from `pymatgen.io.qchem.inputs.QCInput.as_dict()`
-        custodian: dict[str, Any]  # custodian.json file metadata
+    from quacc.calculators.qchem.qchem import Results
 
 
 def write_qchem(
@@ -169,11 +156,11 @@ def read_qchem(directory: Path | str = ".") -> tuple[Results, list[float]]:
             struct.unpack("d", binary[ii * 8 : (ii + 1) * 8])[0]
             for ii in range(len(binary) // 8)
         )
-
-        results["hessian"] = np.reshape(
+        reshaped_hess = np.reshape(
             np.array(tmp_hess_data),
             (len(qc_output["species"]) * 3, len(qc_output["species"]) * 3),
         )
+        results["hessian"] = reshaped_hess * (units.Hartree / units.Bohr**2)
         results["enthalpy"] = qc_output["total_enthalpy"] * (units.kcal / units.mol)
         results["entropy"] = qc_output["total_entropy"] * (
             0.001 * units.kcal / units.mol
