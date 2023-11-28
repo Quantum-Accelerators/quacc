fab50c28330191dd8623311df1362950e3dfc7cd
Andrew Rosen <asrosen93@gmail.com>
2023-11-28 15:10:47 -0800

    fix

---
+++ tests/local/recipes/tblite_recipes/test_tblite_phonons.py
@@ -0,0 +1,15 @@
+import pytest
+from ase.build import bulk
+
+from quacc.recipes.tblite.phonons import phonon_flow
+
+pytest.importorskip("tblite.ase")
+pytest.importorskip("phonopy")
+
+
+def test_phonon_flow(tmpdir):
+    tmpdir.chdir()
+    atoms = bulk("Cu")
+    output = phonon_flow(atoms, static_job_kwargs={"method": "GFN1-xTB"})
+    assert output["results"]["force_constants"].shape == (8, 8, 3, 3)
+    assert len(output["results"]["thermal_properties"]["temperatures"]) == 101
