28817ad4854a67c0a39e9b673199d6fcb01b2fee
Andrew Rosen <asrosen93@gmail.com>
2023-12-03 20:53:20 -0800

    fix

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -6,12 +6,18 @@ conda activate quacc
 pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/master/ase-master.zip
 pip install -e .[dev]
 
-# Gaussian, ORCA, GULP, VASP
+# Gaussian, ORCA, GULP
 module load gaussian/g16
 module load openmpi/gcc/4.1.2
 salloc -N 1 -n 32 -t 00:10:00 pytest \
     tests/local/recipes/gaussian_recipes \
     tests/local/recipes/gulp_recipes \
     tests/local/recipes/orca_recipes \
-    tests/local/recipes/vasp_recipes/jenkins \
     --noconftest
+
+# VASP
+module purge
+module load intel/2021.1.2
+module load intel-mpi/intel/2021.3.1
+module load hdf5/intel-2021.1/1.10.6
+salloc -N 1 -n 32 -t 00:10:00 pytest  --noconftest
