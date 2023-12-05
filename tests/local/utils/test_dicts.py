3d96eb51d86cc19e6fd3cdfa46d563979a7a3c79
Archana Saroj <archanasaroj97@gmail.com>
2023-12-06 00:51:53 +0530

    Added additional test cases

--- tests/local/utils/test_dicts.py
+++ tests/local/utils/test_dicts.py
@@ -1,6 +1,5 @@
 from quacc.utils.dicts import merge_dicts, remove_dict_nones
 
-
 def test_remove_dict_nones():
     d = {
         "output": {
@@ -19,6 +18,15 @@ def test_remove_dict_nones():
         "output": {"output": {"test": [1, None], "test2": 1, "test4": {}, "test5": []}}
     }
 
+    # Additional test case 1: Nested dictionaries with None values
+    d2 = {"a": None, "b": {"c": None, "d": 1}}
+    d2 = remove_dict_nones(d2)
+    assert d2 == {"b": {"d": 1}}
+
+    # Additional test case 2: Empty dictionaries at different levels
+    d3 = {"x": {}, "y": {"z": {}}}
+    d3 = remove_dict_nones(d3)
+    assert d3 == {}
 
 def test_merge_dicts():
     defaults = {"a": 1, "b": {"a": 1, "b": 2}}
@@ -28,3 +36,13 @@ def test_merge_dicts():
         "b": {"a": 1, "b": 3, "d": 1},
         "c": 3,
     }
+
+    # Additional test case 1: Merging dictionaries with missing keys
+    merge_defaults = {"x": 10, "y": 20}
+    merge_calc_swaps = {"y": 30, "z": 40}
+    assert merge_dicts(merge_defaults, merge_calc_swaps) == {"x": 10, "y": 30, "z": 40}
+
+    # Additional test case 2: Merging empty dictionaries
+    empty_dict1 = {}
+    empty_dict2 = {}
+    assert merge_dicts(empty_dict1, empty_dict2) == {}
