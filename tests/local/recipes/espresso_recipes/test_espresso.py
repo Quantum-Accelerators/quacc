09145b4b20632d22b6b73b8475994d66e527d6a6
TomD <T.Demeyere@soton.ac.uk>
2023-12-06 15:51:45 +0000

    second step to testing

--- tests/local/recipes/espresso_recipes/test_espresso.py
+++ tests/local/recipes/espresso_recipes/test_espresso.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+from pathlib import Path
+
+import numpy as np
 from ase.build import bulk
 
 from quacc import SETTINGS
@@ -9,51 +14,72 @@ DEFAULT_SETTINGS = SETTINGS.model_copy()
 def test_static_job(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
 
-    atoms = bulk('Si')
+    atoms = bulk("Si")
 
     input_data = {
-        'occupations': 'smearing',
-        'smearing': 'gaussian',
-        'degauss': 0.005,
-        'mixing_mode': 'plain',
-        'mixing_beta': 0.6,
+        "occupations": "smearing",
+        "smearing": "gaussian",
+        "degauss": 0.005,
+        "mixing_mode": "plain",
+        "mixing_beta": 0.6,
+        "pseudo_dir": Path(__file__).parent,
     }
 
-    pseudopotentials = {'Si': 'Si.upf'}
-    
-    results = static_job(atoms,
-                        input_data=input_data,
-                        pseudopotentials=pseudopotentials,
-                        k_spacing=0.5)
-    
-    assert results
-    
+    pseudopotentials = {"Si": "Si.upf"}
+
+    results = static_job(
+        atoms, input_data=input_data, pseudopotentials=pseudopotentials, kspacing=0.5
+    )
+
+    assert np.allclose(results["atoms"].positions, atoms.positions, atol=1.0e-4)
+
+    assert np.allclose(results["atoms"].cell, atoms.cell, atol=1.0e-3)
+    assert (results["atoms"].symbols == atoms.symbols).all()
+    for key, value in input_data.items():
+        assert results["parameters"]["input_data"][key] == value
+
+
 def test_ph_job(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
 
-    atoms = bulk('Li')
+    atoms = bulk("Li")
+
+    SETTINGS.ESPRESSO_PP_PATH = Path(__file__).parent
 
     input_data = {
-        'calculation': 'vc-relax',
-        'occupations': 'smearing',
-        'smearing': 'cold',
-        'degauss': 0.02,
-        'mixing_mode': 'TF',
-        'mixing_beta': 0.7,
+        "calculation": "scf",
+        "occupations": "smearing",
+        "smearing": "cold",
+        "degauss": 0.02,
+        "mixing_mode": "TF",
+        "mixing_beta": 0.7,
+        "conv_thr": 1.0e-6,
     }
 
-    ph_loose = {
-        'tr2_ph': 1e-10,
-    }
+    ph_loose = {"tr2_ph": 1e-10}
 
-    pseudopotentials = {'Li': 'Li.upf'}
+    pseudopotentials = {"Li": "Li.upf"}
 
-    pw_results = static_job(atoms,
-                        input_data=input_data,
-                        pseudopotentials=pseudopotentials,
-                        k_spacing=0.25)
-    
-    ph_results = ph_job(input_data=ph_loose,
-                    copy_files=pw_results['dir_name'])
-    
-    assert ph_results
+    pw_results = static_job(
+        atoms, input_data=input_data, pseudopotentials=pseudopotentials, kspacing=0.25
+    )
+
+    ph_results = ph_job(input_data=ph_loose, copy_files=pw_results["dir_name"])
+
+    assert (0, 0, 0) in ph_results["results"]
+    assert np.allclose(
+        ph_results["results"][(0, 0, 0)]["atoms"].positions,
+        atoms.positions,
+        atol=1.0e-4,
+    )
+    # ph.x cell param are not defined to a very high level of accuracy,
+    # atol = 1.0e-3 is needed here...
+    assert np.allclose(
+        ph_results["results"][(0, 0, 0)]["atoms"].cell, atoms.cell, atol=1.0e-3
+    )
+    assert (ph_results["results"][(0, 0, 0)]["atoms"].symbols == atoms.symbols).all()
+
+    sections = ["atoms", "eqpoints", "freqs", "kpoints", "mode_symmetries", "modes"]
+
+    for key in sections:
+        assert key in ph_results["results"][(0, 0, 0)]
