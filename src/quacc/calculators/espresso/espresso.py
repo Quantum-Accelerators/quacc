4a31f4e2bd1dbb983b40da196394e88dc82684f9
Andrew Rosen <asrosen93@gmail.com>
2023-12-10 10:52:44 -0800

    Fix Path issue

--- src/quacc/calculators/espresso/espresso.py
+++ src/quacc/calculators/espresso/espresso.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from ase import Atoms
@@ -16,7 +17,6 @@ from quacc.utils.dicts import merge_dicts
 from quacc.utils.files import load_yaml_calc
 
 if TYPE_CHECKING:
-    from pathlib import Path
     from typing import Any
 
 
