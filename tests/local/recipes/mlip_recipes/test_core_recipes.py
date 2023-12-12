81dbf80eaaacc7b297c84d0dd3ec23b24f0b854b
Andrew Rosen <asrosen93@gmail.com>
2023-12-11 20:04:31 -0800

    Add tests

---
+++ tests/local/recipes/mlip_recipes/test_core_recipes.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pytest
+from ase.build import bulk
+
+from quacc.recipes.mlip.core import relax_job, static_job
+
+
+@pytest.mark.parametrize("method", ["chgnet", "m3gnet", "umace"])
+@pytest.mark.parametrize("energy", [-32.688549, -32.662731, -32.662731])
+def test_relax_job(tmp_path, monkeypatch, method, energy):
+    monkeypatch.chdir(tmp_path)
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+        pytestmark = pytest.importorskip("matcalc")
+    elif method == "umace":
+        pytestmark = pytest.importorskip("mace")
+    atoms = bulk("Cu") * (2, 2, 2)
+    atoms[0].position += 0.1
+    output = static_job(atoms, method=method)
+    assert output["results"]["energy"] == pytest.approx(energy)
+    assert np.shape(output["results"]["forces"]) == (8, 3)
+    assert np.shape(output["results"]["stress"]) == (3, 3)
+    assert output["atoms"] == atoms
+
+
+@pytest.mark.parametrize("method", ["chgnet", "m3gnet", "umace"])
+@pytest.mark.parametrize("energy", [-32.662731, -32.662731, -32.662731])
+def test_relax_job(tmp_path, monkeypatch, method, energy):
+    monkeypatch.chdir(tmp_path)
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+        pytestmark = pytest.importorskip("matcalc")
+    elif method == "umace":
+        pytestmark = pytest.importorskip("mace")
+
+    atoms = bulk("Cu") * (2, 2, 2)
+    atoms[0].position += 0.1
+    output = relax_job(atoms, method=method)
+    assert output["results"]["energy"] == pytest.approx(energy)
+    assert np.shape(output["results"]["forces"]) == (8, 3)
+    assert np.shape(output["results"]["stress"]) == (3, 3)
+    assert output["atoms"] != atoms
