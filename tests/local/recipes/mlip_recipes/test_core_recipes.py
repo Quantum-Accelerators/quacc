f7fc955c16b0846ab85c7916cd08c14221b1d602
Andrew Rosen <asrosen93@gmail.com>
2023-12-11 22:47:53 -0800

    Patch test

--- tests/local/recipes/mlip_recipes/test_core_recipes.py
+++ tests/local/recipes/mlip_recipes/test_core_recipes.py
@@ -7,7 +7,7 @@ from quacc.recipes.mlip.core import relax_job, static_job
 
 def test_bad_method():
     atoms = bulk("Cu")
-    pytestmark = pytest.importorskiip("chgnet")
+    pytestmark = pytest.importorskip("chgnet")
     with pytest.raises(ValueError):
         static_job(atoms, method="bad_method")
 
@@ -56,3 +56,29 @@ def test_relax_job(tmp_path, monkeypatch, method):
     assert output["results"]["energy"] == pytest.approx(ref_energy[method])
     assert np.shape(output["results"]["forces"]) == (8, 3)
     assert output["atoms"] != atoms
+    assert output["atoms"].get_volume() == pytest.approx(atoms.get_volume())
+
+
+@pytest.mark.parametrize("method", ["chgnet", "m3gnet", "umace"])
+def test_relax_cell_job(tmp_path, monkeypatch, method):
+    monkeypatch.chdir(tmp_path)
+    ref_energy = {
+        "chgnet": -32.664215087890625,
+        "m3gnet": -32.74749,
+        "umace": -32.67439270019531,
+    }
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+        pytestmark = pytest.importorskip("matcalc")
+    elif method == "umace":
+        pytestmark = pytest.importorskip("mace")
+
+    atoms = bulk("Cu") * (2, 2, 2)
+    atoms[0].position += 0.1
+    output = relax_job(atoms, method=method, relax_cell=True)
+    assert output["results"]["energy"] == pytest.approx(ref_energy[method])
+    assert np.shape(output["results"]["forces"]) == (8, 3)
+    assert output["atoms"] != atoms
+    assert output["atoms"].get_volume() != pytest.approx(atoms.get_volume())
