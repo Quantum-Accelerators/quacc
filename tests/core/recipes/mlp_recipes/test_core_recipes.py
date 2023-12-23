eedc02d1d48366eab1344b3c5b45a63656f3657a
Andrew S. Rosen <asrosen93@gmail.com>
2023-12-22 21:30:40 -0800

    Update test_core_recipes.py

--- tests/core/recipes/mlp_recipes/test_core_recipes.py
+++ tests/core/recipes/mlp_recipes/test_core_recipes.py
@@ -15,12 +15,21 @@ def test_bad_method():
 
 
 def set_default_dtype(method):
-    if method != "mace":
-        torch.set_default_dtype(getattr(torch, "float32", "float32"))
+    if method == "m3gnet":
+        from matgl import set_default_dtype
+
+        set_default_dtype()
 
 
 @pytest.mark.parametrize("method", ["chgnet", "m3gnet", "mace"])
 def test_static_job(tmp_path, monkeypatch, method):
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+    elif method == "mace":
+        pytestmark = pytest.importorskip("mace")
+
     monkeypatch.chdir(tmp_path)
     set_default_dtype(method)
 
@@ -29,12 +38,6 @@ def test_static_job(tmp_path, monkeypatch, method):
         "m3gnet": -4.0938973,
         "mace": -4.083906650543213,
     }
-    if method == "chgnet":
-        pytestmark = pytest.importorskip("chgnet")
-    elif method == "m3gnet":
-        pytestmark = pytest.importorskip("matgl")
-    elif method == "mace":
-        pytestmark = pytest.importorskip("mace")
     atoms = bulk("Cu")
     output = static_job(atoms, method=method)
     assert output["results"]["energy"] == pytest.approx(ref_energy[method])
@@ -44,6 +47,13 @@ def test_static_job(tmp_path, monkeypatch, method):
 
 @pytest.mark.parametrize("method", ["chgnet", "m3gnet", "mace"])
 def test_relax_job(tmp_path, monkeypatch, method):
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+    elif method == "mace":
+        pytestmark = pytest.importorskip("mace")
+
     monkeypatch.chdir(tmp_path)
     set_default_dtype(method)
 
@@ -52,12 +62,6 @@ def test_relax_job(tmp_path, monkeypatch, method):
         "m3gnet": -32.749088287353516,
         "mace": -32.670471191406259,
     }
-    if method == "chgnet":
-        pytestmark = pytest.importorskip("chgnet")
-    elif method == "m3gnet":
-        pytestmark = pytest.importorskip("matgl")
-    elif method == "mace":
-        pytestmark = pytest.importorskip("mace")
 
     atoms = bulk("Cu") * (2, 2, 2)
     atoms[0].position += 0.1
@@ -70,6 +74,13 @@ def test_relax_job(tmp_path, monkeypatch, method):
 
 @pytest.mark.parametrize("method", ["chgnet", "m3gnet", "mace"])
 def test_relax_cell_job(tmp_path, monkeypatch, method):
+    if method == "chgnet":
+        pytestmark = pytest.importorskip("chgnet")
+    elif method == "m3gnet":
+        pytestmark = pytest.importorskip("matgl")
+    elif method == "mace":
+        pytestmark = pytest.importorskip("mace")
+
     monkeypatch.chdir(tmp_path)
     set_default_dtype(method)
 
@@ -78,12 +89,6 @@ def test_relax_cell_job(tmp_path, monkeypatch, method):
         "m3gnet": -32.74995040893555,
         "mace": -32.67771911621094,
     }
-    if method == "chgnet":
-        pytestmark = pytest.importorskip("chgnet")
-    elif method == "m3gnet":
-        pytestmark = pytest.importorskip("matgl")
-    elif method == "mace":
-        pytestmark = pytest.importorskip("mace")
 
     atoms = bulk("Cu") * (2, 2, 2)
     atoms[0].position += 0.1
