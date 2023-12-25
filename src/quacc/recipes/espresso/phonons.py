dd347f72db02d52babc75fb2c8e5f19d078ff46c
Andrew Rosen <asrosen93@gmail.com>
2023-12-25 10:33:28 -0800

    Implement minimal working example

--- src/quacc/recipes/espresso/phonons.py
+++ src/quacc/recipes/espresso/phonons.py
@@ -1,6 +1,7 @@
-"""Core recipes for espresso."""
+"""Phonon recipes for espresso."""
 from __future__ import annotations
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import numpy as np
@@ -19,10 +20,9 @@ if TYPE_CHECKING:
 
 @job
 def phonon_job(
+    prev_dir: str | Path,
     preset: str | None = None,
-    copy_files: str | list[str] | None = None,
     parallel_info: dict[str] | None = None,
-    test_run: bool = False,
     **calc_kwargs,
 ) -> RunSchema:
     """
@@ -30,28 +30,16 @@ def phonon_job(
 
     Parameters
     ----------
+    prev_dir
+        Previous directory where pw.x was run. This is used to copy the entire tree structure
+        of that directory to the working directory of this calculation.
     preset
         The name of a YAML file containing a list of parameters to use as
         a "preset" for the calculator. quacc will automatically look in the
         `ESPRESSO_PRESET_DIR` (default: quacc/calculators/espresso/presets).
-    copy_files
-        List of files to copy to the calculation directory. Almost always needed
-        for ph.x calculations. This parameter can either be a string or a list of
-        strings.
-
-        If a string is provided, it is assumed to be a path to a directory,
-        all of the child tree structure of that directory is going to be copied to the
-        scratch of this calculation. For phonon_job this is what most users will want to do.
-
-        If a list of strings is provided, each string point to a specific file. In this case
-        it is important to note that no directory structure is going to be copied, everything
-        is copied at the root of the temporary directory.
     parallel_info
         Dictionary containing information about the parallelization of the
         calculation. See the ASE documentation for more information.
-    test_run
-        If True, a test run is performed to check that the calculation input_data is correct or
-        to generate some files/info if needed.
     **calc_kwargs
         calc_kwargs dictionary possibly containing the following keys:
 
@@ -79,28 +67,24 @@ def phonon_job(
         "qpts": (0, 0, 0),
     }
 
-    template = EspressoTemplate("ph", test_run=test_run)
-
     return base_fn(
         preset=preset,
-        template=template,
+        template=EspressoTemplate("ph"),
         calc_defaults=calc_defaults,
         calc_swaps=calc_kwargs,
         parallel_info=parallel_info,
         additional_fields={"name": "ph.x Phonon"},
-        copy_files=copy_files,
+        copy_files=prev_dir,
     )
 
 
 @flow
 def grid_phonon_flow(
     atoms: Atoms,
-    preset: str | None = None,
-    parallel_info: dict[str] | None = None,
     custom_pw_job: Job | None = None,
     custom_phonon_job: Job | None = None,
     nblocks: int = 1,
-):
+) -> RunSchema:
     """
     Function to carry out a grid parallelization of a ph.x calculation. Each representation of each
     q-point is calculated in a separate job. This allow the calculations to be run on different machines
@@ -116,13 +100,6 @@ def grid_phonon_flow(
     ----------
     atoms
         Atoms object
-    preset
-        The name of a YAML file containing a list of parameters to use as
-        a "preset" for the calculator. quacc will automatically look in the
-        `ESPRESSO_PRESET_DIR` (default: quacc/calculators/espresso/presets).
-    parallel_info
-        Dictionary containing information about the parallelization of the
-        calculation. See the ASE documentation for more information.
     custom_pw_job
         A custom job to run the pw.x calculation. This job should return a RunSchema dictionary.
         The default job is [quacc.recipes.espresso.core.static_job][].
@@ -140,21 +117,41 @@ def grid_phonon_flow(
         Dictionary of results from [quacc.schemas.ase.summarize_run][]
     """
 
-    # We run a first pw job (single point or relax) depending on the input
     pw_job = static_job if custom_pw_job is None else custom_pw_job
     ph_job = phonon_job if custom_phonon_job is None else custom_phonon_job
 
-    pw_job_results = pw_job(atoms, preset=preset, parallel_info=parallel_info)
-    ph_test_job_results = ph_job(test_run=True, copy_files=pw_job_results["dir_name"])
+    # We run a first pw job (single point or relax) depending on the input
+    # ASR: Where is the relax job??
+    pw_job_results = pw_job(atoms)
 
-    # We parse the patterns
-    ph_patterns = parse_ph_patterns(ph_test_job_results["dir_name"])
+    # Run the phonon subflow
+    grid_results = _phonon_subflow(ph_job, pw_job_results["dir_name"], nblocks=nblocks)
 
-    # We need to get the input_data from the ph job that was sent in...
-    input_data = ph_test_job_results["parameters"]["input_data"]
+    # We have to copy back the files from all of the jobs
+    copy_back = [result["dir_name"] for result in grid_results]
 
+    # We run a 'recover' ph job to diagonalize the dynamical matrix
+    # for each q-point.
+    input_data = grid_results[-1]["parameters"]["input_data"]
+    for k in ["start_q", "last_q", "start_irr", "last_irr"]:
+        input_data["inputph"].pop(k)
+    input_data["inputph"]["recover"] = True
+
+    return ph_job(copy_back, input_data=input_data)
+
+
+@subflow
+def _phonon_subflow(
+    ph_job: Job, pw_job_results_dir: str | Path, nblocks: int = 1
+) -> list[RunSchema]:
     # We loop over the q-points and representations and run a ph job for each
     # of them
+
+    # Run a test phonon job
+    ph_test_job_results = ph_job.__wrapped__(pw_job_results_dir, test_run=True)
+    input_data = ph_test_job_results["parameters"]["input_data"]
+    ph_patterns = parse_ph_patterns(ph_test_job_results["dir_name"])
+
     grid_results = []
     for pattern in ph_patterns:
         input_data["inputph"]["start_q"] = pattern
@@ -167,20 +164,6 @@ def grid_phonon_flow(
         for representation in repr_to_do:
             input_data["inputph"]["start_irr"] = representation[0]
             input_data["inputph"]["last_irr"] = representation[-1]
-            ph_job_results = ph_job(
-                input_data=input_data, copy_files=pw_job_results["dir_name"]
-            )
+            ph_job_results = ph_job(pw_job_results_dir, input_data=input_data)
             grid_results.append(ph_job_results)
-
-    # We have to copy back the files from all of the jobs
-    copy_back = [result["dir_name"] for result in grid_results]
-    # We run a 'recover' ph job to diagonalize the dynamical matrix
-    # for each q-point.
-    input_data["inputph"].pop("start_q")
-    input_data["inputph"].pop("last_q")
-    input_data["inputph"].pop("start_irr")
-    input_data["inputph"].pop("last_irr")
-
-    input_data["inputph"]["recover"] = True
-
-    return ph_job(input_data=input_data, copy_files=copy_back)
+    return grid_results
