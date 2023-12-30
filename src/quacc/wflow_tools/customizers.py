857b95530925d1f53f1dd14b4e29b3e6e05d1059
Andrew Rosen <asrosen93@gmail.com>
2023-12-29 17:04:20 -0800

    fix

--- src/quacc/wflow_tools/customizers.py
+++ src/quacc/wflow_tools/customizers.py
@@ -49,7 +49,7 @@ def strip_decorator(func: Callable) -> Callable:
         if isinstance(func, Delayed):
             return func.__wrapped__
     elif SETTINGS.WORKFLOW_ENGINE == "jobflow":
-        from jobflow import JobflowJob
+        from jobflow import Job as JobflowJob
 
         if isinstance(func, JobflowJob):
             return func.original
