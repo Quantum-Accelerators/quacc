19a4fc9d6aa1152394e8d7e51d66ca2fcbe36b36
TomD <T.Demeyere@soton.ac.uk>
2023-12-30 15:11:01 +0100

    does these tests make sense still?

--- tests/core/calculators/espresso/test_io.py
+++ tests/core/calculators/espresso/test_io.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from quacc.calculators.espresso.io import read_espresso_ph
+from ase.io.espresso import read_espresso_ph
 
 RUN_PATH = Path(__file__).parent / "test_runs"
 
