f181601d17aa9833d30a658a8772503db8a6d223
TomD <T.Demeyere@soton.ac.uk>
2024-01-01 22:54:37 +0100

    yea...

--- src/quacc/recipes/espresso/_base.py
+++ src/quacc/recipes/espresso/_base.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from ase import Atoms
+from ase.io.espresso import Namelist
 
 from quacc.calculators.espresso.espresso import (
     Espresso,
@@ -13,7 +14,6 @@ from quacc.calculators.espresso.espresso import (
 from quacc.runners.ase import run_calc
 from quacc.schemas.ase import summarize_run
 from quacc.utils.dicts import recursive_dict_merge
-from ase.io.espresso import Namelist
 
 if TYPE_CHECKING:
     from typing import Any
@@ -66,13 +66,13 @@ def base_fn(
 
     atoms = Atoms() if atoms is None else atoms
 
-    calc_defaults = Namelist(calc_defaults)
-    calc_swaps = Namelist(calc_swaps)
+    calc_defaults["input_data"] = Namelist(calc_defaults.get("input_data"))
+    calc_swaps["input_data"] = Namelist(calc_swaps.get("input_data"))
 
     binary = template.binary if template else "pw"
 
-    calc_defaults.to_nested(binary = binary)
-    calc_swaps.to_nested(binary = binary)
+    calc_defaults["input_data"].to_nested(binary=binary, **calc_defaults)
+    calc_swaps["input_data"].to_nested(binary=binary, **calc_swaps)
 
     calc_flags = recursive_dict_merge(calc_defaults, calc_swaps, allowed_nones=["kpts"])
 
