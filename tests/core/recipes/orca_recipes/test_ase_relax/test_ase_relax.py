e710d0a46f4965938251b16bc8ead96180fbb859
Andrew Rosen <asrosen93@gmail.com>
2024-01-03 12:20:08 -0800

    Fix test suite

--- tests/core/recipes/orca_recipes/test_ase_relax/test_ase_relax.py
+++ tests/core/recipes/orca_recipes/test_ase_relax/test_ase_relax.py
@@ -1,6 +1,16 @@
+import os
+from shutil import which
+
+import pytest
 from ase.build import molecule
 
+from quacc import SETTINGS
 from quacc.recipes.orca.core import ase_relax_job
+
+orca_path = which(SETTINGS.ORCA_CMD)
+
+has_orca = bool(orca_path and os.path.getsize(orca_path) > 1024 * 1024)
+pytestmark = pytest.mark.skipif(not has_orca, reason="Needs ORCA")
 
 
 def test_ase_relax_job(tmp_path, monkeypatch):
