2993a08e7be1efe3dbf06c5d42409122c6d463d3
Andrew Rosen <asrosen93@gmail.com>
2024-01-03 18:11:40 -0800

    fix

--- tests/core/recipes/gulp_recipes/conftest.py
+++ tests/core/recipes/gulp_recipes/conftest.py
@@ -8,52 +8,71 @@ import pytest
 from ase import Atoms
 from ase.calculators.gulp import GULP
 
-from quacc import SETTINGS
-
 FILE_DIR = Path(__file__).parent
 GULP_DIR = os.path.join(FILE_DIR, "gulp_run")
-has_gulp = bool(which(SETTINGS.GULP_CMD))
-has_gulp_lib = (SETTINGS.GULP_LIB and SETTINGS.GULP_LIB.is_dir()) or (
-    os.environ.get("GULP_LIB") and Path(os.environ.get("GULP_LIB")).is_dir()
-)
 
-if not has_gulp and not has_gulp_lib:
 
-    @pytest.fixture(autouse=True)
-    def mock_settings_env_vars():
+@pytest.fixture(autouse=True)
+def mock_settings_env_vars():
+    from quacc import SETTINGS
+
+    has_gulp = bool(which(str(SETTINGS.GULP_CMD)))
+    has_gulp_lib = bool(
+        (SETTINGS.GULP_LIB and SETTINGS.GULP_LIB.is_dir())
+        or (os.environ.get("GULP_LIB") and Path(os.environ.get("GULP_LIB")).is_dir())
+    )
+
+    if not has_gulp and not has_gulp_lib:
         with mock.patch.dict(os.environ, {"GULP_LIB": "."}):
             yield
 
-    def mock_get_potential_energy(self, **kwargs):
-        # Instead of running .get_potential_energy(), we mock it by attaching
-        # dummy results to the atoms object and returning a fake energy. This
-        # works because the real atoms.get_potential_energy() takes one argument
-        # (i.e. self) and that self object is the atoms object.
-        e = -1.0
-        self.calc.results = {
-            "energy": e,
-            "forces": np.array([[0.0, 0.0, 0.0]] * len(self)),
-        }
-        for f in os.listdir(GULP_DIR):
-            copy(os.path.join(GULP_DIR, f), f)
-        return e
 
-    @pytest.fixture(autouse=True)
-    def patch_get_potential_energy(monkeypatch):
-        # Monkeypatch the .get_potential_energy() method of the Atoms object so
-        # we aren't running the actual calculation during testing.
+def mock_get_potential_energy(self, **kwargs):
+    # Instead of running .get_potential_energy(), we mock it by attaching
+    # dummy results to the atoms object and returning a fake energy. This
+    # works because the real atoms.get_potential_energy() takes one argument
+    # (i.e. self) and that self object is the atoms object.
+    e = -1.0
+    self.calc.results = {"energy": e, "forces": np.array([[0.0, 0.0, 0.0]] * len(self))}
+    for f in os.listdir(GULP_DIR):
+        copy(os.path.join(GULP_DIR, f), f)
+    return e
+
+
+@pytest.fixture(autouse=True)
+def patch_get_potential_energy(monkeypatch):
+    # Monkeypatch the .get_potential_energy() method of the Atoms object so
+    # we aren't running the actual calculation during testing.
+    from quacc import SETTINGS
+
+    has_gulp = bool(which(str(SETTINGS.GULP_CMD)))
+    has_gulp_lib = bool(
+        (SETTINGS.GULP_LIB and SETTINGS.GULP_LIB.is_dir())
+        or (os.environ.get("GULP_LIB") and Path(os.environ.get("GULP_LIB")).is_dir())
+    )
+    if not has_gulp and not has_gulp_lib:
         monkeypatch.setattr(Atoms, "get_potential_energy", mock_get_potential_energy)
 
-    def mock_get_opt_state(self, **kwargs):
-        # Instead of running GULP and getting the opt state, we'll just mock
-        # it as true
 
-        self.get_opt_state = True
+def mock_get_opt_state(self, **kwargs):
+    # Instead of running GULP and getting the opt state, we'll just mock
+    # it as true
 
-        return True
+    self.get_opt_state = True
 
-    @pytest.fixture(autouse=True)
-    def patch_get_opt_state(monkeypatch):
-        # Monkeypatch the .get_opt_state() method of the GULP calculator object so
-        # we aren't fetching the actual state
+    return True
+
+
+@pytest.fixture(autouse=True)
+def patch_get_opt_state(monkeypatch):
+    # Monkeypatch the .get_opt_state() method of the GULP calculator object so
+    # we aren't fetching the actual state
+    from quacc import SETTINGS
+
+    has_gulp = bool(which(str(SETTINGS.GULP_CMD)))
+    has_gulp_lib = bool(
+        (SETTINGS.GULP_LIB and SETTINGS.GULP_LIB.is_dir())
+        or (os.environ.get("GULP_LIB") and Path(os.environ.get("GULP_LIB")).is_dir())
+    )
+    if not has_gulp and not has_gulp_lib:
         monkeypatch.setattr(GULP, "get_opt_state", mock_get_opt_state)
--- tests/core/recipes/orca_recipes/conftest.py
+++ tests/core/recipes/orca_recipes/conftest.py
@@ -6,32 +6,30 @@ import numpy as np
 import pytest
 from ase import Atoms
 
-from quacc import SETTINGS
-
 FILE_DIR = Path(__file__).parent
 ORCA_DIR = os.path.join(FILE_DIR, "orca_run")
 
-orca_path = which(SETTINGS.ORCA_CMD)
 
-has_orca = bool(orca_path and os.path.getsize(orca_path) > 1024 * 1024)
-if not has_orca:
+def mock_get_potential_energy(self, **kwargs):
+    # Instead of running .get_potential_energy(), we mock it by attaching
+    # dummy results to the atoms object and returning a fake energy. This
+    # works because the real atoms.get_potential_energy() takes one argument
+    # (i.e. self) and that self object is the atoms object.
+    e = -1.0
+    self.calc.results = {"energy": e, "forces": np.array([[0.0, 0.0, 0.0]] * len(self))}
+    for f in os.listdir(ORCA_DIR):
+        copy(os.path.join(ORCA_DIR, f), f)
+    return e
 
-    def mock_get_potential_energy(self, **kwargs):
-        # Instead of running .get_potential_energy(), we mock it by attaching
-        # dummy results to the atoms object and returning a fake energy. This
-        # works because the real atoms.get_potential_energy() takes one argument
-        # (i.e. self) and that self object is the atoms object.
-        e = -1.0
-        self.calc.results = {
-            "energy": e,
-            "forces": np.array([[0.0, 0.0, 0.0]] * len(self)),
-        }
-        for f in os.listdir(ORCA_DIR):
-            copy(os.path.join(ORCA_DIR, f), f)
-        return e
 
-    @pytest.fixture(autouse=True)
-    def patch_get_potential_energy(monkeypatch):
-        # Monkeypatch the .get_potential_energy() method of the Atoms object so
-        # we aren't running the actual calculation during testing.
+@pytest.fixture(autouse=True)
+def patch_get_potential_energy(monkeypatch):
+    # Monkeypatch the .get_potential_energy() method of the Atoms object so
+    # we aren't running the actual calculation during testing.
+    from quacc import SETTINGS
+
+    orca_path = which(str(SETTINGS.ORCA_CMD))
+
+    has_orca = bool(orca_path and os.path.getsize(orca_path) > 1024 * 1024)
+    if not has_orca:
         monkeypatch.setattr(Atoms, "get_potential_energy", mock_get_potential_energy)
