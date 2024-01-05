14219ed7c451d5e6da501aa3a951554fa58ddf9e
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-04 19:58:08 -0800

    Remove `.compute()` in Dask subflow (#1471)
    ## Summary of Changes
    
    Closes #1470.
    
    ## Requirements
    
    ### Checklist
    
    - [X] I have read the [Contributing
    Guide](https://quantum-accelerators.github.io/quacc/dev/contributing.html).
    Don't lie! 😉
    - [X] My PR is on a custom branch and is _not_ named `main`.
    - [X] I have used `black`, `isort`, and `ruff` as described in the
    [style
    guide](https://quantum-accelerators.github.io/quacc/dev/contributing.html#style).
    - [X] I have added relevant, comprehensive [unit
    tests](https://quantum-accelerators.github.io/quacc/dev/contributing.html#unit-tests).
    
    ### Notes
    
    - Your PR will likely not be merged without proper and thorough tests.
    - If you are an external contributor, you will see a comment from
    [@buildbot-princeton](https://github.com/buildbot-princeton). This is
    solely for the maintainers.
    - When your code is ready for review, ping one of the [active
    maintainers](https://quantum-accelerators.github.io/quacc/about/contributors.html#active-maintainers).
    
    ---------
    
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- tests/dask/test_dask_syntax.py
+++ tests/dask/test_dask_syntax.py
@@ -28,58 +28,23 @@ def test_dask_decorators(tmp_path, monkeypatch):
         return [val] * 3
 
     @subflow
-    def add_distributed(vals, c):
-        return [add(val, c) for val in vals]
+    def add_distributed(vals, c, op):
+        return [op(val, c) for val in vals]
 
     @flow
     def workflow(a, b, c):
         return mult(add(a, b), c)
 
     @flow
-    def dynamic_workflow(a, b, c):
+    def dynamic_workflow(a, b, c, op):
         result1 = add(a, b)
         result2 = make_more(result1)
-        return add_distributed(result2, c)
+        return add_distributed(result2, c, op)
 
     assert client.compute(add(1, 2)).result() == 3
     assert client.compute(mult(1, 2)).result() == 2
     assert client.compute(workflow(1, 2, 3)).result() == 9
-    assert dask.compute(*client.gather(dynamic_workflow(1, 2, 3))) == (6, 6, 6)
-
-
-def test_dask_decorators_args(tmp_path, monkeypatch):
-    monkeypatch.chdir(tmp_path)
-
-    @job()
-    def add(a, b):
-        return a + b
-
-    @job()
-    def mult(a, b):
-        return a * b
-
-    @job()
-    def make_more(val):
-        return [val] * 3
-
-    @subflow()
-    def add_distributed(vals, c):
-        return [add(val, c) for val in vals]
-
-    @flow()
-    def workflow(a, b, c):
-        return mult(add(a, b), c)
-
-    @flow()
-    def dynamic_workflow(a, b, c):
-        result1 = add(a, b)
-        result2 = make_more(result1)
-        return add_distributed(result2, c)
-
-    assert client.compute(add(1, 2)).result() == 3
-    assert client.compute(mult(1, 2)).result() == 2
-    assert client.compute(workflow(1, 2, 3)).result() == 9
-    assert dask.compute(*client.gather(dynamic_workflow(1, 2, 3))) == (6, 6, 6)
+    assert client.gather(client.compute(dynamic_workflow(1, 2, 3, add))) == [6, 6, 6]
 
 
 def test_strip_decorators():
