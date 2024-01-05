9e533398de00b5ef44ca787fe95d691afe4d8a8c
Andrew Rosen <asrosen93@gmail.com>
2024-01-05 15:44:57 -0800

    fix

--- src/quacc/wflow_tools/customizers.py
+++ src/quacc/wflow_tools/customizers.py
@@ -111,16 +111,16 @@ def update_parameters(
     """
     from quacc import SETTINGS, flow, job, subflow
 
-    if decorator == "job":
-        decorator = job
-    elif decorator == "flow":
-        decorator = flow
-    elif decorator == "subflow":
-        decorator = subflow
-    else:
-        decorator = None
-
     if decorator and SETTINGS.WORKFLOW_ENGINE == "dask":
+        if decorator == "job":
+            decorator = job
+        elif decorator == "flow":
+            decorator = flow
+        elif decorator == "subflow":
+            decorator = subflow
+        else:
+            decorator = None
+
         func = strip_decorator(func)
         return decorator(partial(func, **params))
 
