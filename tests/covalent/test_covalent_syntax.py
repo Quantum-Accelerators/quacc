2453b172f332fa2a004864f860a3ded14fb8900c
Andrew Rosen <asrosen93@gmail.com>
2024-01-05 14:43:09 -0800

    add more tets

--- tests/covalent/test_covalent_syntax.py
+++ tests/covalent/test_covalent_syntax.py
@@ -29,6 +29,10 @@ def test_covalent_decorators(tmp_path, monkeypatch):
     def add_distributed(vals, c):
         return [add(val, c) for val in vals]
 
+    @subflow
+    def add_distributed2(vals, c, op):
+        return [op(val, c) for val in vals]
+
     @flow
     def workflow(a, b, c):
         return mult(add(a, b), c)
@@ -39,10 +43,21 @@ def test_covalent_decorators(tmp_path, monkeypatch):
         result2 = make_more(result1)
         return add_distributed(result2, c)
 
+    @flow
+    def dynamic_workflow2(a, b, c):
+        result1 = add(a, b)
+        result2 = make_more(result1)
+        return add_distributed2(result2, c, add)
+
     assert add(1, 2) == 3
     assert mult(1, 2) == 2
     assert ct.get_result(ct.dispatch(workflow)(1, 2, 3), wait=True).result == 9
     assert ct.get_result(ct.dispatch(dynamic_workflow)(1, 2, 3), wait=True).result == [
+        6,
+        6,
+        6,
+    ]
+    assert ct.get_result(ct.dispatch(dynamic_workflow2)(1, 2, 3), wait=True).result == [
         6,
         6,
         6,
