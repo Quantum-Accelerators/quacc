542916316c00315e9d37cd15e94b3c997843c681
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-07 00:16:37 -0800

    Fix handling of decorator kwargs in Dask subflows (#1488)

--- src/quacc/wflow_tools/decorators.py
+++ src/quacc/wflow_tools/decorators.py
@@ -453,12 +453,12 @@ def subflow(_func: Callable | None = None, **kwargs) -> Subflow:
         from dask.distributed import worker_client
 
         @wraps(_func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*f_args, **f_kwargs):
             with worker_client() as client:
-                futures = client.compute(_func(*args, **kwargs))
+                futures = client.compute(_func(*f_args, **f_kwargs))
                 return client.gather(futures)
 
-        return delayed(wrapper)
+        return delayed(wrapper, **kwargs)
     else:
         return _func
 
