23ef075caf094600cfd0cde85c7b0c559a1a6415
TomD <115232841+tomdemeyere@users.noreply.github.com>
2024-01-11 23:14:55 +0000

    Proposal: improve espresso settings in quacc.yaml (#1515)
    ## Requirements
    
    ### Checklist
    
    - [x] I have read the [Contributing
    Guide](https://quantum-accelerators.github.io/quacc/dev/contributing.html).
    Don't lie! 😉
    - [x] My PR is on a custom branch and is _not_ named `main`.
    - [x] I have used `black`, `isort`, and `ruff` as described in the
    [style
    guide](https://quantum-accelerators.github.io/quacc/dev/contributing.html#style).
    - [ ] I have added relevant, comprehensive [unit
    tests](https://quantum-accelerators.github.io/quacc/dev/contributing.html#unit-tests).
    
    ### Notes
    
    - Your PR will likely not be merged without proper and thorough tests.
    - If you are an external contributor, you will see a comment from
    [@buildbot-princeton](https://github.com/buildbot-princeton). This is
    solely for the maintainers.
    - When your code is ready for review, ping one of the [active
    maintainers](https://quantum-accelerators.github.io/quacc/about/contributors.html#active-maintainers).
    
    ## Summary of Changes
    
    As of now there is no way to change binary location neither using
    .quacc.yaml or the CLI? (Can't change the dictionary directly)
    
    This additional keyword would allow it. If there is already a way then
    these changes are obsolete but this PR could then be used to explain it
    better in the doc
    
    ---------
    
    Co-authored-by: Andrew S. Rosen <asrosen93@gmail.com>

--- src/quacc/settings.py
+++ src/quacc/settings.py
@@ -126,21 +126,23 @@ class QuaccSettings(BaseSettings):
     # ---------------------------
     # ESPRESSO Settings
     # ---------------------------
-    ESPRESSO_BIN_PATHS: dict[str, Path] = Field(
+    ESPRESSO_BIN_DIR: Path = Field(
+        Path(), description="Base path to the espresso binaries."
+    )
+    ESPRESSO_BINARIES: dict[str, str] = Field(
         {
-            "pw": Path("pw.x"),
-            "ph": Path("ph.x"),
-            "neb": Path("neb.x"),
-            "q2r": Path("q2r.x"),
-            "matdyn": Path("matdyn.x"),
-            "dynmat": Path("dynmat.x"),
-            "bands": Path("bands.x"),
-            "projwfc": Path("projwfc.x"),
-            "pp": Path("pp.x"),
-            "wannier90": Path("wannier90.x"),
+            "pw": "pw.x",
+            "ph": "ph.x",
+            "neb": "neb.x",
+            "q2r": "q2r.x",
+            "matdyn": "matdyn.x",
+            "dynmat": "dynmat.x",
+            "bands": "bands.x",
+            "projwfc": "projwfc.x",
+            "pp": "pp.x",
+            "wannier90": "wannier90.x",
         },
-        description="Name for each espresso binary and its corresponding path. "
-        "By default, the binaries are assumed to be in PATH.",
+        description="Name for each espresso binary."
     )
     ESPRESSO_PSEUDO: Optional[Path] = Field(
         None, description=("Path to a pseudopotential library for espresso.")
