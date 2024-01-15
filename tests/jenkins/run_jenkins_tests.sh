8f3650de3e804daaa6dba2e5641b8113b803cf41
Andrew Rosen <asrosen93@gmail.com>
2024-01-15 10:36:51 -0800

    fix

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -7,5 +7,5 @@ pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/mas
 pip install -e .[dev]
 
 # Gaussian, ORCA, GULP
-salloc -N 1 -n 32 -t 00:10:00 \
+salloc -N 1 -n 32 -t 00:10:00 hpc_tests.sh
     module purge && pytest tests/core/recipes/gulp_recipes --noconftest
