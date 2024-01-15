4868fee0c6c6117ecbdd07ce0294a3033f6493f3
Andrew Rosen <asrosen93@gmail.com>
2024-01-15 10:22:36 -0800

    fix

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -7,17 +7,8 @@ pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/mas
 pip install -e .[dev]
 
 # Gaussian, ORCA, GULP
-module load gaussian/g16
-module load openmpi/gcc/4.1.2
-salloc -N 1 -n 32 -t 00:10:00 pytest \
-    tests/core/recipes/gaussian_recipes \
-    tests/core/recipes/gulp_recipes \
-    tests/core/recipes/orca_recipes \
-    --noconftest
-
-# VASP
-module purge
-module load intel/2021.1.2
-module load intel-mpi/intel/2021.3.1
-module load hdf5/intel-2021.1/1.10.6
-salloc -N 1 -n 32 -t 00:10:00 pytest tests/core/recipes/vasp_recipes/jenkins --noconftest
+salloc -N 1 -n 32 -t 00:10:00 \
+    module purge && pytest tests/core/recipes/gulp_recipes --noconftest \
+    module purge && module load gaussian/g16 && pytest tests/core/recipes/gaussian_recipes --noconftest \
+    module purge && module load openmpi/gcc/4.1.2 && pytest tests/core/recipes/orca_recipes --noconftest \
+    module purge && module load intel/2021.1.2 intel-mpi/intel/2021.3.1 hdf5/intel-2021.1/1.10.6 && pytest tests/core/recipes/vasp_recipes/jenkins --noconftest
