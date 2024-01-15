1635bdff0a55ab0ee9b74e4e23f0419354f173d4
Andrew Rosen <asrosen93@gmail.com>
2024-01-15 10:09:50 -0800

    Add a VASP test

--- tests/core/recipes/vasp_recipes/jenkins/test_jenkins_vasp_recipes.py
+++ tests/core/recipes/vasp_recipes/jenkins/test_jenkins_vasp_recipes.py
@@ -26,3 +26,24 @@ def test_static_job(tmp_path, monkeypatch):
     assert output["parameters"]["efermi"] == "midgap"
     assert output["parameters"]["kpts"] == [3, 3, 3]
     assert output["results"]["energy"] < 0
+
+
+def test_static_job_spin(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
+
+    atoms = bulk("Fe")
+    atoms.set_initial_magnetic_moments([5.0] * len(atoms))
+
+    output = static_job(atoms, kpts=[3, 3, 3])
+    assert output["nsites"] == len(atoms)
+    assert "isym" not in output["parameters"]
+    assert output["parameters"]["ispin"] == 2
+    assert output["parameters"]["nsw"] == 0
+    assert output["parameters"]["lwave"] is True
+    assert output["parameters"]["encut"] == 520
+    assert output["parameters"]["efermi"] == "midgap"
+    assert output["parameters"]["kpts"] == [3, 3, 3]
+    assert output["results"]["energy"] < 0
+    output_magmoms = output["results"]["atoms"].get_magnetic_moments()
+    assert output_magmoms.all() is True
+    assert output["atoms"].get_initial_magnetic_moments().all() is True
