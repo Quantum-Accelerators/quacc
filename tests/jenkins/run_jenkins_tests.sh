b89964030b8ae08068bc5dcae90006e15a4a8621
Andrew Rosen <asrosen93@gmail.com>
2024-01-15 10:43:44 -0800

    fix

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -7,4 +7,5 @@ pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/mas
 pip install -e .[dev]
 
 # Gaussian, ORCA, GULP
-salloc -N 1 -n 32 -t 00:10:00 ./hpc_tests.sh
+cd /home/asrosen/
+salloc -N 1 -n 32 -t 00:10:00 tests/jenkins/hpc_tests.sh
