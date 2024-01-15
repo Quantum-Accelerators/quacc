09f6de6cc01a3313cfeedd39ddbe81457a35e10e
Andrew Rosen <asrosen93@gmail.com>
2024-01-15 10:13:49 -0800

    fix

--- tests/core/recipes/vasp_recipes/jenkins/test_jenkins_vasp_recipes.py
+++ tests/core/recipes/vasp_recipes/jenkins/test_jenkins_vasp_recipes.py
@@ -5,6 +5,7 @@ import pytest
 pytestmark = pytest.mark.skipif(which("vasp_std") is None, reason="VASP not installed")
 
 from ase.build import bulk
+from numpy.testing import assert_equal
 
 from quacc import SETTINGS
 from quacc.recipes.vasp.core import static_job
@@ -37,13 +38,16 @@ def test_static_job_spin(tmp_path, monkeypatch):
     output = static_job(atoms, kpts=[3, 3, 3])
     assert output["nsites"] == len(atoms)
     assert "isym" not in output["parameters"]
-    assert output["parameters"]["ispin"] == 2
     assert output["parameters"]["nsw"] == 0
     assert output["parameters"]["lwave"] is True
     assert output["parameters"]["encut"] == 520
     assert output["parameters"]["efermi"] == "midgap"
     assert output["parameters"]["kpts"] == [3, 3, 3]
     assert output["results"]["energy"] < 0
+    assert_equal(
+        output["results"]["atoms"].get_initial_magnetic_moments(),
+        atoms.get_initial_magnetic_moments(),
+    )
     output_magmoms = output["results"]["atoms"].get_magnetic_moments()
     assert output_magmoms.all() is True
-    assert output["atoms"].get_initial_magnetic_moments().all() is True
+    assert_equal(output["atoms"].get_initial_magnetic_moments(), output_magmoms)
