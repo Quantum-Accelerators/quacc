d3ae2957acb77f9440d12fc70dff51034d15d6a5
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-18 16:20:24 -0800

    Parsl speedup (#1573)
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>

--- tests/parsl/conftest.py
+++ tests/parsl/conftest.py
@@ -5,8 +5,15 @@ TEST_RESULTS_DIR = Path(__file__).parent / "_test_results"
 TEST_SCRATCH_DIR = Path(__file__).parent / "_test_scratch"
 TEST_RUNINFO = Path(__file__).parent / "runinfo"
 
+try:
+    import parsl
+except ImportError:
+    parsl = None
+
 
 def pytest_sessionstart():
+    if parsl:
+        parsl.load()
     file_dir = Path(__file__).parent
     os.environ["QUACC_CONFIG_FILE"] = str(file_dir / "quacc.yaml")
     os.environ["QUACC_RESULTS_DIR"] = str(TEST_RESULTS_DIR)
@@ -14,6 +21,8 @@ def pytest_sessionstart():
 
 
 def pytest_sessionfinish(exitstatus):
+    if parsl:
+        parsl.clear()
     if exitstatus == 0:
         from shutil import rmtree
 
