81c4999bd77fc3f384e2b93b26367be23fbe82bc
Andrew Rosen <asrosen93@gmail.com>
2024-01-20 21:28:26 -0800

    fix

--- tests/covalent/conftest.py
+++ tests/covalent/conftest.py
@@ -1,6 +1,11 @@
 import os
 from pathlib import Path
 
+try:
+    import covalent as ct
+except ImportError:
+    ct = None
+
 TEST_RESULTS_DIR = Path(__file__).parent / "_test_results"
 TEST_SCRATCH_DIR = Path(__file__).parent / "_test_scratch"
 
@@ -10,6 +15,10 @@ def pytest_sessionstart():
     os.environ["QUACC_CONFIG_FILE"] = str(file_dir / "quacc.yaml")
     os.environ["QUACC_RESULTS_DIR"] = str(TEST_RESULTS_DIR)
     os.environ["QUACC_SCRATCH_DIR"] = str(TEST_SCRATCH_DIR)
+
+    if ct:
+        ct.set_config("executors.dask.create_unique_workdir", True)
+        ct.set_config("executors.local.create_unique_workdir", True)
 
 
 def pytest_sessionfinish(exitstatus):
