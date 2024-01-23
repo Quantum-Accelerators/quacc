356039f29caea79b095964eb0834c303d82d593c
Andrew Rosen <asrosen93@gmail.com>
2024-01-23 00:42:24 -0800

    Fix docstrings

--- src/quacc/recipes/qchem/core.py
+++ src/quacc/recipes/qchem/core.py
@@ -68,7 +68,7 @@ def static_job(
         File(s) to copy to the runtime directory. If a directory is provided, it will be recursively unpacked.
     **calc_kwargs
         Custom kwargs for the calculator. Set a value to `quacc.Remove` to remove
-        a pre-existing key entirely. See [quacc.calculators._qchem_legacy.qchem.QChem][] for more
+        a pre-existing key entirely. See [quacc.calculators.qchem.qchem.QChem][] for more
         details.
 
     Returns
@@ -128,7 +128,7 @@ def relax_job(
         File(s) to copy to the runtime directory. If a directory is provided, it will be recursively unpacked.
     **calc_kwargs
         Custom kwargs for the calculator. Set a value to `quacc.Remove` to remove
-        a pre-existing key entirely. See [quacc.calculators._qchem_legacy.qchem.QChem][] for more
+        a pre-existing key entirely. See [quacc.calculators.qchem.qchem.QChem][] for more
         details.
 
     Returns
@@ -190,7 +190,7 @@ def freq_job(
         File(s) to copy to the runtime directory. If a directory is provided, it will be recursively unpacked.
     **calc_kwargs
         Custom kwargs for the calculator. Set a value to `quacc.Remove` to remove
-        a pre-existing key entirely. See [quacc.calculators._qchem_legacy.qchem.QChem][] for more
+        a pre-existing key entirely. See [quacc.calculators.qchem.qchem.QChem][] for more
         details.
 
     Returns
--- src/quacc/recipes/qchem/ts.py
+++ src/quacc/recipes/qchem/ts.py
@@ -136,7 +136,7 @@ def irc_job(
         File(s) to copy to the runtime directory. If a directory is provided, it will be recursively unpacked.
     **calc_kwargs
         Custom kwargs for the calculator. Set a value to `quacc.Remove` to remove
-        a pre-existing key entirely. See [quacc.calculators._qchem_legacy.qchem.QChem][] for more
+        a pre-existing key entirely. See [quacc.calculators.qchem.qchem.QChem][] for more
         details.
 
     Returns
