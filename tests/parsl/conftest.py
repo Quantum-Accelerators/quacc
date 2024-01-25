9aa816a6e38df99cb21ee1bb92bcc1b1b4281eac
Andrew S. Rosen <asrosen93@gmail.com>
2024-01-25 09:25:50 -0800

    Use faster Parsl config in test suite

--- tests/parsl/conftest.py
+++ tests/parsl/conftest.py
@@ -13,7 +13,29 @@ except ImportError:
 
 def pytest_sessionstart():
     if parsl:
-        parsl.load()
+        from parsl.providers import LocalProvider
+        from parsl.channels import LocalChannel
+        from parsl.launchers import SimpleLauncher
+
+        from parsl.config import Config
+        from parsl.executors import HighThroughputExecutor
+        config = Config(
+                executors=[
+                    HighThroughputExecutor(
+                        label="test_htex_local",
+                        worker_debug=True,
+                        cores_per_worker=1,
+                        provider=LocalProvider(
+                            channel=LocalChannel(),
+                            init_blocks=1,
+                            max_blocks=1,
+                            launcher=SimpleLauncher(),
+                        ),
+                    )
+                ],
+                strategy='none',
+            )
+        parsl.load(config)
     file_dir = Path(__file__).parent
     os.environ["QUACC_CONFIG_FILE"] = str(file_dir / "quacc.yaml")
     os.environ["QUACC_RESULTS_DIR"] = str(TEST_RESULTS_DIR)
