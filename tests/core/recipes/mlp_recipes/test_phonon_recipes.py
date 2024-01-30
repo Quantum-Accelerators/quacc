28e08d5b93d824e99a6aa91f0a78e0cc18b4f031
Andrew Rosen <asrosen93@gmail.com>
2024-01-30 10:38:48 -0800

    fix

--- tests/core/recipes/mlp_recipes/test_phonon_recipes.py
+++ tests/core/recipes/mlp_recipes/test_phonon_recipes.py
@@ -11,6 +11,6 @@ from quacc.recipes.mlp.phonons import phonon_flow
 def test_phonon_flow(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
     atoms = bulk("Cu")
-    output = phonon_flow(atoms, min_length=5.0)
+    output = phonon_flow(atoms, method="mace", min_length=5.0)
     assert output["results"]["force_constants"].shape == (8, 8, 3, 3)
     assert len(output["results"]["thermal_properties"]["temperatures"]) == 101
