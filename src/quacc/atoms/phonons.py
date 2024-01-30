451b1387f18fb2d77bc1471f35dcf2dcd06edbcb
Andrew Rosen <asrosen93@gmail.com>
2024-01-29 19:55:32 -0800

    fix

--- src/quacc/atoms/phonons.py
+++ src/quacc/atoms/phonons.py
@@ -56,14 +56,17 @@ def get_phonopy(
     """
     phonopy_kwargs = phonopy_kwargs or {}
 
-    structure = AseAtomsAdaptor().get_structure(structure)
+    structure = AseAtomsAdaptor().get_structure(atoms)
     if symmetrize:
         structure = SpacegroupAnalyzer(
             structure, symprec=symprec
         ).get_symmetrized_structure()
 
-    n_supercells = np.round(np.ceil(min_length / atoms.cell.lengths()))
-    supercell_matrix = np.diag([n_supercells, n_supercells, n_supercells])
+    if min_length:
+        n_supercells = np.round(np.ceil(min_length / atoms.cell.lengths()))
+        supercell_matrix = np.diag([n_supercells, n_supercells, n_supercells])
+    else:
+        supercell_matrix = None
 
     phonopy_atoms = get_phonopy_structure(structure)
     phonon = phonopy.Phonopy(
