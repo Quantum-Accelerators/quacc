7c1151da8f764f3cbb6b0fef30e02bfcb76dc608
Andrew Rosen <asrosen93@gmail.com>
2024-01-30 09:57:09 -0800

    Add MLP workflow

---
+++ tests/core/recipes/mlp_recipes/test_phonon_recipes.py
@@ -0,0 +1,16 @@
+import pytest
+
+pytest.importorskip("torch")
+pytest.importorskip("mace")
+pytest.importorskip("phonopy")
+from ase.build import bulk
+
+from quacc.recipes.mlp.phonons import phonon_flow
+
+
+def test_phonon_flow(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
+    atoms = bulk("Cu")
+    output = phonon_flow(atoms, min_length=5.0)
+    assert output["results"]["force_constants"].shape == (8, 8, 3, 3)
+    assert len(output["results"]["thermal_properties"]["temperatures"]) == 101
