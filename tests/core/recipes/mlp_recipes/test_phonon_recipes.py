bfa05e1772ec82f7231dc02153bc2635b941b455
Andrew S. Rosen <asrosen93@gmail.com>
2024-02-02 12:38:53 -0800

    Update test_phonon_recipes.py

--- tests/core/recipes/mlp_recipes/test_phonon_recipes.py
+++ tests/core/recipes/mlp_recipes/test_phonon_recipes.py
@@ -28,6 +28,7 @@ def test_phonon_flow_dispersion(tmp_path, monkeypatch):
     monkeypatch.chdir(tmp_path)
     _set_dtype(64)
     atoms = bulk("Cu")
-    output = phonon_flow(atoms, method="mace", min_lengths=5.0, dispersion=True)
+    output = phonon_flow(atoms, method="mace", min_lengths=5.0, job_params={"all": {"dispersion": True}})
     assert output["results"]["force_constants"].shape == (8, 8, 3, 3)
     assert len(output["results"]["thermal_properties"]["temperatures"]) == 101
+    assert output["parameters"]["dispersion"] == True
