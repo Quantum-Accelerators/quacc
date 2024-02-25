b1d13c75389f06668afb49d4a152fbb10478d010
Andrew Rosen <asrosen93@gmail.com>
2024-02-24 16:46:38 -0800

    fix

--- src/quacc/utils/files.py
+++ src/quacc/utils/files.py
@@ -70,7 +70,8 @@ def copy_decompress_files(
     destination = Path(destination).expanduser()
     globbed_source_files = []
     for f in source_files:
-        globs_found = list(Path(f).expanduser().parent.glob(f.name))
+        f_path = Path(f).expanduser()
+        globs_found = list(f_path.parent.glob(f_path.name))
         globbed_source_files.extend(globs_found)
 
     for f in globbed_source_files:
