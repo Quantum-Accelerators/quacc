d69842b89bad0d95d40a37610448eb0152042b6a
Andrew Rosen <asrosen93@gmail.com>
2024-02-27 08:24:35 -0800

    Clean up tests

--- tests/core/atoms/test_elastic.py
+++ tests/core/atoms/test_elastic.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pytest
 from ase.io import read
 from numpy.testing import assert_equal
 
@@ -13,7 +14,7 @@ def test_make_deformations_from_bulk():
     atoms.info["test"] = "hi"
     deformations = make_deformations_from_bulk(atoms)
     assert len(deformations) == 24
-    assert deformations[0].get_volume() != atoms.get_volume()
+    assert deformations[0].get_volume() != pytest.approx(atoms.get_volume())
     for deformation in deformations:
         assert_equal(deformation.get_atomic_numbers(), [30, 30, 30, 30, 52, 52, 52, 52])
         assert_equal(deformation.get_chemical_formula(), "Te4Zn4")
--- tests/core/recipes/emt_recipes/test_emt_elastic.py
+++ tests/core/recipes/emt_recipes/test_emt_elastic.py
@@ -1,3 +1,4 @@
+import pytest
 from ase.build import bulk
 
 from quacc.recipes.emt.elastic import bulk_to_deformations_flow
@@ -8,10 +9,22 @@ def test_elastic_jobs(tmp_path, monkeypatch):
 
     atoms = bulk("Cu")
 
-    outputs = bulk_to_deformations_flow(atoms, run_static=True)
+    outputs = bulk_to_deformations_flow(atoms, run_static=False)
+    assert outputs[0]["atoms"].get_volume() != pytest.approx(atoms.get_volume())
     for output in outputs:
         assert output["parameters"]["asap_cutoff"] is False
         assert output["name"] == "EMT Relax"
+        assert output["nelements"] == 1
+        assert output["nsites"] == 1
+        assert len(outputs) == 24
+
+    outputs = bulk_to_deformations_flow(
+        atoms, run_static=True, job_params={"static_job": {"asap_cutoff": True}}
+    )
+    assert outputs[0]["atoms"].get_volume() != pytest.approx(atoms.get_volume())
+    for output in outputs:
+        assert output["parameters"]["asap_cutoff"] is True
+        assert output["name"] == "EMT Static"
         assert output["nelements"] == 1
         assert output["nsites"] == 1
         assert len(outputs) == 24
