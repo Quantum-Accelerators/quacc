cc5b0be5a5eebebd541fd99d6eca533909aa1255
Andrew S. Rosen <asrosen93@gmail.com>
2024-02-27 08:09:51 -0800

    Update test_elastic.py

--- tests/core/atoms/test_elastic.py
+++ tests/core/atoms/test_elastic.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from ase.io import read
+from numpy.testing import assert_equal
 
 from quacc.atoms.deformation import make_deformations_from_bulk
 
@@ -12,11 +13,7 @@ def test_make_deformations_from_bulk():
     atoms.info["test"] = "hi"
     deformations = make_deformations_from_bulk(atoms)
     assert len(deformations) == 24
-    assert [
-        deformation.get_atomic_numbers() == [30, 30, 30, 30, 52, 52, 52, 52]
-        for deformation in deformations
-    ]
-    assert [
-        deformation.get_chemical_formula() == "Te4Zn4" for deformation in deformations
-    ]
-    assert [atoms.info["test"] == "hi" for deformation in deformations]
+    for deformation in deformations:
+        assert_equal(deformation.get_atomic_numbers(), [30, 30, 30, 30, 52, 52, 52, 52])
+        assert_equal(deformation.get_chemical_formula(), "Te4Zn4")
+        assert deformation.info["test"] == "hi"
