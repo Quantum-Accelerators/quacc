9ff6d9ef25e483ebe3230d5b78b48f673c9d76d0
Andrew-S-Rosen <asrosen93@gmail.com>
2024-02-29 15:22:11 -0800

    Clean up conftest.py imports

--- tests/core/recipes/gulp_recipes/conftest.py
+++ tests/core/recipes/gulp_recipes/conftest.py
@@ -1,13 +1,8 @@
 import os
 from pathlib import Path
-from shutil import copy
 from unittest import mock
 
 import pytest
-from ase.calculators.calculator import FileIOCalculator
-from ase.calculators.gulp import GULP
-from ase.calculators.lj import LennardJones
-from ase.io import read
 
 FILE_DIR = Path(__file__).parent
 GULP_DIR = Path(FILE_DIR, "gulp_run")
@@ -20,6 +15,8 @@ def mock_settings_env_vars():
 
 
 def mock_execute(self, *args, **kwargs):
+    from shutil import copy
+
     copy(GULP_DIR / "gulp.got.gz", "gulp.got.gz")
     copy(GULP_DIR / "gulp.cif.gz", "gulp.cif.gz")
     copy(GULP_DIR / "gulp.xyz.gz", "gulp.xyz.gz")
@@ -27,10 +24,15 @@ def mock_execute(self, *args, **kwargs):
 
 @pytest.fixture(autouse=True)
 def patch_execute(monkeypatch):
+    from ase.calculators.calculator import FileIOCalculator
+
     monkeypatch.setattr(FileIOCalculator, "execute", mock_execute)
 
 
 def mock_read_results(self, *args, **kwargs):
+    from ase.calculators.lj import LennardJones
+    from ase.io import read
+
     atoms = read("gulp.xyz.gz")
     atoms.calc = LennardJones()
     atoms.get_potential_energy()
@@ -40,6 +42,8 @@ def mock_read_results(self, *args, **kwargs):
 
 @pytest.fixture(autouse=True)
 def patch_read_results(monkeypatch):
+    from ase.calculators.gulp import GULP
+
     monkeypatch.setattr(GULP, "read_results", mock_read_results)
 
 
@@ -54,6 +58,8 @@ def mock_get_opt_state(self, **kwargs):
 
 @pytest.fixture(autouse=True)
 def patch_get_opt_state(monkeypatch):
+    from ase.calculators.gulp import GULP
+
     # Monkeypatch the .get_opt_state() method of the GULP calculator object so
     # we aren't fetching the actual state
     monkeypatch.setattr(GULP, "get_opt_state", mock_get_opt_state)
