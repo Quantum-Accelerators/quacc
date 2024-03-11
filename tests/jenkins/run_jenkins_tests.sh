4ca9d1a85686810a30e137b32580bb503b33de5d
Andrew S. Rosen <asrosen93@gmail.com>
2024-03-11 16:15:27 -0700

    Update run_jenkins_tests.sh

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -6,7 +6,7 @@ conda activate quacc
 pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/master/ase-master.zip
 pip install -r tests/requirements.txt
 pip install -e .[dev]
-pip install --force-reinstall https://github.com/materialsproject/custodian.git
+pip install --force-reinstall git+https://github.com/materialsproject/custodian.git
 
 # Request and run Slurm job
 salloc -N 1 -n 32 -t 00:10:00 tests/jenkins/hpc_tests.sh
