f4da7580e2275b4056f4315281cc8603fb2574db
Andrew Rosen <asrosen93@gmail.com>
2024-03-20 08:27:39 -0700

    Use "engrad" instead of "sp" for ORCA static job

--- tests/core/recipes/orca_recipes/test_orca_recipes.py
+++ tests/core/recipes/orca_recipes/test_orca_recipes.py
@@ -16,7 +16,7 @@ def test_static_job(tmp_path, monkeypatch):
     assert output["natoms"] == len(atoms)
     assert (
         output["parameters"]["orcasimpleinput"]
-        == "def2-tzvp normalprint slowconv sp wb97x-d3bj xyzfile"
+        == "def2-tzvp engrad normalprint slowconv wb97x-d3bj xyzfile"
     )
     assert output["parameters"]["charge"] == 0
     assert output["parameters"]["mult"] == 1
