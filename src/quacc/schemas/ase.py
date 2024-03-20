024e8a97937b4c199a6da9db75498cf7b51cc033
Andrew Rosen <asrosen93@gmail.com>
2024-03-19 18:05:37 -0700

    `black `.

--- src/quacc/schemas/ase.py
+++ src/quacc/schemas/ase.py
@@ -123,8 +123,8 @@ def summarize_run(
         with (
             gzip.open(Path(directory, "quacc_results.pkl.gz"), "wb")
             if SETTINGS.GZIP_FILES
-            else Path(directory, "quacc_results.pkl").open("wb") as f
-        ):
+            else Path(directory, "quacc_results.pkl").open("wb")
+        ) as f:
             pickle.dump(task_doc, f)
 
     if store:
@@ -230,8 +230,8 @@ def summarize_opt_run(
         with (
             gzip.open(Path(directory, "quacc_results.pkl.gz"), "wb")
             if SETTINGS.GZIP_FILES
-            else Path(directory, "quacc_results.pkl").open("wb") as f
-        ):
+            else Path(directory, "quacc_results.pkl").open("wb")
+        ) as f:
             pickle.dump(task_doc, f)
 
     if store:
@@ -303,8 +303,8 @@ def summarize_vib_and_thermo(
             with (
                 gzip.open(Path(directory, "quacc_results.pkl.gz"), "wb")
                 if SETTINGS.GZIP_FILES
-                else Path(directory, "quacc_results.pkl").open("wb") as f
-            ):
+                else Path(directory, "quacc_results.pkl").open("wb")
+            ) as f:
                 pickle.dump(task_doc, f)
     if store:
         results_to_db(store, task_doc)
