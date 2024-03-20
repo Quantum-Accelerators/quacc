99f616416314df1e6a0f300de642307f43dd801f
Andrew S. Rosen <asrosen93@gmail.com>
2024-03-20 10:41:35 -0700

    Update mp.py

--- src/quacc/recipes/vasp/mp.py
+++ src/quacc/recipes/vasp/mp.py
@@ -8,15 +8,6 @@ This set of recipes is meant to be compatible with the Materials Project
     Make sure that you use the Materials Project-compatible pseudpotential
     versions. The GGA workflows use the old (no version) PAW PBE potentials.
     The meta-GGA workflows currently use the v.54 PAW PBE potentials.
-
-!!! Info
-
-    The one true source of Materials Project workflows is
-    [atomate2](https://github.com/materialsproject/atomate2).
-    If you need an MP-compatible workflow, we strongly encourage you to
-    use atomate2 to ensure that all of your settings are fully compatible
-    and up-to-date. This module is a best effort to be used at your own
-    discretion.
 """
 
 from __future__ import annotations
