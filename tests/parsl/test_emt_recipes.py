3302750e94643038c683ebc0e00d1473c6998a4a
Andrew-S-Rosen <asrosen93@gmail.com>
2024-03-25 12:00:41 -0700

    Fix `copy_files` support with Parsl

--- tests/parsl/test_emt_recipes.py
+++ tests/parsl/test_emt_recipes.py
@@ -6,7 +6,7 @@ parsl = pytest.importorskip("parsl")
 
 from ase.build import bulk
 
-from quacc import SETTINGS, job
+from quacc import SETTINGS, flow, job
 from quacc.recipes.emt.core import relax_job  # skipcq: PYL-C0412
 from quacc.recipes.emt.slabs import bulk_to_slabs_flow  # skipcq: PYL-C0412
 
@@ -32,16 +32,16 @@ def test_functools(tmp_path, monkeypatch, job_decorators):
     assert result[-1]["fmax"] == 0.1
 
 
-# def test_copy_files(tmp_path, monkeypatch):
-#     monkeypatch.chdir(tmp_path)
-#     atoms = bulk("Cu")
+def test_copy_files(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
+    atoms = bulk("Cu")
 
-#     @flow
-#     def myflow(atoms):
-#         result1 = relax_job(atoms)
-#         return relax_job(result1["atoms"], copy_files={result1["dir_name"]: "opt.*"})
+    @flow
+    def myflow(atoms):
+        result1 = relax_job(atoms)
+        return relax_job(result1["atoms"], copy_files={result1["dir_name"]: "opt.*"})
 
-#     assert "atoms" in myflow(atoms).result()
+    assert "atoms" in myflow(atoms).result()
 
 
 def test_phonon_flow(tmp_path, monkeypatch):
