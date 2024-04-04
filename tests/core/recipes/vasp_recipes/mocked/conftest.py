9e5efdf4a1cc4a2dec5e402db6aea797b30b19a6
Andrew Rosen <asrosen93@gmail.com>
2024-04-03 21:47:48 -0700

    fix

--- tests/core/recipes/vasp_recipes/mocked/conftest.py
+++ tests/core/recipes/vasp_recipes/mocked/conftest.py
@@ -44,14 +44,14 @@ def patch_read_results(monkeypatch):
     monkeypatch.setattr(Vasp, "read_results", mock_read_results)
 
 
-def mock_taskdoc(directory, *args, **kwargs):
+def mock_taskdoc(dir_name, *args, **kwargs):
     from ase.io import read
     from monty.os.path import zpath
 
     from quacc.atoms.core import check_is_metal
 
     MOCK_TASKDOC.output.bandgap = (
-        0.0 if check_is_metal(read(zpath(Path(directory, "CONTCAR")))) else 0.5
+        0.0 if check_is_metal(read(zpath(Path(dir_name, "CONTCAR")))) else 0.5
     )
     return MOCK_TASKDOC
 
