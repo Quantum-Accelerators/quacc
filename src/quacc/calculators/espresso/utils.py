395a9cb049d9573ed9ab3b9b8a7e199f93c85391
Andrew S. Rosen <asrosen93@gmail.com>
2024-04-08 10:39:29 -0700

    Add missing typehints

--- src/quacc/calculators/espresso/utils.py
+++ src/quacc/calculators/espresso/utils.py
@@ -240,7 +240,7 @@ def espresso_prepare_dir(outdir: str | Path, binary: str = "pw") -> dict[str, An
     return outkeys.get(binary, {})
 
 
-def remove_conflicting_kpts_kspacing(to_change_dict, reference_dict):
+def remove_conflicting_kpts_kspacing(to_change_dict: dict[str, Any], reference_dict: dict[str, Any]) -> dict[str, Any]:
     """
     Parameters
     ----------
