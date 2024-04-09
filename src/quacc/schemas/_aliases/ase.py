a075555ab77c17e66ca41f0ae34abfe5d49e1000
TomD <T.Demeyere@soton.ac.uk>
2024-04-10 01:38:32 +0200

    schema

--- src/quacc/schemas/_aliases/ase.py
+++ src/quacc/schemas/_aliases/ase.py
@@ -23,6 +23,14 @@ class ParametersOpt(TypedDict):
     """Dictionary of parameters from Optimizer.todict()"""
 
 
+class ParametersDyn(TypedDict):
+    """Dictionary of parameters from Dynamics.todict()"""
+
+
+class TrajectoryLog(TypedDict):
+    """Dictionary of parameters from Dynamics.todict()"""
+
+
 class RunSchema(AtomsSchema):
     """Schema for [quacc.schemas.ase.summarize_run][]"""
 
@@ -48,12 +56,11 @@ class OptSchema(RunSchema):
 class DynSchema(RunSchema):
     """Schema for [quacc.schemas.ase.summarize_md_run][]"""
 
-    parameters_md: dict[str, Any]  # from Dynamics.todict()
+    parameters_md: ParametersDyn  # from Dynamics.todict()
     nsteps: int
     trajectory: list[Atoms]
-    trajectory_log: list[dict[str, Any]]
+    trajectory_log: TrajectoryLog
     trajectory_results: list[Results]
-    restart_data: dict[str, Any]
 
 
 class ParametersVib(TypedDict):
