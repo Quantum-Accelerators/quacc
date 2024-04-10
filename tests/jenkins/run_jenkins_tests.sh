dd4eb566d110b0366b402cf7fc83e7043df405d5
Andrew S. Rosen <asrosen93@gmail.com>
2024-04-09 23:05:28 -0700

    Update run_jenkins_tests.sh

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -4,8 +4,6 @@
 source ~/.bashrc
 
 # ------- Package Setup -------
-if ! conda env list | grep 'quacc' > /dev/null; then
-    conda create --name quacc python=3.11
 conda activate quacc
 pip install uv
 uv pip install --force-reinstall --no-deps "ase @ https://gitlab.com/ase/ase/-/archive/master/ase-master.zip"
