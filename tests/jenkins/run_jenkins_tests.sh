6a95d936d51ce7a8c5553d157a30f206239c0dfc
Andrew S. Rosen <asrosen93@gmail.com>
2024-04-09 22:41:14 -0700

    Update run_jenkins_tests.sh

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -3,9 +3,8 @@
 # Setup
 source ~/.bashrc
 conda activate quacc
-pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/master/ase-master.zip
-pip install -r tests/requirements.txt
-pip install -e .[dev]
+pip install uv
+pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/master/ase-master.zip -r tests/requirements.txt "quacc[docs] @ ."
 
 # Request and run Slurm job
 salloc -N 1 -n 32 -t 00:10:00 tests/jenkins/hpc_tests.sh
