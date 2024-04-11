dee51739e2a54dc603b86801cb41d609e72a2c3f
Andrew Rosen <asrosen93@gmail.com>
2024-04-11 15:55:10 -0700

    Sourcery refactor

--- src/quacc/calculators/espresso/espresso.py
+++ src/quacc/calculators/espresso/espresso.py
@@ -298,7 +298,9 @@ class EspressoTemplate(EspressoTemplate_):
             smearing = system.get("smearing", None)
             degauss = system.get("degauss", None)
 
-            if occupations == "fixed" and not (smearing is None and degauss is None):
+            if occupations == "fixed" and (
+                smearing is not None or degauss is not None
+            ):
                 LOGGER.warning(
                     "The occupations are set to 'fixed' but smearing or degauss is also set. This will be ignored."
                 )
