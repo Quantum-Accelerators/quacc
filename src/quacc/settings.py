8258702cabff3b006f7bd503ea71a0d8bed9065f
Andrew Rosen <asrosen93@gmail.com>
2024-04-13 15:26:25 -0700

    Apply ruff `N` rules

--- src/quacc/settings.py
+++ src/quacc/settings.py
@@ -451,7 +451,7 @@ class QuaccSettings(BaseSettings):
         return v
 
     @field_validator("STORE")
-    def generate_store(cls, v: Union[dict[str, dict[str, Any]], Store]) -> Store:
+    def generate_store(self, v: Union[dict[str, dict[str, Any]], Store]) -> Store:
         """Generate the Maggma store."""
         from maggma import stores
 
