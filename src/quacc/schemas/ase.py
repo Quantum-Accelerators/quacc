246906596818dd0699b8d788e818d2ba1c52c9da
Andrew Rosen <asrosen93@gmail.com>
2024-04-21 17:59:05 -0700

    fix

--- src/quacc/schemas/ase.py
+++ src/quacc/schemas/ase.py
@@ -267,6 +267,7 @@ def summarize_md_run(
 
     initial_atoms = trajectory[0]
     final_atoms = get_final_atoms_from_dynamics(dyn)
+    directory = final_atoms.calc.directory
 
     # Base task doc
     base_task_doc = summarize_run(
