280dee4c7e7aa579db43b57c8278094efe493c33
Andrew Rosen <asrosen93@gmail.com>
2024-04-21 22:35:35 -0700

    fix

--- tests/core/atoms/conftest.py
+++ tests/core/atoms/conftest.py
@@ -9,7 +9,9 @@ import pytest
 FILE_DIR = Path(__file__).parent
 
 
-def mock_generate_chemshell_cluster(slab, slab_center_idx, atom_oxi_states, filepath, **kwargs):
+def mock_generate_chemshell_cluster(
+    slab, slab_center_idx, atom_oxi_states, filepath, **kwargs
+):
     with (
         gzip.open(
             Path(FILE_DIR, "skzcam_files", "REF_ChemShell_cluster.xyz.gz"), "rb"
