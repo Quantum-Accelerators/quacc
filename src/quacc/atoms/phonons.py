451412479250bc26c98df91e535d320d27b0af50
Andrew Rosen <asrosen93@gmail.com>
2024-04-23 19:03:10 -0700

    Looser requirements to use `get_phonopy`

--- src/quacc/atoms/phonons.py
+++ src/quacc/atoms/phonons.py
@@ -14,9 +14,9 @@ from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
 try:
     import phonopy
 
-    has_deps = find_spec("seekpath") is not None
+    has_phonopy = True
 except ImportError:
-    has_deps = False
+    has_phonopy = False
 
 if TYPE_CHECKING:
     from ase.atoms import Atoms
@@ -26,7 +26,7 @@ if TYPE_CHECKING:
         from phonopy.structure.atoms import PhonopyAtoms
 
 
-@requires(has_deps, "Phonopy or seekpath is not installed.")
+@requires(has_phonopy, "Phonopy not installed.")
 def get_phonopy(
     atoms: Atoms,
     min_lengths: float | tuple[float, float, float] | None = None,
