ff53e12523ea0151342a564cf974f1c07b42032d
Andrew S. Rosen <asrosen93@gmail.com>
2024-04-29 11:23:48 -0700

    Update conftest.py

--- tests/parsl/conftest.py
+++ tests/parsl/conftest.py
@@ -2,6 +2,9 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from parsl.config import Config
+from parsl.dataflow.dependency_resolvers import DEEP_DEPENDENCY_RESOLVER
+
 TEST_RESULTS_DIR = Path(__file__).parent / "_test_results"
 TEST_SCRATCH_DIR = Path(__file__).parent / "_test_scratch"
 TEST_RUNINFO = Path(__file__).parent / "runinfo"
@@ -16,7 +19,7 @@ def pytest_sessionstart():
     import os
 
     if parsl:
-        parsl.load()
+        parsl.load(Config(dependency_resolver=DEEP_DEPENDENCY_RESOLVER))
     file_dir = Path(__file__).parent
     os.environ["QUACC_CONFIG_FILE"] = str(file_dir / "quacc.yaml")
     os.environ["QUACC_RESULTS_DIR"] = str(TEST_RESULTS_DIR)
