d46bc057d548c08f366b41d2de6dd2ce666959cd
Andrew Rosen <asrosen93@gmail.com>
2024-05-12 00:19:26 -0700

    fix

--- src/quacc/settings.py
+++ src/quacc/settings.py
@@ -469,60 +469,60 @@ class QuaccSettings(BaseSettings):
         dict
             Loaded settings.
         """
-        return _type_handler(_use_custom_config_settings(settings))
+        return cls._type_handler(cls._use_custom_config_settings(settings))
 
+    @classmethod
+    def _use_custom_config_settings(cls, settings: dict[str, Any]) -> dict[str, Any]:
+        """Parse user settings from a custom YAML.
 
-def _use_custom_config_settings(settings: dict[str, Any]) -> dict[str, Any]:
-    """Parse user settings from a custom YAML.
+        Parameters
+        ----------
+        settings : dict
+            Initial settings.
 
-    Parameters
-    ----------
-    settings : dict
-        Initial settings.
+        Returns
+        -------
+        dict
+            Updated settings based on the custom YAML.
+        """
+        from monty.serialization import loadfn
 
-    Returns
-    -------
-    dict
-        Updated settings based on the custom YAML.
-    """
-    from monty.serialization import loadfn
+        config_file_path = (
+            Path(settings.get("CONFIG_FILE", _DEFAULT_CONFIG_FILE_PATH))
+            .expanduser()
+            .resolve()
+        )
 
-    config_file_path = (
-        Path(settings.get("CONFIG_FILE", _DEFAULT_CONFIG_FILE_PATH))
-        .expanduser()
-        .resolve()
-    )
+        new_settings = {}  # type: dict
+        if config_file_path.exists() and config_file_path.stat().st_size > 0:
+            new_settings |= loadfn(config_file_path)
 
-    new_settings = {}  # type: dict
-    if config_file_path.exists() and config_file_path.stat().st_size > 0:
-        new_settings |= loadfn(config_file_path)
+        new_settings.update(settings)
+        return new_settings
 
-    new_settings.update(settings)
-    return new_settings
+    @classmethod
+    def _type_handler(cls, settings: dict[str, Any]) -> dict[str, Any]:
+        """
+        Convert common strings to their proper types.
 
+        Parameters
+        ----------
+        settings : dict
+            Initial settings.
 
-def _type_handler(settings: dict[str, Any]) -> dict[str, Any]:
-    """
-    Convert common strings to their proper types.
+        Returns
+        -------
+        dict
+            Updated settings.
+        """
+        for key, value in settings.items():
+            if isinstance(value, str):
+                if value.lower() in {"null", "none"}:
+                    settings[key] = None
+                elif value.lower() in {"true", "false"}:
+                    settings[key] = value.lower() == "true"
 
-    Parameters
-    ----------
-    settings : dict
-        Initial settings.
-
-    Returns
-    -------
-    dict
-        Updated settings.
-    """
-    for key, value in settings.items():
-        if isinstance(value, str):
-            if value.lower() in {"null", "none"}:
-                settings[key] = None
-            elif value.lower() in {"true", "false"}:
-                settings[key] = value.lower() == "true"
-
-    return settings
+        return settings
 
 
 @contextmanager
@@ -533,8 +533,7 @@ def change_settings(changes: dict[str, Any]) -> QuaccSettings:  # type: ignore
     Parameters
     ----------
     changes
-        Dictionary of changes to make formatted as attribute: value, e.g.
-        `{"RESULTS_DIR": "/path/to/results", "DEBUG": True}`.
+        Dictionary of changes to make formatted as attribute: value.
 
     Returns
     -------
