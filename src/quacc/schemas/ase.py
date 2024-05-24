e53cc2cbfbc84a2765979cb68e2c1dabfb165b2f
Andrew S. Rosen <asrosen93@gmail.com>
2024-05-23 20:13:32 -0700

    Update `Optimizer.todict()` call in ASE opt schema (#2177)
    This mainly updates a schema to reflect changes in
    https://gitlab.com/ase/ase/-/merge_requests/3360. The only user-facing
    change is that `nsteps` is moved slightly.

--- src/quacc/schemas/ase.py
+++ src/quacc/schemas/ase.py
@@ -193,14 +193,13 @@ def summarize_opt_run(
     )
 
     # Clean up the opt parameters
-    parameters_opt = dyn.todict() | {"fmax": getattr(dyn, "fmax", None)}
+    parameters_opt = dyn.todict()
     parameters_opt.pop("logfile", None)
     parameters_opt.pop("restart", None)
 
     opt_fields = {
         "parameters_opt": parameters_opt,
         "converged": is_converged,
-        "nsteps": dyn.get_number_of_steps(),
         "trajectory": trajectory,
         "trajectory_results": [atoms.calc.results for atoms in trajectory],
     }
