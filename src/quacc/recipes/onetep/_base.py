e5dd50bfb3aa49b75043e1b6db01e3f25165a640
Andrew S. Rosen <asrosen93@gmail.com>
2024-05-30 19:39:43 -0700

    Update _base.py

--- src/quacc/recipes/onetep/_base.py
+++ src/quacc/recipes/onetep/_base.py
@@ -129,7 +129,7 @@ def prep_calculator(
 
     return Onetep(
         profile=OnetepProfile(
-            command=f"{SETTINGS.ONETEP_CMD} {SETTINGS.ONETEP_PARALLEL_CMD}"
+            command=f"{SETTINGS.ONETEP_PARALLEL_CMD} {SETTINGS.ONETEP_CMD}"
         ),
         pseudo_path=str(SETTINGS.ONETEP_PP_PATH),
         **calc_flags,
