3f37306f484c8870719c846340b84b409044b573
Andrew S. Rosen <asrosen93@gmail.com>
2024-06-04 17:46:53 -0700

    Update phonons.py

--- src/quacc/runners/phonons.py
+++ src/quacc/runners/phonons.py
@@ -20,11 +20,12 @@ if TYPE_CHECKING:
 
 
 class PhonopyRunner(Runner):
-    @requires(has_phonopy, "Phonopy is not installed.")
-    @requires(has_seekpath, "Seekpath is not installed")
+
     def __init__(self, *args, **kwargs):
         super().__init__(None, *args, **kwargs)
 
+    @requires(has_phonopy, "Phonopy is not installed.")
+    @requires(has_seekpath, "Seekpath is not installed")
     def run_phonopy(
         self,
         phonon: Phonopy,
