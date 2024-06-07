eed23bdc86f24b92e563745ff2b37e0e2ef71585
Andrew S. Rosen <asrosen93@gmail.com>
2024-06-07 00:06:29 -0700

    Update conftest

--- tests/covalent/conftest.py
+++ tests/covalent/conftest.py
@@ -8,12 +8,12 @@ TEST_RESULTS_DIR = Path(__file__).parent / "_test_results"
 TEST_SCRATCH_DIR = Path(__file__).parent / "_test_scratch"
 
 
-def pytest_sessionstart():
+def pytest_sessionstart(monkeypatch):
     file_dir = Path(__file__).parent
-    os.environ["QUACC_CONFIG_FILE"] = str(file_dir / "quacc.yaml")
-    os.environ["QUACC_RESULTS_DIR"] = str(TEST_RESULTS_DIR)
-    os.environ["QUACC_SCRATCH_DIR"] = str(TEST_SCRATCH_DIR)
-    os.environ["COVALENT_CONFIG_DIR"] = str(file_dir)
+    monkeypatch.setenv("QUACC_CONFIG_FILE"], str(file_dir / "quacc.yaml"))
+    monkeypatch.setenv("QUACC_RESULTS_DIR"], str(TEST_RESULTS_DIR))
+    monkeypatch.setenv("QUACC_SCRATCH_DIR"], str(TEST_SCRATCH_DIR))
+    monkeypatch.setenv("COVALENT_CONFIG_DIR", str(file_dir))
 
 
 def pytest_sessionfinish(exitstatus):
