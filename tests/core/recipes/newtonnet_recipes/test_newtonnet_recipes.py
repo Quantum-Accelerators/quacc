9a20e54cc606a9d456a7c910d9b89573601612cb
kumaranu <anukumar94663@gmail.com>
2024-06-14 13:16:46 -0700

    Made function imports to multiline to match the remove branch locally.

--- tests/core/recipes/newtonnet_recipes/test_newtonnet_recipes.py
+++ tests/core/recipes/newtonnet_recipes/test_newtonnet_recipes.py
@@ -13,7 +13,14 @@ from ase.mep.neb import NEBOptimizer
 
 from quacc import SETTINGS
 from quacc.recipes.newtonnet.core import freq_job, relax_job, static_job
-from quacc.recipes.newtonnet.ts import irc_job, neb_job, neb_ts_job, geodesic_ts_job, quasi_irc_job, ts_job
+from quacc.recipes.newtonnet.ts import (
+    irc_job,
+    neb_job,
+    neb_ts_job,
+    geodesic_ts_job,
+    quasi_irc_job,
+    ts_job,
+)
 
 DEFAULT_SETTINGS = SETTINGS.model_copy()
 
