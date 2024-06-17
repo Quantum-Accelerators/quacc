e0606f23739304e0a4ceec519fffe8767d837376
kumaranu <anukumar94663@gmail.com>
2024-06-17 12:58:55 -0700

    added schema as a class in schema/_aliasis/ase.py

--- src/quacc/schemas/_aliases/ase.py
+++ src/quacc/schemas/_aliases/ase.py
@@ -97,3 +97,10 @@ class ThermoSchema(AtomsSchema):
 
 class VibThermoSchema(VibSchema, ThermoSchema):
     """Schema for [quacc.schemas.ase.summarize_vib_and_thermo][]"""
+
+
+class NebSchema(TypedDict):
+    relax_reactant: OptSchema
+    relax_product: OptSchema
+    geodesic_results: list[Atoms]
+    neb_results: dict
