332cedd0a1a545a55f49d42d19fbea85f2d95402
Andrew S. Rosen <asrosen93@gmail.com>
2024-06-20 12:12:22 -0700

    Update md.py

--- src/quacc/recipes/emt/md.py
+++ src/quacc/recipes/emt/md.py
@@ -52,17 +52,6 @@ def md_job(
         Additionally, Quacc uses the keywords `fix_com` and `fix_rot` instead of
         `fixcm` and `fixrot` to fix the center of mass and rotation, respectively.
 
-        As of March 2024, ASE still accept deprecated keywords such as:
-
-        - temperature instead of temperature_K
-        - pressure instead of pressure_au
-        - compressibility instead of compressibility_au
-        - dt instead of timestep
-
-        All of these keywords will be accepted by Quacc, but it is recommended to use the
-        new keywords to avoid confusion. Everything will always be converted to the Quacc
-        units mentioned above.
-
         Shared keywords between various dynamics type such as `timestep` and `steps` should
         be specified in the `md_params` dictionary. Keywords specific to the dynamics type
         should be specified in a dictionary `dynamics_kwargs` inside `md_params`.
