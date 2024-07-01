c78090c6fb93d6cb5f87341ff08d8a4c332cd74a
Mouad <mouad.aitelbad@gmail.com>
2024-07-01 22:30:08 +0200

    fix typing for python < 3.11

--- src/quacc/types.py
+++ src/quacc/types.py
@@ -4,7 +4,7 @@ Custom types used throughout quacc.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, NotRequired, TypedDict
+from typing import TYPE_CHECKING
 
 
 class DefaultSetting:
@@ -35,6 +35,7 @@ if TYPE_CHECKING:
     from pymatgen.core.structure import Molecule, Structure
     from pymatgen.entries.computed_entries import ComputedEntry
     from pymatgen.io.vasp.inputs import Incar, Kpoints, Poscar, Potcar
+    from typing_extensions import NotRequired, TypedDict
 
     # ----------- File handling -----------
 
