bbf0eb75527596daeb1e855c162e9c0e1fbd7fe2
Andrew S. Rosen <asrosen93@gmail.com>
2024-07-25 16:20:27 -0400

    Update test_orca_recipes.py

--- tests/core/recipes/orca_recipes/test_orca_recipes.py
+++ tests/core/recipes/orca_recipes/test_orca_recipes.py
@@ -32,7 +32,6 @@ def test_static_job(tmp_path, monkeypatch):
     assert output["parameters"]["mult"] == 1
     assert output["spin_multiplicity"] == 1
     assert output["charge"] == 0
-    assert output.get("attributes")
 
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
@@ -57,7 +56,6 @@ def test_static_job_parallel(tmp_path, monkeypatch):
         == "def2-svp engrad normalprint wb97x-d3bj xyzfile"
     )
     assert "%scf maxiter 300 end" in output["parameters"]["orcablocks"]
-    assert output.get("attributes")
 
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
@@ -93,7 +91,6 @@ def test_relax_job(tmp_path, monkeypatch):
     )
     assert output.get("trajectory")
     assert len(output["trajectory"]) > 1
-    assert output.get("attributes")
 
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
@@ -136,7 +133,6 @@ def test_ase_relax_job(tmp_path, monkeypatch):
     assert output["parameters_opt"]["fmax"] == 0.1
     assert output.get("trajectory")
     assert output.get("trajectory_results")
-    assert output.get("attributes")
 
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
@@ -152,7 +148,6 @@ def test_ase_relax_job_store(tmp_path, monkeypatch):
         assert f"step{i}" in os.listdir(output["dir_name"])
         assert "orca.xyz.gz" in os.listdir(Path(output["dir_name"], f"step{i}"))
     assert len(output["steps"]) == nsteps
-    assert "attributes" in output["steps"][0]
 
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
@@ -174,8 +169,6 @@ def test_freq_job(tmp_path, monkeypatch):
     assert output["parameters"]["charge"] == 0
     assert output["parameters"]["mult"] == 1
     assert output["parameters"]["orcasimpleinput"] == "def2-svp freq hf xyzfile"
-    assert output.get("attributes")
-
 
 @pytest.mark.skipif(os.name == "nt", reason="mpirun not available on Windows")
 def test_ase_quasi_irc_perturb_job(tmp_path, monkeypatch):
@@ -202,4 +195,3 @@ def test_ase_quasi_irc_perturb_job(tmp_path, monkeypatch):
     assert output["parameters"]["charge"] == 0
     assert output["parameters"]["mult"] == 1
     assert output["parameters"]["orcasimpleinput"] == "def2-svp engrad hf xyzfile"
-    assert output.get("attributes")
