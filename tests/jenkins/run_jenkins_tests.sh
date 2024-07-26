135f29d2ff21895c87aaa7bcc3c61aaf98ca7556
Andrew S. Rosen <asrosen93@gmail.com>
2024-07-26 00:11:57 -0400

    Update run_jenkins_tests.sh

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -7,6 +7,7 @@ source ~/.bashrc
 conda activate quacc
 conda install -c conda-forge openbabel
 pip install uv
+uv pip install --force-reinstall --no-deps https://gitlab.com/ase/ase/-/archive/master/ase-master.zip
 uv pip install -r tests/requirements.txt "quacc[dev] @ ."
 
 # ------- Request and run Slurm job -------
