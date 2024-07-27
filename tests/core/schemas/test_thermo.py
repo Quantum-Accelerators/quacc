2330be765093bd539692cb377ef31e1eda240d16
Andrew Rosen <asrosen93@gmail.com>
2024-07-27 01:59:32 -0400

    fix

--- tests/core/schemas/test_thermo.py
+++ tests/core/schemas/test_thermo.py
@@ -92,10 +92,7 @@ def test_summarize_ideal_gas_thermo(tmp_path, caplog):
         results = ThermoSummarize(
             atoms, np.array(vib_energies) / invcm, energy=-10.0, directory=tmp_path
         ).ideal_gas(temperature=1000.0, pressure=20.0)
-    assert (
-        "No multiplicity provided. Using a spin multiplicity of 2 from the Atoms object"
-        in caplog.text
-    )
+    assert "Using a spin multiplicity of 2" in caplog.text
 
     assert results["natoms"] == len(atoms)
     assert results["atoms"] == atoms
