2b13c6533f1caddbf9580d7f3c8c884008c538e5
Andrew Rosen <asrosen93@gmail.com>
2024-07-29 20:13:31 -0400

    fix

--- tests/dask/test_espresso_recipes.py
+++ tests/dask/test_espresso_recipes.py
@@ -56,15 +56,9 @@ def test_phonon_grid_single(tmp_path, monkeypatch):
         )
     ).result()
     # Doing a mistake in qpts here for testing purposes
-    job_params = {
-        "ph_job": {
-            "prev_outdir": relax_output["dir_name"],
-            "input_data": ph_loose,
-            "qpts": [(0.1, 0, 0, 1)],
-        }
-    }
+    job_params = {"ph_job": {"input_data": ph_loose, "qpts": [(0.1, 0, 0, 1)]}}
 
-    future = grid_phonon_flow(job_params=job_params)
+    future = grid_phonon_flow(prev_dir=relax_output["dir_name"], job_params=job_params)
     grid_results = client.compute(future).result()
     sections = [
         "atoms",
