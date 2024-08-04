f73c91abd79917f82582e0fd447ea7db6f021858
Andrew Rosen <asrosen93@gmail.com>
2024-08-04 19:56:19 -0400

    Remove safe dict copy

--- src/quacc/utils/dicts.py
+++ src/quacc/utils/dicts.py
@@ -66,7 +66,7 @@ def recursive_dict_merge(
     old_dict = dicts[0]
     for i in range(len(dicts) - 1):
         merged = _recursive_dict_pair_merge(old_dict, dicts[i + 1], verbose=verbose)
-        old_dict = safe_dict_copy(merged)
+        old_dict = deepcopy(merged)
     return remove_dict_entries(merged, remove_trigger=remove_trigger)
 
 
@@ -94,8 +94,7 @@ def _recursive_dict_pair_merge(
     """
     dict1 = dict1 or ({} if dict1 is None else dict1.__class__())
     dict2 = dict2 or ({} if dict2 is None else dict2.__class__())
-    merged = safe_dict_copy(dict1)
-
+    merged = deepcopy(dict1)
     for key, value in dict2.items():
         if key in merged:
             if isinstance(merged[key], MutableMapping) and isinstance(
@@ -112,27 +111,6 @@ def _recursive_dict_pair_merge(
             merged[key] = value
 
     return merged
-
-
-def safe_dict_copy(d: MutableMapping[str, Any] | dict[str, Any]) -> dict:
-    """
-    Safely copy a dictionary to account for deepcopy errors.
-
-    Parameters
-    ----------
-    d
-        Dictionary to copy
-
-    Returns
-    -------
-    dict
-        Copied dictionary
-    """
-
-    try:
-        return deepcopy(dict(d))
-    except Exception:
-        return dict(d).copy()
 
 
 def remove_dict_entries(
