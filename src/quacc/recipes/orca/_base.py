c4fa8ade24197c10ccb868faf6a2a1c977bd537b
Andrew S. Rosen <asrosen93@gmail.com>
2024-08-04 17:26:29 -0400

    Support ORCA 6.0.0

--- src/quacc/recipes/orca/_base.py
+++ src/quacc/recipes/orca/_base.py
@@ -8,7 +8,7 @@ from ase.calculators.orca import ORCA, OrcaProfile, OrcaTemplate
 
 from quacc import get_settings
 from quacc.runners.ase import Runner
-from quacc.schemas.cclib import CclibSummarize
+from quacc.schemas.ase import Summarize
 from quacc.utils.dicts import recursive_dict_merge
 from quacc.utils.lists import merge_list_params
 
@@ -85,9 +85,9 @@ def run_and_summarize(
         **calc_kwargs,
     )
 
-    atoms = Runner(atoms, calc, copy_files=copy_files).run_calc(geom_file=GEOM_FILE)
+    final_atoms = Runner(atoms, calc, copy_files=copy_files).run_calc(geom_file=GEOM_FILE)
 
-    return CclibSummarize(LOG_FILE, additional_fields=additional_fields).run(atoms)
+    return Summarize(charge_and_multiplicity=(charge, spin_multiplicity), additional_fields=additional_fields).run(final_atoms,atoms)
 
 
 def run_and_summarize_opt(
@@ -153,7 +153,7 @@ def run_and_summarize_opt(
 
     opt_flags = recursive_dict_merge(opt_defaults, opt_params)
     dyn = Runner(atoms, calc, copy_files=copy_files).run_opt(**opt_flags)
-    return CclibSummarize(LOG_FILE, additional_fields=additional_fields).opt(dyn)
+    return Summarize(charge_and_multiplicity=(charge, spin_multiplicity), additional_fields=additional_fields).opt(dyn)
 
 
 def prep_calculator(
