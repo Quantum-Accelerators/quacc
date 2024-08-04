edf1e1301bddd81eff539f4e954b33f259321939
Andrew Rosen <asrosen93@gmail.com>
2024-08-04 18:07:39 -0400

    Support dev version of ASE

--- tests/jenkins/hpc_tests.sh
+++ tests/jenkins/hpc_tests.sh
@@ -17,7 +17,8 @@ pytest tests/core/recipes/gaussian_recipes --noconftest
 
 # ORCA
 module purge
-module load openmpi/gcc/4.1.2
+module load nvhpc/24.5 openmpi/nvhpc-24.5/4.1.6
+export PATH=$SOFTWARE/orca_6_0_0_shared_openmpi416:$PATH
 pytest tests/core/recipes/orca_recipes --noconftest
 
 # VASP
