af755f11594f8c19e4b303f61fe297e571e484f5
Andrew Rosen <asrosen93@gmail.com>
2024-08-04 18:10:38 -0400

    Clean up jenkins

--- tests/jenkins/hpc_tests.sh
+++ tests/jenkins/hpc_tests.sh
@@ -3,10 +3,15 @@
 # Fail fast
 set -e
 
+# Define path to software
+export SOFTWARE=/scratch/network/asrosen/software
+
 # Ensure results are written to a scratch directory
 export QUACC_RESULTS_DIR=$SCRATCH/jenkins
 
 # GULP
+export GULP_LIB=$SOFTWARE/gulp/gulp-6.1.2/Libraries
+export PATH=$SOFTWARe/gulp/gulp-6.1.2/bin:$PATH
 module purge
 pytest tests/core/recipes/gulp_recipes --noconftest
 
@@ -16,12 +21,16 @@ module load gaussian/g16
 pytest tests/core/recipes/gaussian_recipes --noconftest
 
 # ORCA
+export PATH=$SOFTWARE/orca:$PATH
 module purge
 module load openmpi/gcc/4.1.2
 pytest tests/core/recipes/orca_recipes --noconftest
 
 # VASP
 export QUACC_VASP_PARALLEL_CMD="srun -N 1 --ntasks-per-node 32"
+export VASP_PP_PATH=$SOFTWARE/vasp
+export ASE_VASP_VDW=$SOFTWARE/vasp/vdw_kernel.bindat
+export PATH=$SOFTWARE/vasp/vasp.6.4.3/bin:$PATH
 module purge
 module load intel/2021.1.2 intel-mpi/intel/2021.3.1 hdf5/intel-2021.1/1.10.6
 pytest tests/core/recipes/vasp_recipes/jenkins --noconftest
