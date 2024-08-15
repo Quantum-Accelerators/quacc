cee2b094839d28e886430017fbae19360f95a281
Andrew S. Rosen <asrosen93@gmail.com>
2024-08-15 15:53:46 -0400

    Update hpc_tests.sh

--- tests/jenkins/hpc_tests.sh
+++ tests/jenkins/hpc_tests.sh
@@ -6,7 +6,7 @@ set -e
 # Define path to software
 export SOFTWARE=/scratch/network/ROSENGROUP/software
 
-# Ensure results are written to a scratch directory
+# Ensure results are written to a purged directory
 export QUACC_RESULTS_DIR=$SCRATCH/jenkins
 
 # GULP
