5b6a97e6c3bd71bda117b6a90641fbd8231d65cd
Zack Ulissi <zulissi@meta.com>
2024-09-19 00:00:11 +0000

    add nested futures

--- tests/prefect/test_prefect_utils.py
+++ tests/prefect/test_prefect_utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import pytest
 from prefect import flow, task
 
 from quacc.wflow_tools.prefect_utils import (
@@ -16,10 +17,27 @@ def test_resolve_futures_to_results():
     @flow
     def test_flow():
         future = test_task.submit()
-        return resolve_futures_to_results(future)
+        nested_future = {"nest": future}
+        return resolve_futures_to_results(nested_future)
 
     result = test_flow()
-    assert result["test"] == 5
+    assert result["nest"]["test"] == 5
+
+
+def test_resolve_futures_to_results_taskfail():
+    @task
+    def test_task():
+        raise Exception
+        return {"test": 5}
+
+    @flow
+    def test_flow():
+        future = test_task.submit()
+        nested_future = {"nest": future}
+        return resolve_futures_to_results(nested_future)
+
+    with pytest.raises(BaseException):  # noqa: B017
+        test_flow()
 
 
 async def test_resolve_futures_to_results_async():
@@ -30,7 +48,8 @@ async def test_resolve_futures_to_results_async():
     @flow
     async def test_flow():
         future = test_task.submit()
-        return await resolve_futures_to_results_async(future)
+        nested_future = {"nest": future}
+        return await resolve_futures_to_results_async(nested_future)
 
     result = await test_flow()
-    assert result["test"] == 5
+    assert result["nest"]["test"] == 5
