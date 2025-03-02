dc14dfa5e0f9b208039fc158295f38c9c5fb52a1
Andrew S. Rosen <asrosen93@gmail.com>
2025-03-02 12:26:17 -0500

    Update run_jenkins_tests.sh

--- tests/jenkins/run_jenkins_tests.sh
+++ tests/jenkins/run_jenkins_tests.sh
@@ -4,10 +4,11 @@
 source ~/.bashrc
 
 # ------- Package Setup -------
+module load anaconda3/2024.10
 conda activate quacc
 conda install -c conda-forge openbabel
 pip install uv
 uv pip install -r tests/requirements.txt "quacc[dev] @ ."
 
 # ------- Request and run Slurm job -------
-salloc -N 1 -n 32 -t 00:10:00 tests/jenkins/hpc_tests.sh
+salloc -N 1 -n 32 -t 00:15:00 tests/jenkins/hpc_tests.sh
