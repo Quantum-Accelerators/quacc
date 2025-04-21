497cd4a3fecfe27f95ee291eadbd831b20ea5eca
Andrew S. Rosen <asrosen93@gmail.com>
2025-04-21 14:56:09 -0400

    Update test_kpts.py

--- tests/core/utils/test_kpts.py
+++ tests/core/utils/test_kpts.py
@@ -6,4 +6,4 @@ from quacc.utils.kpts import bandgap_to_kspacing
 
 
 def test_bandgap_to_kspacing():
-    assert bandgap_to_kspacing(0.60) == pytest.approx(0.2004)
+    assert bandgap_to_kspacing(0.60) == pytest.approx(0.20040662647547047)
