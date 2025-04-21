56e33740ec3ae03df2a7d6432cddc50c45b105be
Blake Dallmann <bd8619@princeton.edu>
2025-04-21 14:08:37 -0400

    Update test_kpts.py to include test_bandgap_to_kspacing

---
+++ tests/core/utils/test_kpts.py
@@ -0,0 +1,11 @@
+import math
+import pytest
+
+from quacc.utils.kpts import (
+    bandgap_to_kspacing
+)
+
+def test_bandgap_to_kspacing():
+  out = bandgap_to_kspacing(0.60)
+  assert math.isclose(out, 0.2004, rel_tol=1e-6)
+  
