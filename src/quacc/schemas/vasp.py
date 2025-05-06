342dd2e1bbe7bb173acee9c41388ce78b6d05346
Andrew S. Rosen <asrosen93@gmail.com>
2025-05-05 21:02:43 -0400

    Update vasp.py

--- src/quacc/schemas/vasp.py
+++ src/quacc/schemas/vasp.py
@@ -157,7 +157,7 @@ class VaspSummarize:
             additional_fields=self.additional_fields,
         ).run(final_atoms, initial_atoms)
 
-        if nsteps := len([f for f in Path.itderdir(directory) if f.startswith("step")]):
+        if nsteps := len([f for f in Path.iterdir(directory) if f.startswith("step")]):
             intermediate_vasp_task_docs = {
                 "steps": {
                     n: TaskDoc.from_directory(directory / f"step{n}").model_dump()
