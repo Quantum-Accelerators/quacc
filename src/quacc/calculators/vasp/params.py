80197352d0cef545b8a12c1ba478e0be2de63c2a
Andrew S. Rosen <asrosen93@gmail.com>
2025-05-24 22:35:27 -0400

    Update params.py

--- src/quacc/calculators/vasp/params.py
+++ src/quacc/calculators/vasp/params.py
@@ -154,7 +154,8 @@ def get_param_swaps(
 
     if (
         calc.int_params["ismear"] == -5
-        and calc.string_params["algo"].get("normal").lower() == "all"
+        and calc.string_params["algo"]
+        and calc.string_params["algo"].lower() == "all":
     ):
         LOGGER.info(
             "Recommending ALGO = Normal because ISMEAR = -5 was requested, and ALGO = All is not compatible."
