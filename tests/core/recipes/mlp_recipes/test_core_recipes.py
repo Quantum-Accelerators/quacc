20b058b99293c7dcd667e86af9470156af87148a
Andrew S. Rosen <asrosen93@gmail.com>
2025-05-29 09:16:11 -0400

    Update fairchem test

--- tests/core/recipes/mlp_recipes/test_core_recipes.py
+++ tests/core/recipes/mlp_recipes/test_core_recipes.py
@@ -181,7 +181,7 @@ def test_relax_cell_job(tmp_path, monkeypatch, method):
         "mace-mp-0": -32.8069374165035,
         "sevennet": -32.76963806152344,
         "orb": -32.73428726196289,
-        "fairchem": -32.82823944091797,
+        "fairchem": -30.005004590392726,
     }
 
     atoms = bulk("Cu") * (2, 2, 2)
