aa8cd3dd9edba8d9db8923f735e006e0fbff8e5f
Andrew S. Rosen <asrosen93@gmail.com>
2025-12-20 19:51:34 -0500

    Refactor potcar_functional parsing for clarity

--- src/quacc/calculators/vasp/params.py
+++ src/quacc/calculators/vasp/params.py
@@ -574,10 +574,9 @@ class MPtoASEConverter:
             The ASE VASP parameters.
         """
         self.incar_dict = {k.lower(): v for k, v in self.incar_dict.items()}
-        pp, pp_version = (
-            self.potcar_functional.split("_")[0],
-            str(self.potcar_functional.split("_")[1]),
-        )
+        parts = self.potcar_functional.split("_", 1)
+        pp = parts[0]
+        pp_version = parts[1] if len(parts) > 1 and parts[1] else None
         assert pp.lower() in ["lda", "pbe"]
         potcar_setups = {symbol.split("_")[0]: symbol for symbol in self.potcar_symbols}
         for k, v in potcar_setups.items():
