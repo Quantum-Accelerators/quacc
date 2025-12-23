da18ac940d92a55a8d9908e8209235f88c985ff3
Andrew S. Rosen <asrosen93@gmail.com>
2025-12-23 10:23:12 -0500

    Update ase.py

--- src/quacc/runners/ase.py
+++ src/quacc/runners/ase.py
@@ -207,7 +207,7 @@ class Runner(BaseRunner):
         """
         # Set defaults
         merged_optimizer_kwargs = recursive_dict_merge(
-            {"logfile": self.tmpdir / "opt.log", "restart": self.tmpdir / "opt.json"},
+            {"logfile": str(self.tmpdir / "opt.log"), "restart": str(self.tmpdir / "opt.json")},
             optimizer_kwargs,
         )
         run_kwargs = run_kwargs or {}
