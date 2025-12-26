c88325261a75f9db4b29d45ccb12c474288c5f61
Andrew S. Rosen <asrosen93@gmail.com>
2025-12-25 20:08:55 -0500

    Remove 'covalent' from WORKFLOW_ENGINE options
    Removed 'covalent' option from WORKFLOW_ENGINE and its related validation.

--- src/quacc/settings.py
+++ src/quacc/settings.py
@@ -59,7 +59,7 @@ class QuaccSettings(BaseSettings):
     # ---------------------------
 
     WORKFLOW_ENGINE: (
-        Literal["covalent", "dask", "parsl", "prefect", "redun", "jobflow"] | None
+        Literal["dask", "parsl", "prefect", "redun", "jobflow"] | None
     ) = Field(None, description=("The workflow manager to use, if any."))
 
     # ---------------------------
@@ -490,15 +490,6 @@ class QuaccSettings(BaseSettings):
             Loaded settings.
         """
         return _type_handler(cls._use_custom_config_settings(settings))
-
-    @model_validator(mode="after")
-    def enforce_unique_dir_rule(self):
-        """
-        If WORKFLOW_ENGINE is covalent, force CREATE_UNIQUE_DIR = False.
-        """
-        if self.WORKFLOW_ENGINE == "covalent":
-            object.__setattr__(self, "CREATE_UNIQUE_DIR", False)
-        return self
 
 
 def _type_handler(settings: dict[str, Any]) -> dict[str, Any]:
