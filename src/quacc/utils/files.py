11f5800188ec38bd30365d5e5e1aaeb8e3d38be6
Andrew S. Rosen <asrosen93@gmail.com>
2026-01-15 20:23:56 -0500

    Move error folder check to the correct location (#3112)

--- src/quacc/utils/files.py
+++ src/quacc/utils/files.py
@@ -10,7 +10,7 @@ from datetime import UTC, datetime
 from logging import getLogger
 from pathlib import Path
 from random import randint
-from shutil import copy, copytree
+from shutil import copy, copytree, ignore_patterns
 from typing import TYPE_CHECKING
 
 from monty.io import zopen
@@ -157,6 +157,7 @@ def copy_decompress_files(
                     destination_filepath,
                     symlinks=True,
                     dirs_exist_ok=True,
+                    ignore=ignore_patterns("error.*"),
                 )
                 decompress_dir(destination_filepath)
 
