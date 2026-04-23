50f1812a8acd59d4e55ffc82d8fe131abcf49aa4
Andrew S. Rosen <asrosen93@gmail.com>
2026-04-22 23:00:42 -0400

    Fix check for fairchem.omc availability

--- tests/core/recipes/vasp_recipes/mocked/test_vasp_recipes.py
+++ tests/core/recipes/vasp_recipes/mocked/test_vasp_recipes.py
@@ -38,7 +38,7 @@ from quacc.recipes.vasp.slabs import static_job as slab_static_job
 has_atomate2 = util.find_spec("atomate2") is not None
 has_fairchem = util.find_spec("fairchem") is not None
 has_fairchem_omat = has_fairchem and util.find_spec("fairchem.data.omat") is not None
-has_fairchem_omc = has_fairchem and util.find_spec("fairchem.data.omc") is not None
+has_fairchem_omc = has_fairchem and util.find_spec("fairchem.data") is not None and util.find_spec("fairchem.data.omc") is not None
 
 FILE_DIR = Path(__file__).parent
 MOCKED_DIR = FILE_DIR / "mocked_vasp_runs"
