93c5216d36e762845f84edf7397cf39c24834d61
Andrew S. Rosen <asrosen93@gmail.com>
2026-08-13 16:51:26 -0400

    Update matpes.py

--- src/quacc/recipes/vasp/matpes.py
+++ src/quacc/recipes/vasp/matpes.py
@@ -35,7 +35,6 @@ if TYPE_CHECKING:
         pbe: VaspSchema
         r2scan: VaspSchema
 
-
 @job
 @requires(has_atomate2, "atomate2 is not installed. Run `pip install quacc[mp]`")
 def matpes_static_job(
@@ -174,3 +173,56 @@ def matpes_static_flow(
     )
 
     return {"pbe": pbe_results, "r2scan": r2scan_results}
+
+@job
+@requires(has_atomate2, "atomate2 is not installed. Run `pip install quacc[mp]`")
+def fastpes_static_job(
+    atoms: Atoms,
+    *,
+    hf_exchange: float = 0.0,
+    use_improvements: bool = False,
+    write_extra_files: bool = False,
+    auto_ispin: bool = False,
+    prev_dir: SourceDirectory | None = None,
+    **calc_kwargs: Any,
+) -> VaspSchema:
+    """
+    Function to run a MatPES-compatible static calculation.
+
+    Parameters
+    ----------
+    atoms
+        Atoms object
+    hf_exchange
+        The fraction of Hartree-Fock exchange, from 0.0 (PBE) to 1.0.
+        HSE06 is 0.25.
+    kspacing
+        The KSPACING parameter to use. Default: 0.22 as in the MatPES
+        paper. This is likely too expensive in many cases.
+    use_improvements
+        Whether to make the following improvements to the VASP settings:
+        ALGO = All, EFERMI = MIDGAP, GGA_COMPAT = False, ISEARCH = 1,
+        and ENAUG deleted.
+    write_extra_files
+        Whether to write out the following IO files: LELF = True and NEDOS = 3001.
+    auto_ispin
+        If generating input set from a previous calculation, this controls whether
+        to disable magnetisation (ISPIN = 1) if the absolute value of all magnetic
+        moments are less than 0.02.
+    prev_dir
+        A previous directory for a prior step in the workflow.
+    **calc_kwargs
+        Custom kwargs for the Vasp calculator. Set a value to
+        `None` to remove a pre-existing key entirely. For a list of available
+        keys, refer to [quacc.calculators.vasp.vasp.Vasp][]. All of the ASE
+        Vasp calculator keyword arguments are supported.
+
+    Returns
+    -------
+    VaspSchema
+        Dictionary of results from [quacc.schemas.vasp.VaspSummarize.run][].
+        See the type-hint for the data structure.
+    """
+    return matpes_static_job(atoms, level="PBE" if hf_exchange == 0.0 else "HSE06",kspacing=0.40,use_improvements=True,encut=435, isif=2,setups=_fastpes_paw,aexx=hf_exchange,additional_fields={"name": f"FastPES {hf_exchange} Static"}"
+
+_fastpes_paw = {'Ac': '', 'Ag': '_GW', 'Al': '', 'Am': '', 'Ar': '_GW', 'As': '_GW', 'At': '', 'Au': '_GW', 'B': '_GW', 'Ba': '_sv_GW', 'Be': '', 'Bi': '_GW', 'Br': '_GW', 'C': '_s_GW', 'Ca': '_pv', 'Cd': '', 'Ce': '_3', 'Cf': '', 'Cl': '_GW', 'Cm': '', 'Co': '_GW', 'Cr': '', 'Cs': '_sv', 'Cu': '', 'Dy': '_3', 'Er': '_2', 'Eu': '_2', 'F': '_s', 'Fe': '_GW', 'Fr': '_sv', 'Ga': '', 'Gd': '_3', 'Ge': '', 'H': '_GW', 'He': '_GW', 'Hf': '', 'Hg': '', 'Ho': '_3', 'I': '', 'In': '', 'Ir': '', 'K': '_pv', 'Kr': '_GW', 'La': '_s', 'Li': '', 'Lu': '_3', 'Mg': '', 'Mn': '_GW', 'Mo': '', 'N': '_s_GW', 'Na': '', 'Nb': '_pv', 'Nd': '_3', 'Ne': '_s_GW', 'Ni': '', 'Np': '', 'O': '_s_GW', 'Os': '', 'P': '_GW', 'Pa': '_s', 'Pb': '', 'Pd': '', 'Pm': '_3', 'Po': '', 'Pr': '_3', 'Pt': '_GW', 'Pu': '', 'Ra': '_sv', 'Rb': '_pv', 'Re': '', 'Rh': '_GW', 'Rn': '', 'Ru': '', 'S': '_GW', 'Sb': '', 'Sc': '', 'Se': '', 'Si': '_GW', 'Sm': '_3', 'Sn': '', 'Sr': '_sv', 'Ta': '', 'Tb': '_3', 'Tc': '', 'Te': '', 'Th': '_s', 'Ti': '', 'Tl': '', 'Tm': '_3', 'U': '', 'V': '', 'W': '', 'Xe': '_GW', 'Y': '_sv', 'Yb': '_2', 'Zn': '_GW', 'Zr': '_sv'}
