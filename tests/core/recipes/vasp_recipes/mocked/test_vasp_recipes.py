aa5c2b27a68ca964a833989fe3cd6b52cb223f52
Andrew S. Rosen <asrosen93@gmail.com>
2026-08-13 16:39:55 -0400

    Add test for mattersim_static_job functionality
    Added a test for the mattersim_static_job function to validate its output parameters and job name.

--- tests/core/recipes/vasp_recipes/mocked/test_vasp_recipes.py
+++ tests/core/recipes/vasp_recipes/mocked/test_vasp_recipes.py
@@ -36,6 +36,7 @@ from quacc.recipes.vasp.qmof import qmof_relax_job
 from quacc.recipes.vasp.slabs import bulk_to_slabs_flow, slab_to_ads_flow
 from quacc.recipes.vasp.slabs import relax_job as slab_relax_job
 from quacc.recipes.vasp.slabs import static_job as slab_static_job
+from quacc.recipes.vasp.mattersim import mattersim_static_job
 
 has_atomate2 = util.find_spec("atomate2") is not None
 has_fairchem = util.find_spec("fairchem") is not None
@@ -1585,3 +1586,42 @@ def test_md_job_error():
 
     with pytest.raises(ValueError, match="Unsupported ensemble"):
         md_job(atoms, ensemble="NNT")
+
+def test_mattersim_static_job(patch_nonmetallic_taskdoc):
+    atoms = bulk("Ni") * (2, 1, 1)
+    atoms[0].symbol = "O"
+    del atoms.arrays["initial_magmoms"]
+
+    output = mattersim_static_job(atoms, ncore=1)
+
+    assert output["parameters"] == {
+        "algo": "fast",
+        "ediff": 0.0001,
+        "encut": 520,
+        "gamma": True,
+        "isif": 3,
+        "ismear": -5,
+        "ispin": 2,
+        "kpts": (5, 11, 11),
+        "lasph": True,
+        "ldau": True,
+        "ldauj": [0, 0],
+        "ldaul": [0, 2],
+        "ldauprint": 1,
+        "ldautype": 2,
+        "ldauu": [0, 6.2],
+        "lmaxmix": 4,
+        "lorbit": 11,
+        "lreal": "auto",
+        "lwave": False,
+        "magmom": [0.6, 5],
+        "nelm": 100,
+        "ncore": 1,
+        "nsw": 0,
+        "pp": "pbe",
+        "pp_version": "original",
+        "prec": "accurate",
+        "setups": {"Ni": "_pv", "O": ""},
+        "sigma": 0.05,
+    }
+    assert output["name"] == "MatterSim Static"
